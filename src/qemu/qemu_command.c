c38a3ada91c8006a0aef52117d65622c2a370bbb
Michal Privoznik <mprivozn@redhat.com>
2016-08-01 17:10:09 +0200

    qemu: Implement memAccess for <memory/> banks
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <e2c63229be42c5c25b017e05acf6ec276878b423.1470062741.git.mprivozn@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -3064,6 +3064,7 @@ qemuBuildControllerDevCommandLine(virCommandPtr cmd,
  * @hostNodes: map of host nodes to alloc the memory in, NULL for default
  * @autoNodeset: fallback nodeset in case of automatic numa placement
  * @memPath: request memory-backend-file with specific mem-path
+ * @memAccessReq: specifically requested memAccess mode
  * @def: domain definition object
  * @qemuCaps: qemu capabilities object
  * @cfg: qemu driver config object
@@ -3086,6 +3087,7 @@ qemuBuildMemoryBackendStr(unsigned long long size,
                           virBitmapPtr userNodeset,
                           virBitmapPtr autoNodeset,
                           const char *memPath,
+                          virNumaMemAccess memAccessReq,
                           virDomainDefPtr def,
                           virQEMUCapsPtr qemuCaps,
                           virQEMUDriverConfigPtr cfg,
@@ -3120,6 +3122,9 @@ qemuBuildMemoryBackendStr(unsigned long long size,
 
         memAccess = virDomainNumaGetNodeMemoryAccessMode(def->numa, guestNode);
     }
+
+    if (memAccessReq)
+        memAccess = memAccessReq;
 
     if (virDomainNumatuneGetMode(def->numa, guestNode, &mode) < 0 &&
         virDomainNumatuneGetMode(def->numa, -1, &mode) < 0)
@@ -3320,7 +3325,8 @@ qemuBuildMemoryCellBackendStr(virDomainDefPtr def,
         goto cleanup;
 
     if ((rc = qemuBuildMemoryBackendStr(memsize, 0, cell, NULL, auto_nodeset,
-                                        NULL, def, qemuCaps, cfg, &backendType,
+                                        NULL, VIR_NUMA_MEM_ACCESS_DEFAULT,
+                                        def, qemuCaps, cfg, &backendType,
                                         &props, false)) < 0)
         goto cleanup;
 
@@ -3362,7 +3368,8 @@ qemuBuildMemoryDimmBackendStr(virDomainMemoryDefPtr mem,
 
     if (qemuBuildMemoryBackendStr(mem->size, mem->pagesize,
                                   mem->targetNode, mem->sourceNodes, auto_nodeset,
-                                  mem->path, def, qemuCaps, cfg,
+                                  mem->path, mem->memAccess,
+                                  def, qemuCaps, cfg,
                                   &backendType, &props, true) < 0)
         goto cleanup;
 
