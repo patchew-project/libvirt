patchew-project/libvirt

commit b3e4401dc6203b16f4fb2c123a6f033f1747f448

systemd: don't report an error if the guest is already terminated
In many cases where we invoke virSystemdTerminateMachine the
process(es) will have already gone away on their own accord.
In these cases we log an error message that the machine does
not exist. We should catch this particular error and simply
ignore it, so we don't pollute the logs.

src/util/virsystemd.c
@@ -338,18 +338,22 @@ int virSystemdTerminateMachine(const char *name,
     int ret;
     DBusConnection *conn;
     char *machinename = NULL;
+    virError error;
+
+    memset(&error, 0, sizeof(error));
 
     ret = virDBusIsServiceEnabled("org.freedesktop.machine1");
     if (ret < 0)
-        return ret;
+        goto cleanup;
 
     if ((ret = virDBusIsServiceRegistered("org.freedesktop.systemd1")) < 0)
-        return ret;
-
-    if (!(conn = virDBusGetSystemBus()))
-        return -1;
+        goto cleanup;
 
     ret = -1;
+
+    if (!(conn = virDBusGetSystemBus()))
+        goto cleanup;
+
     if (!(machinename = virSystemdMakeMachineName(name, drivername, privileged)))
         goto cleanup;
 
@@ -366,7 +370,7 @@ int virSystemdTerminateMachine(const char *name,
     VIR_DEBUG("Attempting to terminate machine via systemd");
     if (virDBusCallMethod(conn,
                           NULL,
-                          NULL,
+                          &error,
                           "org.freedesktop.machine1",
                           "/org/freedesktop/machine1",
                           "org.freedesktop.machine1.Manager",
@@ -375,9 +379,18 @@ int virSystemdTerminateMachine(const char *name,
                           machinename) < 0)
         goto cleanup;
 
+    if (error.code == VIR_ERR_ERROR &&
+        !STREQ_NULLABLE("org.freedesktop.machine1.NoSuchMachine",
+                        error.str1)) {
+        virReportErrorObject(&error);
+        goto cleanup;
+    }
+
     ret = 0;
 
  cleanup:
+    virResetError(&error);
+
     VIR_FREE(machinename);
     return ret;
 }

commit 447503578f581e5ec296c48987c2fda79b88f7a4

virsystemd: Fix error check
Commit b3e4401dc620 introduced a check to ignore an error if the guest
is already terminated. However the check accidentally compared
error.code with VIR_ERR_ERROR, which is an error level, not an error
code. Because of this, almost every error got silently ignored.

Fixes: b3e4401dc620 ("systemd: don't report an error if the guest is
already terminated")

Signed-off-by: Marc Hartmayer <mhartmay@linux.vnet.ibm.com>
Reviewed-by: Sascha Silbe <silbe@linux.vnet.ibm.com>
Reviewed-by: Boris Fiuczynski <fiuczy@linux.vnet.ibm.com>
Message-Id: <1470413067-119023-1-git-send-email-mhartmay@linux.vnet.ibm.com>
Reviewed-by: Sascha Silbe <silbe@linux.vnet.ibm.com>
Reviewed-by: Boris Fiuczynski <fiuczy@linux.vnet.ibm.com>

src/util/virsystemd.c
@@ -468,7 +468,7 @@ int virSystemdTerminateMachine(const char *name)
                           name) < 0)
         goto cleanup;
 
-    if (error.code == VIR_ERR_ERROR &&
+    if (error.level == VIR_ERR_ERROR &&
         STRNEQ_NULLABLE("org.freedesktop.machine1.NoSuchMachine",
                         error.str1)) {
         virReportErrorObject(&error);