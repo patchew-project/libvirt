a535ed79d671f8a7fbef1a418066c72e0d706608
Michal Privoznik <mprivozn@redhat.com>
2016-08-11 15:26:23 +0200

    qemu: Introduce QEMU_CAPS_DEVICE_NVDIMM
    Introduce a qemu capability for -device nvdimm.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <c3ed1f36b8bc9529fb8f23f1f361b4ded2ca2f4e.1470921599.git.mprivozn@redhat.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -340,6 +340,7 @@ VIR_ENUM_IMPL(virQEMUCaps, QEMU_CAPS_LAST,
               "display", /* 230 */
               "intel-iommu",
               "smm",
+              "nvdimm",
     );
 
 
@@ -1561,6 +1562,7 @@ struct virQEMUCapsStringFlags virQEMUCapsObjectTypes[] = {
     { "pxb-pcie", QEMU_CAPS_DEVICE_PXB_PCIE },
     { "tls-creds-x509", QEMU_CAPS_OBJECT_TLS_CREDS_X509 },
     { "intel-iommu", QEMU_CAPS_DEVICE_INTEL_IOMMU },
+    { "nvdimm", QEMU_CAPS_DEVICE_NVDIMM },
 };
 
 static struct virQEMUCapsStringFlags virQEMUCapsObjectPropsVirtioBalloon[] = {
