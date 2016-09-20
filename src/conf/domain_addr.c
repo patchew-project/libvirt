6b54597270d37c5fbd6d9f19d240cc904e59ee91
Laine Stump <laine@laine.org>
2016-09-20 15:14:11 -0400

    conf: new function virDomainPCIAddressReserveNextAddr()
    There is an existing virDomainPCIAddressReserveNextSlot() which will
    reserve all functions of the next available PCI slot. One place in the
    qemu PCI address assignment code requires reserving a *single*
    function of the next available PCI slot. This patch modifies and
    renames virDomainPCIAddressReserveNextSlot() so that it can fulfill
    both the original purpose and the need to reserve a single function.
    
    (This is being done so that the abovementioned code in qemu can have
    its "kind of open coded" solution replaced with a call to this new
    function).
    Message-Id: <1474398864-9039-6-git-send-email-laine@laine.org>

--- src/conf/domain_addr.c
+++ src/conf/domain_addr.c
@@ -691,26 +691,66 @@ virDomainPCIAddressGetNextSlot(virDomainPCIAddressSetPtr addrs,
     return 0;
 }
 
+/**
+ * virDomainPCIAddressReserveNextAddr:
+ *
+ *    find the next *completely unreserved* slot with compatible
+ *    connection @flags, and mark either one function or the entire
+ *    slot (according to @function and @reserveEntireSlot).
+ *
+ * @addrs:     a set of addresses - one bit for each function on each
+ *             slot on each bus, set if in use, clear if not in use.
+ *
+ * @dev:       virDomainDeviceInfo that should get the new address set.
+ *
+ * @flags:     CONNECT_TYPE flags for the port we're looking for
+ *
+ * @function:  which function on the slot to mark as reserved
+ *             (if @reserveEntireSlot is false)
+ *
+ * @reserveEntireSlot: true to reserve all functions on the new slot,
+ *              false to reserve just @function
+ *
+ *
+ * returns 0 (and dev->addr.pci set) on success, or -1 on failure.
+ */
 int
-virDomainPCIAddressReserveNextSlot(virDomainPCIAddressSetPtr addrs,
+virDomainPCIAddressReserveNextAddr(virDomainPCIAddressSetPtr addrs,
                                    virDomainDeviceInfoPtr dev,
-                                   virDomainPCIConnectFlags flags)
+                                   virDomainPCIConnectFlags flags,
+                                   unsigned int function,
+                                   bool reserveEntireSlot)
 {
     virPCIDeviceAddress addr;
+
     if (virDomainPCIAddressGetNextSlot(addrs, &addr, flags) < 0)
         return -1;
 
-    if (virDomainPCIAddressReserveSlot(addrs, &addr, flags) < 0)
+    addr.function = reserveEntireSlot ? 0 : function;
+
+    if (virDomainPCIAddressReserveAddr(addrs, &addr, flags, reserveEntireSlot, false) < 0)
         return -1;
+
+    addrs->lastaddr = addr;
+    addrs->lastaddr.function = 0;
+    addrs->lastaddr.multi = VIR_TRISTATE_SWITCH_ABSENT;
+    addrs->lastFlags = flags;
 
     if (!addrs->dryRun) {
         dev->type = VIR_DOMAIN_DEVICE_ADDRESS_TYPE_PCI;
         dev->addr.pci = addr;
     }
 
-    addrs->lastaddr = addr;
-    addrs->lastFlags = flags;
     return 0;
+}
+
+
+int
+virDomainPCIAddressReserveNextSlot(virDomainPCIAddressSetPtr addrs,
+                                   virDomainDeviceInfoPtr dev,
+                                   virDomainPCIConnectFlags flags)
+{
+    return virDomainPCIAddressReserveNextAddr(addrs, dev, flags, 0, true);
 }
 
 
