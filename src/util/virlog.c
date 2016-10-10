patchew-project/libvirt

commit 640b58abdf2c5e97476dd6e2be475cfab311e9ad

virlog: Take a special care of syslog when setting new set of log outputs
Now that we're in the critical section, syslog connection can be re-opened
by issuing openlog, which is something that cannot be done beforehand, since
syslog keeps its file descriptor private and changing the tag earlier might
introduce a log inconsistency if something went wrong with preparing a new set
of logging outputs in order to replace the existing one.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

src/util/virlog.c
@@ -1794,16 +1794,37 @@ virLogFindOutput(virLogOutputPtr *outputs, size_t noutputs,
 int
 virLogDefineOutputs(virLogOutputPtr *outputs, size_t noutputs)
 {
+    int ret = -1;
+    int id;
+    char *tmp = NULL;
+
     if (virLogInitialize() < 0)
         return -1;
 
     virLogLock();
     virLogResetOutputs();
+
+    /* syslog needs to be special-cased, since it keeps the fd in private */
+    if ((id = virLogFindOutput(outputs, noutputs, VIR_LOG_TO_SYSLOG,
+                               current_ident)) != -1) {
+        /* nothing can go wrong now (except for malloc) and since we're also
+         * holding the lock so it's safe to call openlog and change the message
+         * tag
+         */
+        if (VIR_STRDUP_QUIET(tmp, outputs[id]->name) < 0)
+            goto cleanup;
+        VIR_FREE(current_ident);
+        current_ident = tmp;
+        openlog(current_ident, 0, 0);
+    }
+
     virLogOutputs = outputs;
     virLogNbOutputs = noutputs;
-    virLogUnlock();
 
-    return 0;
+    ret = 0;
+ cleanup:
+    virLogUnlock();
+    return ret;
 }
 
 

commit f83f4630d764f29212739f2eecd556fe685e6fdf

virlog: Enclose part of virLogDefineOutputs in HAVE_SYSLOG_H block
Commit 640b58ab broke the mingw build because it referenced 'current_ident'
as well as 'openlog' symbols both of which are declared conditionally within
HAVE_SYSLOG_H directive. This patch fixes the broken build. Additional changes
like moving all variable declarations in virLogDefineOutputs into the
conditional block were necessary to avoid 'unused variable' errors from mingw.

Signed-off-by: Erik Skultety <eskultet@redhat.com>
Message-Id: <7e2367fadf513b5dc6566e3a52b8b79456c10725.1476100938.git.eskultet@redhat.com>

src/util/virlog.c
@@ -1362,16 +1362,15 @@ virLogFindOutput(virLogOutputPtr *outputs, size_t noutputs,
 int
 virLogDefineOutputs(virLogOutputPtr *outputs, size_t noutputs)
 {
-    int ret = -1;
-    int id;
-    char *tmp = NULL;
-
     if (virLogInitialize() < 0)
         return -1;
 
     virLogLock();
     virLogResetOutputs();
 
+#if HAVE_SYSLOG_H
+    int id = -1;
+    char *tmp = NULL;
     /* syslog needs to be special-cased, since it keeps the fd in private */
     if ((id = virLogFindOutput(outputs, noutputs, VIR_LOG_TO_SYSLOG,
                                current_ident)) != -1) {
@@ -1379,20 +1378,21 @@ virLogDefineOutputs(virLogOutputPtr *outputs, size_t noutputs)
          * holding the lock so it's safe to call openlog and change the message
          * tag
          */
-        if (VIR_STRDUP_QUIET(tmp, outputs[id]->name) < 0)
-            goto cleanup;
+        if (VIR_STRDUP_QUIET(tmp, outputs[id]->name) < 0) {
+            virLogUnlock();
+            return -1;
+        }
         VIR_FREE(current_ident);
         current_ident = tmp;
         openlog(current_ident, 0, 0);
     }
+#endif
 
     virLogOutputs = outputs;
     virLogNbOutputs = noutputs;
 
-    ret = 0;
- cleanup:
     virLogUnlock();
-    return ret;
+    return 0;
 }
 
 