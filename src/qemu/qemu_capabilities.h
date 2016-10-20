a0c3e1cfd960d773ec568525600b08db8a5fad5f
Peter Krempa <pkrempa@redhat.com>
2016-10-20 17:25:02 +0200

    qemu: capabilities: Add support for QMP schema introspection
    Allow detecting capabilities according to the qemu QMP schema. This is
    necessary as sometimes the availability of certain options depends on
    the presence of a field in the schema.
    
    This patch adds support for loading the QMP schema when detecting qemu
    capabilities and adds a very simple query language to allow traversing
    the schema and selecting a certain element from it.
    
    The infrastructure in this patch allows to use a query path and set a
    specific capability flag according to the availability of the given
    element in the schema.
    Message-Id: <894ebf1cb8f80a6b3c9b38a1ce3a6646f760026c.1476976716.git.pkrempa@redhat.com>

--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -380,6 +380,7 @@ typedef enum {
     QEMU_CAPS_VIRTIO_NET_RX_QUEUE_SIZE, /* virtio-net-*.rx_queue_size */
     QEMU_CAPS_MACHINE_IOMMU, /* -machine iommu=on */
     QEMU_CAPS_DEVICE_VIRTIO_VGA, /* -device virtio-vga */
+    QEMU_CAPS_QUERY_QMP_SCHEMA, /* query-qmp-schema command */
 
     QEMU_CAPS_LAST /* this must always be the last item */
 } virQEMUCapsFlags;
