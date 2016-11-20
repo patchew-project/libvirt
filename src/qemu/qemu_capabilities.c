eedcf657b4a5b7bda68cb130d7d72f598c42729a
Jiri Denemark <jdenemar@redhat.com>
2016-11-21 00:20:59 +0100

    qemu: Probe KVM state earlier
    Let's set QEMU_CAPS_KVM and QEMU_CAPS_ENABLE_KVM early so that the rest
    of the probing code can use these capabilities to handle KVM/TCG replies
    differently.
    
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
    Message-Id: <d85dfca4262ca99f049a9cd16548d22eb274070f.1479683721.git.jdenemar@redhat.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -3980,6 +3980,11 @@ virQEMUCapsInitQMPMonitor(virQEMUCapsPtr qemuCaps,
 
     if (virQEMUCapsProbeQMPCommands(qemuCaps, mon) < 0)
         goto cleanup;
+
+    /* Some capabilities may differ depending on KVM state */
+    if (virQEMUCapsProbeQMPKVMState(qemuCaps, mon) < 0)
+        goto cleanup;
+
     if (virQEMUCapsProbeQMPEvents(qemuCaps, mon) < 0)
         goto cleanup;
     if (virQEMUCapsProbeQMPObjects(qemuCaps, mon) < 0)
@@ -3987,8 +3992,6 @@ virQEMUCapsInitQMPMonitor(virQEMUCapsPtr qemuCaps,
     if (virQEMUCapsProbeQMPMachineTypes(qemuCaps, mon) < 0)
         goto cleanup;
     if (virQEMUCapsProbeQMPCPUDefinitions(qemuCaps, mon) < 0)
-        goto cleanup;
-    if (virQEMUCapsProbeQMPKVMState(qemuCaps, mon) < 0)
         goto cleanup;
     if (virQEMUCapsProbeQMPTPM(qemuCaps, mon) < 0)
         goto cleanup;
