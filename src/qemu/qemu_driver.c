04d8f02b2f914dd2628295e003c541f9f8d632b4
Nitesh Konkar <niteshkonkar.libvirt@gmail.com>
2016-12-13 21:31:38 +0530

    perf: add bus_cycles perf event support
    This patch adds support and documentation
    for the bus_cycles perf event.
    
    Signed-off-by: Nitesh Konkar <nitkon12@linux.vnet.ibm.com>
    Message-Id: <1481644901-30902-2-git-send-email-niteshkonkar.libvirt@gmail.com>

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -9854,6 +9854,7 @@ qemuDomainSetPerfEvents(virDomainPtr dom,
                                VIR_PERF_PARAM_CACHE_MISSES, VIR_TYPED_PARAM_BOOLEAN,
                                VIR_PERF_PARAM_BRANCH_INSTRUCTIONS, VIR_TYPED_PARAM_BOOLEAN,
                                VIR_PERF_PARAM_BRANCH_MISSES, VIR_TYPED_PARAM_BOOLEAN,
+                               VIR_PERF_PARAM_BUS_CYCLES, VIR_TYPED_PARAM_BOOLEAN,
                                NULL) < 0)
         return -1;
 
