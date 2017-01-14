2319d0111cb69e7f3ffb2fe5663581d23aa54bdb
Nitesh Konkar <niteshkonkar.libvirt@gmail.com>
2017-01-14 13:49:59 +0530

    perf: Compute cache_l1d config value correctly
    This patch computes the .attrConfig value for
    cache_l1d correctly and updates the documentation.
    The cache_l1d perf event now is renamed as
    cache_l1dra perf event for measuring read accesses
    for level 1 data cache
    
    Signed-off-by: Nitesh Konkar <nitkon12@linux.vnet.ibm.com>
    Message-Id: <1484382000-17426-2-git-send-email-niteshkonkar.libvirt@gmail.com>

--- src/util/virperf.c
+++ src/util/virperf.c
@@ -44,7 +44,7 @@ VIR_ENUM_IMPL(virPerfEvent, VIR_PERF_EVENT_LAST,
               "branch_instructions", "branch_misses",
               "bus_cycles", "stalled_cycles_frontend",
               "stalled_cycles_backend", "ref_cpu_cycles",
-              "cache_l1d");
+              "cache_l1dra");
 
 struct virPerfEvent {
     int type;
@@ -113,9 +113,11 @@ static struct virPerfEventAttr attrs[] = {
      .attrConfig = 0,
 # endif
     },
-    {.type = VIR_PERF_EVENT_CACHE_L1D,
+    {.type = VIR_PERF_EVENT_CACHE_L1DRA,
      .attrType = PERF_TYPE_HW_CACHE,
-     .attrConfig = PERF_COUNT_HW_CACHE_L1D},
+     .attrConfig = (PERF_COUNT_HW_CACHE_L1D) |
+                   (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                   (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16)},
 };
 typedef struct virPerfEventAttr *virPerfEventAttrPtr;
 
