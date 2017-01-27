0001e56a8abbf57cbcc6a3233d6c1a4391644fee
Nitesh Konkar <niteshkonkar.libvirt@gmail.com>
2017-01-27 16:31:00 +0530

    perf: add cpu_clock software perf event support
    This patch adds support and documentation for
    the cpu_clock perf event.
    
    Signed-off-by: Nitesh Konkar <nitkon12@linux.vnet.ibm.com>
    Message-Id: <1485514868-31441-2-git-send-email-niteshkonkar.libvirt@gmail.com>

--- include/libvirt/libvirt-domain.h
+++ include/libvirt/libvirt-domain.h
@@ -2188,6 +2188,16 @@ void virDomainStatsRecordListFree(virDomainStatsRecordPtr *stats);
  */
 # define VIR_PERF_PARAM_REF_CPU_CYCLES "ref_cpu_cycles"
 
+/**
+ * VIR_PERF_PARAM_CPU_CLOCK:
+ *
+ * Macro for typed parameter name that represents cpu_clock
+ * perf event which can be used to measure the count of cpu
+ * clock by applications running on the platform. It corresponds
+ * to the "perf.cpu_clock" field in the *Stats APIs.
+ */
+# define VIR_PERF_PARAM_CPU_CLOCK "cpu_clock"
+
 int virDomainGetPerfEvents(virDomainPtr dom,
                            virTypedParameterPtr *params,
                            int *nparams,
