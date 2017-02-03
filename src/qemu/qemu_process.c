4007badc73fdd8c08b8887f81f5348b82e781200
Eli Qiao <liyong.qiao@intel.com>
2017-02-03 11:47:16 +0800

    Qemu: Set cache banks
    Set cache banks while booting a new domain.
    
    Signed-off-by: Eli Qiao <liyong.qiao@intel.com>
    Message-Id: <1486093636-63999-6-git-send-email-liyong.qiao@intel.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -76,6 +76,7 @@
 #include "configmake.h"
 #include "nwfilter_conf.h"
 #include "netdev_bandwidth_conf.h"
+#include "virresctrl.h"
 
 #define VIR_FROM_THIS VIR_FROM_QEMU
 
@@ -5714,6 +5715,11 @@ qemuProcessLaunch(virConnectPtr conn,
         qemuProcessAutoDestroyAdd(driver, vm, conn) < 0)
         goto cleanup;
 
+    VIR_DEBUG("Cache allocation");
+
+    if (virResCtrlSetCacheBanks(&(vm->def->cachetune), vm->def->uuid, vm->pid) < 0)
+        goto cleanup;
+
     ret = 0;
 
  cleanup:
@@ -6215,6 +6221,10 @@ void qemuProcessStop(virQEMUDriverPtr driver,
 
     virPerfFree(priv->perf);
     priv->perf = NULL;
+
+    if(virResCtrlUpdate() < 0)
+        VIR_WARN("Failed to update resource control for %s",
+                 vm->def->name);
 
     qemuProcessRemoveDomainStatus(driver, vm);
 
