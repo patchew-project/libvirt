107c557d45d1a855bdb88701e97878ca1d706325
Michal Privoznik <mprivozn@redhat.com>
2017-02-27 14:19:23 +0100

    qemuBuildMemoryBackendStr: Reorder args and update comment
    Frankly, this function is one big mess. A lot of arguments,
    complicated behaviour. It's really surprising that arguments were
    in random order (input and output arguments were mixed together),
    the documentation was outdated, the description of return values
    was bogus.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <2602c7ec0320061df33682a93de531aaf8bff5ee.1488201118.git.mprivozn@redhat.com>

--- src/qemu/qemu_hotplug.c
+++ src/qemu/qemu_hotplug.c
@@ -2216,10 +2216,9 @@ qemuDomainAttachMemory(virQEMUDriverPtr driver,
     if (!(devstr = qemuBuildMemoryDeviceStr(mem)))
         goto cleanup;
 
-    if (qemuBuildMemoryBackendStr(mem->size, mem->pagesize,
-                                  mem->targetNode, mem->sourceNodes, NULL,
-                                  vm->def, priv->qemuCaps, cfg,
-                                  &backendType, &props, true) < 0)
+    if (qemuBuildMemoryBackendStr(&props, &backendType, cfg, priv->qemuCaps,
+                                  vm->def, mem->targetNode, mem->size,
+                                  mem->pagesize, mem->sourceNodes, NULL, true) < 0)
         goto cleanup;
 
     if (virDomainMemoryInsert(vm->def, mem) < 0) {
