53f1b4e5203a62a443eef7ccd5188bb778214dad
Peter Krempa <pkrempa@redhat.com>
2017-03-17 16:48:49 +0100

    rpc: socket: Add possibility to suppress errors on read hangup
    In some cases a read error due to connection hangup is expected. This
    patch adds a flag that removes the logging of a virError in such case.
    Message-Id: <55534f63a59d9253d98b77430413c2366f3cce75.1489765683.git.pkrempa@redhat.com>

--- src/rpc/virnetsocket.c
+++ src/rpc/virnetsocket.c
@@ -82,6 +82,7 @@ struct _virNetSocket {
     int errfd;
     bool client;
     bool ownsFd;
+    bool quietEOF;
 
     /* Event callback fields */
     virNetSocketIOFunc func;
@@ -1792,12 +1793,18 @@ static ssize_t virNetSocketReadWire(virNetSocketPtr sock, char *buf, size_t len)
                                  _("Cannot recv data"));
         ret = -1;
     } else if (ret == 0) {
-        if (errout)
-            virReportSystemError(EIO,
-                                 _("End of file while reading data: %s"), errout);
-        else
-            virReportSystemError(EIO, "%s",
-                                 _("End of file while reading data"));
+        if (sock->quietEOF) {
+            VIR_DEBUG("socket='%p' EOF while reading: errout='%s'",
+                      socket, NULLSTR(errout));
+        } else {
+            if (errout)
+                virReportSystemError(EIO,
+                                     _("End of file while reading data: %s"),
+                                     errout);
+            else
+                virReportSystemError(EIO, "%s",
+                                     _("End of file while reading data"));
+        }
         ret = -1;
     }
 
@@ -2232,4 +2239,18 @@ void virNetSocketClose(virNetSocketPtr sock)
 #endif
 
     virObjectUnlock(sock);
+}
+
+
+/**
+ * virNetSocketSetQuietEOF:
+ * @sock: socket object pointer
+ *
+ * Disables reporting I/O errors as a virError when @socket is closed while
+ * reading data.
+ */
+void
+virNetSocketSetQuietEOF(virNetSocketPtr sock)
+{
+    sock->quietEOF = true;
 }
