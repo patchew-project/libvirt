9b44fc21a087d01931e680d1baace4282d913880
Roman Bogorodskiy <bogorodskiy@gmail.com>
2017-03-27 19:25:16 +0400

    netdev: fix build on non-Linux
    Fix some typos in the source code:
    
     - Missplaced ")",
     - virNetDevVLanPtr -> virNetDevVlanPtr
    Message-Id: <20170327152516.38756-1-bogorodskiy@gmail.com>

--- src/util/virnetdev.c
+++ src/util/virnetdev.c
@@ -316,7 +316,7 @@ virNetDevSetMACInternal(const char *ifname,
 
         ret = 0;
  cleanup:
-        VIR_DEBUG("SIOCSIFLLADDR %s MAC=%s - %s", ifname, mac + 1),
+        VIR_DEBUG("SIOCSIFLLADDR %s MAC=%s - %s", ifname, mac + 1,
                   ret < 0 ? "Fail" : "Success");
 
         VIR_FORCE_CLOSE(s);
@@ -2336,7 +2336,7 @@ virNetDevReadNetConfig(const char *linkdev ATTRIBUTE_UNUSED,
                        int vf ATTRIBUTE_UNUSED,
                        const char *stateDir ATTRIBUTE_UNUSED,
                        virMacAddrPtr *adminMAC ATTRIBUTE_UNUSED,
-                       virNetDevVLanPtr *vlan ATTRIBUTE_UNUSED,
+                       virNetDevVlanPtr *vlan ATTRIBUTE_UNUSED,
                        virMacAddrPtr *MAC ATTRIBUTE_UNUSED)
 {
     virReportSystemError(ENOSYS, "%s",
