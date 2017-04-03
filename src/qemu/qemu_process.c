2846356fa1221cd942664da665413f6082a0649d
Marc Hartmayer <mhartmay@linux.vnet.ibm.com>
2017-04-03 10:24:36 +0200

    qemu: Turn qemuDomainLogContext into virObject
    This way qemuDomainLogContextRef() and qemuDomainLogContextFree() is
    no longer needed. The naming qemuDomainLogContextFree() was also
    somewhat misleading. Additionally, it's easier to turn
    qemuDomainLogContext in a self-locking object.
    
    Signed-off-by: Marc Hartmayer <mhartmay@linux.vnet.ibm.com>
    Reviewed-by: Bjoern Walk <bwalk@linux.vnet.ibm.com>
    Message-Id: <20170403082439.10180-3-mhartmay@linux.vnet.ibm.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -1692,7 +1692,7 @@ static void
 qemuProcessMonitorLogFree(void *opaque)
 {
     qemuDomainLogContextPtr logCtxt = opaque;
-    qemuDomainLogContextFree(logCtxt);
+    virObjectUnref(logCtxt);
 }
 
 static int
@@ -1731,7 +1731,7 @@ qemuConnectMonitor(virQEMUDriverPtr driver, virDomainObjPtr vm, int asyncJob,
                           driver);
 
     if (mon && logCtxt) {
-        qemuDomainLogContextRef(logCtxt);
+        virObjectRef(logCtxt);
         qemuMonitorSetDomainLog(mon,
                                 qemuProcessMonitorReportLogError,
                                 logCtxt,
@@ -5871,7 +5871,7 @@ qemuProcessLaunch(virConnectPtr conn,
  cleanup:
     qemuDomainSecretDestroy(vm);
     virCommandFree(cmd);
-    qemuDomainLogContextFree(logCtxt);
+    virObjectUnref(logCtxt);
     virObjectUnref(cfg);
     virObjectUnref(caps);
     VIR_FREE(nicindexes);
@@ -6667,7 +6667,7 @@ int qemuProcessAttach(virConnectPtr conn ATTRIBUTE_UNUSED,
             goto error;
     }
 
-    qemuDomainLogContextFree(logCtxt);
+    virObjectUnref(logCtxt);
     VIR_FREE(seclabel);
     VIR_FREE(sec_managers);
     virObjectUnref(cfg);
@@ -6687,7 +6687,7 @@ int qemuProcessAttach(virConnectPtr conn ATTRIBUTE_UNUSED,
 
     qemuMonitorClose(priv->mon);
     priv->mon = NULL;
-    qemuDomainLogContextFree(logCtxt);
+    virObjectUnref(logCtxt);
     VIR_FREE(seclabel);
     VIR_FREE(sec_managers);
     if (seclabelgen)
