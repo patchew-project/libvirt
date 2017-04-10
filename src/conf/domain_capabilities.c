patchew-project/libvirt

commit 14319c81a0c50f024a9cc9fda854d12af7d86a01

Show host model in domain capabilities
The domain capabilities XML is capable of showing whether each guest CPU
mode is supported or not with a possibility to provide additional
details. This patch enhances host-model capability to advertise the
exact CPU model which will be used as a host-model:

    <cpu>
        ...
        <mode name='host-model' supported='yes'>
            <model fallback='allow'>Broadwell</model>
            <vendor>Intel</vendor>
            <feature policy='disable' name='aes'/>
            <feature policy='require' name='vmx'/>
        </mode>
        ...
    </cpu>

Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

src/conf/domain_capabilities.c
@@ -402,9 +402,19 @@ virDomainCapsCPUFormat(virBufferPtr buf,
                       virCPUModeTypeToString(VIR_CPU_MODE_HOST_PASSTHROUGH),
                       cpu->hostPassthrough ? "yes" : "no");
 
-    virBufferAsprintf(buf, "<mode name='%s' supported='%s'/>\n",
-                      virCPUModeTypeToString(VIR_CPU_MODE_HOST_MODEL),
-                      cpu->hostModel ? "yes" : "no");
+    virBufferAsprintf(buf, "<mode name='%s' ",
+                      virCPUModeTypeToString(VIR_CPU_MODE_HOST_MODEL));
+    if (cpu->hostModel) {
+        virBufferAddLit(buf, "supported='yes'>\n");
+        virBufferAdjustIndent(buf, 2);
+
+        virCPUDefFormatBuf(buf, cpu->hostModel, false);
+
+        virBufferAdjustIndent(buf, -2);
+        virBufferAddLit(buf, "</mode>\n");
+    } else {
+        virBufferAddLit(buf, "supported='no'/>\n");
+    }
 
     virBufferAsprintf(buf, "<mode name='%s' ",
                       virCPUModeTypeToString(VIR_CPU_MODE_CUSTOM));

docs/formatdomaincaps.html.in
@@ -154,7 +154,12 @@
   ...
   &lt;cpu&gt;
     &lt;mode name='host-passthrough' supported='yes'/&gt;
-    &lt;mode name='host-model' supported='yes'/&gt;
+    &lt;mode name='host-model' supported='yes'&gt;
+      &lt;model fallback='allow'&gt;Broadwell&lt;/model&gt;
+      &lt;vendor&gt;Intel&lt;/vendor&gt;
+      &lt;feature policy='disable' name='aes'/&gt;
+      &lt;feature policy='require' name='vmx'/&gt;
+    &lt;/mode&gt;
     &lt;mode name='custom' supported='yes'&gt;
       &lt;model usable='no'&gt;Broadwell&lt;/model&gt;
       &lt;model usable='yes'&gt;Broadwell-noTSX&lt;/model&gt;
@@ -177,7 +182,19 @@
       <dd>No mode specific details are provided.</dd>
 
       <dt><code>host-model</code></dt>
-      <dd>No mode specific details are provided yet.</dd>
+      <dd>
+        If <code>host-model</code> is supported by the hypervisor, the
+        <code>mode</code> describes the guest CPU which will be used when
+        starting a domain with <code>host-model</code> CPU. The hypervisor
+        specifics (such as unsupported CPU models or features, machine type,
+        etc.) may be accounted for in this guest CPU specification and thus
+        the CPU can be different from the one shown in host capabilities XML.
+        This is indicated by the <code>fallback</code> attribute of the
+        <code>model</code> sub element: <code>allow</code> means not all
+        specifics were accounted for and thus the CPU a guest will see may
+        be different; <code>forbid</code> indicates that the CPU a guest will
+        see should match this CPU definition.
+      </dd>
 
       <dt><code>custom</code></dt>
       <dd>

docs/schemas/domaincaps.rng
@@ -2,6 +2,7 @@
 <!-- A Relax NG schema for the libvirt domain capabilities XML format -->
 <grammar xmlns="http://relaxng.org/ns/structure/1.0" datatypeLibrary="http://www.w3.org/2001/XMLSchema-datatypes">
   <include href='basictypes.rng'/>
+  <include href='cputypes.rng'/>
   <start>
     <ref name='domainCapabilities'/>
   </start>
@@ -94,6 +95,15 @@
         <value>host-model</value>
       </attribute>
       <ref name='supported'/>
+      <optional>
+        <ref name="cpuModel"/>
+        <optional>
+          <ref name="cpuVendor"/>
+        </optional>
+        <zeroOrMore>
+          <ref name="cpuFeature"/>
+        </zeroOrMore>
+      </optional>
     </element>
   </define>
 

src/conf/domain_capabilities.h
@@ -132,7 +132,7 @@ typedef struct _virDomainCapsCPU virDomainCapsCPU;
 typedef virDomainCapsCPU *virDomainCapsCPUPtr;
 struct _virDomainCapsCPU {
     bool hostPassthrough;
-    bool hostModel;
+    virCPUDefPtr hostModel;
     virDomainCapsCPUModelsPtr custom;
 };
 

src/qemu/qemu_capabilities.c
@@ -4358,9 +4358,7 @@ virQEMUCapsFillDomainCPUCaps(virCapsPtr caps,
         virQEMUCapsGuestIsNative(caps->host.arch, qemuCaps->arch))
         domCaps->cpu.hostPassthrough = true;
 
-    if (qemuCaps->cpuDefinitions && caps->host.cpu)
-        domCaps->cpu.hostModel = virQEMUCapsGuestIsNative(caps->host.arch,
-                                                          qemuCaps->arch);
+    domCaps->cpu.hostModel = virCPUDefCopy(qemuCaps->hostCPUModel);
 
     if (qemuCaps->cpuDefinitions &&
         cpuGetModels(domCaps->arch, &models) >= 0) {

tests/domaincapsschemadata/full.xml
@@ -21,7 +21,10 @@
   </os>
   <cpu>
     <mode name='host-passthrough' supported='yes'/>
-    <mode name='host-model' supported='yes'/>
+    <mode name='host-model' supported='yes'>
+      <model>host</model>
+      <vendor>CPU Vendorrr</vendor>
+    </mode>
     <mode name='custom' supported='yes'>
       <model usable='unknown'>Model1</model>
       <model usable='no'>Model2</model>

tests/domaincapsschemadata/qemu_1.7.0.x86_64.xml
@@ -20,7 +20,9 @@
   </os>
   <cpu>
     <mode name='host-passthrough' supported='yes'/>
-    <mode name='host-model' supported='yes'/>
+    <mode name='host-model' supported='yes'>
+      <model fallback='allow'>Broadwell</model>
+    </mode>
     <mode name='custom' supported='yes'>
       <model usable='unknown'>Opteron_G5</model>
       <model usable='unknown'>Opteron_G4</model>

tests/domaincapsschemadata/qemu_2.6.0-gicv2-virt.aarch64.xml
@@ -20,7 +20,7 @@
   </os>
   <cpu>
     <mode name='host-passthrough' supported='yes'/>
-    <mode name='host-model' supported='yes'/>
+    <mode name='host-model' supported='no'/>
     <mode name='custom' supported='yes'>
       <model usable='unknown'>pxa262</model>
       <model usable='unknown'>pxa270-a0</model>

commit cb23d64fcd7a42930454648b774e64ff6a6f49ef

conf/domain_capabilities: fix resource leak
Commit 14319c81a0 introduced CPU host model in domain capabilities
and the *hostmodel* variable is always filled by virCPUDefCopy()
and needs to be freed.

Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Message-Id: <f395c4f409a21a3a0c29ab0d69e8967898f4ae33.1491810803.git.phrdina@redhat.com>

src/conf/domain_capabilities.c
@@ -82,6 +82,7 @@ virDomainCapsDispose(void *obj)
     VIR_FREE(caps->path);
     VIR_FREE(caps->machine);
     virObjectUnref(caps->cpu.custom);
+    virCPUDefFree(caps->cpu.hostModel);
 
     virDomainCapsStringValuesFree(&caps->os.loader.values);
 }