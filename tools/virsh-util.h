74382440e5119ceb2a53a81d06ef4b42adbc31ba
Michal Privoznik <mprivozn@redhat.com>
2017-04-13 15:31:45 +0200

    virsh: Implement sparse stream to vol-download
    The command grew new --sparse switch that does nothing more than
    enables the sparse streams feature for this command. Among with
    the switch new helper function is introduced: virshStreamSkip().
    This is the callback that is called whenever daemon sends us a
    hole. In the callback we reflect the hole in underlying file by
    seeking as many bytes as told.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <838ab73de2c8b08dd9cef4a0d7c62639e44ff29f.1492089792.git.mprivozn@redhat.com>

--- tools/virsh-util.h
+++ tools/virsh-util.h
@@ -58,6 +58,11 @@ virshStreamSink(virStreamPtr st,
                 void *opaque);
 
 int
+virshStreamSkip(virStreamPtr st,
+                unsigned long long offset,
+                void *opaque);
+
+int
 virshDomainGetXMLFromDom(vshControl *ctl,
                          virDomainPtr dom,
                          unsigned int flags,
