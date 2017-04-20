1dabc18eb3f207eb67de86047b886fbdabfed695
Pavel Hrdina <phrdina@redhat.com>
2017-04-20 15:44:58 +0200

    qemu: use qemu-xhci USB controller by default for ppc64 and aarch64
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1438682
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Message-Id: <cff1a745d9cf32a08de0a8cc4e8e676d6d5eb909.1492695824.git.phrdina@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -3172,14 +3172,23 @@ qemuDomainControllerDefPostParse(virDomainControllerDefPtr cont,
             } else if (ARCH_IS_PPC64(def->os.arch)) {
                 /* To not break migration we need to set default USB controller
                  * for ppc64 to pci-ohci if we cannot change ABI of the VM.
-                 * The nec-usb-xhci controller is used as default only for
-                 * newly defined domains or devices. */
+                 * The nec-usb-xhci or qemu-xhci controller is used as default
+                 * only for newly defined domains or devices. */
                 if ((parseFlags & VIR_DOMAIN_DEF_PARSE_ABI_UPDATE) &&
+                    virQEMUCapsGet(qemuCaps, QEMU_CAPS_DEVICE_QEMU_XHCI)) {
+                    cont->model = VIR_DOMAIN_CONTROLLER_MODEL_USB_QEMU_XHCI;
+                } else if ((parseFlags & VIR_DOMAIN_DEF_PARSE_ABI_UPDATE) &&
                     virQEMUCapsGet(qemuCaps, QEMU_CAPS_NEC_USB_XHCI)) {
                     cont->model = VIR_DOMAIN_CONTROLLER_MODEL_USB_NEC_XHCI;
                 } else if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_PCI_OHCI)) {
                     cont->model = VIR_DOMAIN_CONTROLLER_MODEL_USB_PCI_OHCI;
                 }
+            } else if (def->os.arch == VIR_ARCH_AARCH64 &&
+                       virQEMUCapsGet(qemuCaps, QEMU_CAPS_DEVICE_QEMU_XHCI)) {
+                /* If possible use qemu-xhci as default controller for
+                 * aarch64, it's USB3 controller and we want to avoid using
+                 * nec-usb-xhci. */
+                cont->model = VIR_DOMAIN_CONTROLLER_MODEL_USB_QEMU_XHCI;
             } else {
                 /* Default USB controller for anything else is piix3-uhci */
                 if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_PIIX3_USB_UHCI))
