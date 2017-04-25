4a9653ffd5af9803a11709b6f652c0d6fd0745ba
John Ferlan <jferlan@redhat.com>
2017-04-25 18:36:55 -0400

    interface: Clean up virInterfaceObjListFindByMACString
    Alter the algorithm to return a list of matching names rather than a
    list of match virInterfaceObjPtr which are then just dereferenced
    extracting the def->name and def->mac. Since the def->mac would be
    the same as the passed @mac, just return a list of names and as long
    as there's only one, extract the [0] entry from the passed list.
    Also alter the error message on failure to include the mac that wasn't
    found.
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20170425223656.20740-8-jferlan@redhat.com>

--- src/conf/virinterfaceobj.c
+++ src/conf/virinterfaceobj.c
@@ -108,11 +108,11 @@ virInterfaceObjListNew(void)
 int
 virInterfaceObjListFindByMACString(virInterfaceObjListPtr interfaces,
                                    const char *mac,
-                                   virInterfaceObjPtr *matches,
+                                   char **const matches,
                                    int maxmatches)
 {
     size_t i;
-    unsigned int matchct = 0;
+    int matchct = 0;
 
     for (i = 0; i < interfaces->count; i++) {
         virInterfaceObjPtr obj = interfaces->objs[i];
@@ -121,18 +121,23 @@ virInterfaceObjListFindByMACString(virInterfaceObjListPtr interfaces,
         virInterfaceObjLock(obj);
         def = obj->def;
         if (STRCASEEQ(def->mac, mac)) {
-            matchct++;
-            if (matchct <= maxmatches) {
-                matches[matchct - 1] = obj;
-                /* keep the lock if we're returning object to caller */
-                /* it is the caller's responsibility to unlock *all* matches */
-                continue;
+            if (matchct < maxmatches) {
+                if (VIR_STRDUP(matches[matchct], def->name) < 0) {
+                    virInterfaceObjUnlock(obj);
+                    goto error;
+                }
+                matchct++;
             }
         }
         virInterfaceObjUnlock(obj);
-
     }
     return matchct;
+
+ error:
+    while (--matchct >= 0)
+        VIR_FREE(matches[matchct]);
+
+    return -1;
 }
 
 
