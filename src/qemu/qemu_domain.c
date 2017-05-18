7552e322146d62ccf8a508dbcab8837fd09e509e
Ján Tomko <jtomko@redhat.com>
2017-05-18 11:09:32 +0200

    qemu: format eim on intel-iommu command line
    This option turns on extended interrupt mode,
    which allows more than 255 vCPUs.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1451282
    Message-Id: <ee20861f98d9a00810f63dd2d54904aecea0e344.1495098511.git.jtomko@redhat.com>
    Reviewed-by: Andrea Bolognani <abologna@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -2663,6 +2663,9 @@ qemuDomainRecheckInternalPaths(virDomainDefPtr def,
 }
 
 
+#define QEMU_MAX_VCPUS_WITHOUT_EIM 255
+
+
 static int
 qemuDomainDefVcpusPostParse(virDomainDefPtr def)
 {
@@ -3068,6 +3071,23 @@ qemuDomainDefValidate(const virDomainDef *def,
             virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
                            _("CPU topology doesn't match maximum vcpu count"));
             goto cleanup;
+        }
+    }
+
+    if (ARCH_IS_X86(def->os.arch) &&
+        virDomainDefGetVcpusMax(def) > QEMU_MAX_VCPUS_WITHOUT_EIM) {
+        if (!qemuDomainIsQ35(def)) {
+            virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                           _("more than %d vCPUs are only supported on "
+                             "q35-based machine types"),
+                           QEMU_MAX_VCPUS_WITHOUT_EIM);
+            goto cleanup;
+        }
+        if (!def->iommu || def->iommu->eim != VIR_TRISTATE_SWITCH_ON) {
+            virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                           _("more than %d vCPUs require extended interrupt "
+                             "mode enabled on the iommu device"),
+                           QEMU_MAX_VCPUS_WITHOUT_EIM);
         }
     }
 
