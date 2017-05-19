2beaeaacf53d4d7198489e139bf1434d8f4e80b2
John Ferlan <jferlan@redhat.com>
2017-05-19 09:08:52 -0400

    nodedev: Remove privateData from virNodeDeviceObj
    It was only ever used in node_device_hal.c which really never used it
    anyway since the NODE_DEV_UDI was never referenced.  Remove free_udi()
    and @privData as well as the references to obj->privateData & obj->privateFree.
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20170519130853.13965-13-jferlan@redhat.com>

--- src/conf/virnodedeviceobj.c
+++ src/conf/virnodedeviceobj.c
@@ -226,8 +226,6 @@ virNodeDeviceObjFree(virNodeDeviceObjPtr obj)
         return;
 
     virNodeDeviceDefFree(obj->def);
-    if (obj->privateFree)
-        (*obj->privateFree)(obj->privateData);
 
     virMutexDestroy(&obj->lock);
 
