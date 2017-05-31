ca44b3a3a293bb09e44f032f0a307b95a94b8084
Claudio André <claudioandre.br@gmail.com>
2017-05-31 12:21:05 -0300

    CI: also run tests using updated distro(s)
    It is possible to test libvirt using other distros in Travis via Docker;
    including (but not limited to) Fedora and Ubuntu.
    Message-Id: <20170531152105.17869-1-claudioandre.br@gmail.com>

---
+++ tests/travis-ci.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -e
+
+function do_Install_Dependencies(){
+    echo
+    echo '-- Installing Dependencies --'
+
+    apt-get update -qq
+    apt-get -y -qq install \
+            build-essential git clang autoconf libtool libcmpicppimpl0 gettext \
+            xsltproc autopoint libxml2-dev libncurses5-dev libreadline-dev \
+            zlib1g-dev libgnutls28-dev libgcrypt11-dev libavahi-client-dev libsasl2-dev \
+            libxen-dev lvm2 libgcrypt11-dev libparted0-dev libdevmapper-dev uuid-dev \
+            libudev-dev libpciaccess-dev libcap-ng-dev libnl-3-dev libnl-route-3-dev \
+            libyajl-dev libpcap0.8-dev libnuma-dev libnetcf-dev libaudit-dev \
+            libxml2-utils libapparmor-dev dnsmasq-base librbd-dev w3c-markup-validator kmod > /dev/null
+}
+
+
+function do_Show_Info(){
+    echo
+    echo '-- Environment --'
+    echo "Running on Docker: $DISTRO"
+    id
+    uname -a
+}
+
+
+function do_Show_Compiler(){
+
+    if [[ -n $CC ]]; then
+        echo
+        echo '-- Compiler in use --'
+        "$CC" --version
+    fi
+}
+
+
+# ----------- Build and Test libvirt -----------
+
+if [[ -n $IMAGE ]]; then
+    # Run docker using the selected image; then build and test
+    docker run --privileged --cap-add=ALL -v /lib/modules:/lib/modules \
+      -v "$(pwd)":/cwd -e CC=$CCO -e DISTRO=$IMAGE "$IMAGE" sh -e -c " \
+        cd /cwd; \
+        tests/travis-ci.sh"
+    exit $?
+fi
+
+if [[ -n $DISTRO ]]; then
+    do_Show_Info
+    do_Install_Dependencies
+    do_Show_Compiler
+fi
+
+echo -en 'travis_fold:start:autogen\r'
+    echo '-- Running ./autogen.sh --'
+    # The custom PATH is just to pick up OS-X homebrew & its harmless on Linux
+    PATH="/usr/local/opt/gettext/bin:/usr/local/opt/rpcgen/bin:$PATH" ./autogen.sh
+echo -en 'travis_fold:end:autogen\r'
+
+# Build and test
+if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+    # many unit tests fail & so does syntax-check, so skip for now
+    # one day we must fix it though....
+    make -j3
+else
+    VIR_TEST_DEBUG=1 make -j3 && make -j3 syntax-check && make -j3 check
+fi
+
+exit $?
