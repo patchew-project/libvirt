8b4ec5ea7b2b09faa15f0e069dc2c7531717a7e7
Peter Krempa <pkrempa@redhat.com>
2017-07-10 14:07:36 +0200

    conf: Pre-fill default ports when parsing network disk sources
    Fill them in right away rather than having to figure out at runtime
    whether they are necessary or not.
    
    virStorageSourceNetworkDefaultPort does not need to be exported any
    more.
    Message-Id: <38ac1913170e7f80eb5a1ee2bc76d4bf7371be7b.1499688409.git.pkrempa@redhat.com>

--- src/util/virstoragefile.c
+++ src/util/virstoragefile.c
@@ -4020,7 +4020,7 @@ virStorageSourceFindByNodeName(virStorageSourcePtr top,
 }
 
 
-const char *
+static const char *
 virStorageSourceNetworkDefaultPort(virStorageNetProtocol protocol)
 {
     switch (protocol) {
@@ -4064,4 +4064,22 @@ virStorageSourceNetworkDefaultPort(virStorageNetProtocol protocol)
     }
 
     return NULL;
+}
+
+
+int
+virStorageSourceNetworkAssignDefaultPorts(virStorageSourcePtr src)
+{
+    size_t i;
+
+    for (i = 0; i < src->nhosts; i++) {
+        if (src->hosts[i].transport == VIR_STORAGE_NET_HOST_TRANS_TCP &&
+            src->hosts[i].port == NULL) {
+            if (VIR_STRDUP(src->hosts[i].port,
+                           virStorageSourceNetworkDefaultPort(src->protocol)) < 0)
+                return -1;
+        }
+    }
+
+    return 0;
 }
