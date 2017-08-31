91e248b4812a4c0267160019630579f4fd75c5cb
Ashish Mittal <Ashish.Mittal@veritas.com>
2017-08-31 07:01:51 -0400

    util: Add haveTLS to virStorageSource
    Add an optional virTristateBool haveTLS to virStorageSource to
    manage whether a storage source will be using TLS.
    
    Sample XML for a VxHS disk:
    
    <disk type='network' device='disk'>
      <driver name='qemu' type='raw' cache='none'/>
      <source protocol='vxhs' name='eb90327c-8302-4725-9e1b-4e85ed4dc251' tls='yes'>
        <host name='192.168.0.1' port='9999'/>
      </source>
      <target dev='vda' bus='virtio'/>
    </disk>
    
    Update the qemuxml2xmltest in order to add a test to show the proper
    parsing.
    
    Also update the docs to describe the tls attribute plus clean up the
    description in the surrounding area to make the information a bit more
    readable rather than one winding paragraph.
    
    Signed-off-by: Ashish Mittal <Ashish.Mittal@veritas.com>
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20170831110156.11549-9-jferlan@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -8039,6 +8039,7 @@ virDomainDiskSourceParse(xmlNodePtr node,
     int ret = -1;
     char *protocol = NULL;
     xmlNodePtr saveNode = ctxt->node;
+    char *haveTLS = NULL;
 
     ctxt->node = node;
 
@@ -8070,6 +8071,19 @@ virDomainDiskSourceParse(xmlNodePtr node,
             virReportError(VIR_ERR_XML_ERROR, "%s",
                            _("missing name for disk source"));
             goto cleanup;
+        }
+
+        /* Check tls=yes|no domain setting for the block device
+         * At present only VxHS. Other block devices may be added later */
+        if (src->protocol == VIR_STORAGE_NET_PROTOCOL_VXHS &&
+            (haveTLS = virXMLPropString(node, "tls"))) {
+            if ((src->haveTLS =
+                virTristateBoolTypeFromString(haveTLS)) <= 0) {
+                virReportError(VIR_ERR_XML_ERROR,
+                           _("unknown disk source 'tls' setting '%s'"),
+                           haveTLS);
+                goto cleanup;
+            }
         }
 
         /* for historical reasons the volume name for gluster volume is stored
@@ -8127,6 +8141,7 @@ virDomainDiskSourceParse(xmlNodePtr node,
 
  cleanup:
     VIR_FREE(protocol);
+    VIR_FREE(haveTLS);
     ctxt->node = saveNode;
     return ret;
 }
@@ -21622,6 +21637,10 @@ virDomainDiskSourceFormatNetwork(virBufferPtr buf,
     virBufferEscapeString(buf, " name='%s'", path ? path : src->path);
 
     VIR_FREE(path);
+
+    if (src->haveTLS != VIR_TRISTATE_BOOL_ABSENT)
+        virBufferAsprintf(buf, " tls='%s'",
+                          virTristateBoolTypeToString(src->haveTLS));
 
     if (src->nhosts == 0 && !src->snapshot && !src->configFile) {
         virBufferAddLit(buf, "/>\n");
