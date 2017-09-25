f440112ff8896488f8be5844efb354a55b2bb8e1
Peter Krempa <pkrempa@redhat.com>
2017-09-25 16:25:46 +0200

    qemu: process: Refresh data from qemu monitor after migration
    Some values we read from the qemu monitor may be changed with the actual
    state by the incomming migration. This means that we should refresh
    certain things only after the migration has finished.
    
    This is mostly visible in the cdrom tray state, which is by default
    closed but may be opened by the guest OS. This would be refreshed before
    qemu transferred the actual state and thus libvirt would think that the
    tray is closed.
    
    Note that this patch moves only a few obvious query commands. Other may
    be moved later after individual assesment.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1463168
    Message-Id: <ddf9aa091014a2a89a952b7847f7d514b43df18f.1506349510.git.pkrempa@redhat.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -5787,14 +5787,6 @@ qemuProcessLaunch(virConnectPtr conn,
     if (qemuProcessSetLinkStates(driver, vm, asyncJob) < 0)
         goto cleanup;
 
-    VIR_DEBUG("Fetching list of active devices");
-    if (qemuDomainUpdateDeviceList(driver, vm, asyncJob) < 0)
-        goto cleanup;
-
-    VIR_DEBUG("Updating info of memory devices");
-    if (qemuDomainUpdateMemoryDeviceInfo(driver, vm, asyncJob) < 0)
-        goto cleanup;
-
     VIR_DEBUG("Setting initial memory amount");
     if (qemuProcessSetupBalloon(driver, vm, asyncJob) < 0)
         goto cleanup;
@@ -5804,14 +5796,6 @@ qemuProcessLaunch(virConnectPtr conn,
      * and friends return the correct size in case they can't grab the job */
     if (!incoming && !snapshot &&
         qemuProcessRefreshBalloonState(driver, vm, asyncJob) < 0)
-        goto cleanup;
-
-    VIR_DEBUG("Detecting actual memory size for video device");
-    if (qemuProcessUpdateVideoRamSize(driver, vm, asyncJob) < 0)
-        goto cleanup;
-
-    VIR_DEBUG("Updating disk data");
-    if (qemuProcessRefreshDisks(driver, vm, asyncJob) < 0)
         goto cleanup;
 
     if (flags & VIR_QEMU_PROCESS_START_AUTODESTROY &&
@@ -5832,6 +5816,46 @@ qemuProcessLaunch(virConnectPtr conn,
 
 
 /**
+ * qemuProcessRefreshState:
+ * @driver: qemu driver data
+ * @vm: domain to refresh
+ * @asyncJob: async job type
+ *
+ * This function gathers calls to refresh qemu state after startup. This
+ * function is called after a deferred migration finishes so that we can update
+ * state influenced by the migration stream.
+ */
+static int
+qemuProcessRefreshState(virQEMUDriverPtr driver,
+                        virDomainObjPtr vm,
+                        qemuDomainAsyncJob asyncJob)
+{
+    int ret = -1;
+
+    VIR_DEBUG("Fetching list of active devices");
+    if (qemuDomainUpdateDeviceList(driver, vm, asyncJob) < 0)
+        goto cleanup;
+
+    VIR_DEBUG("Updating info of memory devices");
+    if (qemuDomainUpdateMemoryDeviceInfo(driver, vm, asyncJob) < 0)
+        goto cleanup;
+
+    VIR_DEBUG("Detecting actual memory size for video device");
+    if (qemuProcessUpdateVideoRamSize(driver, vm, asyncJob) < 0)
+        goto cleanup;
+
+    VIR_DEBUG("Updating disk data");
+    if (qemuProcessRefreshDisks(driver, vm, asyncJob) < 0)
+        goto cleanup;
+
+    ret = 0;
+
+ cleanup:
+    return ret;
+}
+
+
+/**
  * qemuProcessFinishStartup:
  *
  * Finish starting a new domain.
@@ -5846,6 +5870,9 @@ qemuProcessFinishStartup(virConnectPtr conn,
 {
     virQEMUDriverConfigPtr cfg = virQEMUDriverGetConfig(driver);
     int ret = -1;
+
+    if (qemuProcessRefreshState(driver, vm, asyncJob) < 0)
+        goto cleanup;
 
     if (startCPUs) {
         VIR_DEBUG("Starting domain CPUs");
