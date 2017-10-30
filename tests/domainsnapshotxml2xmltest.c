44e1e4032facf2497c316941cb21931fa2bd4fb8
Kothapally Madhu Pavan <kmp@linux.vnet.ibm.com>
2017-10-30 14:21:56 +0530

    tests: docs: Add schema and testcase for domainsnapshot
    Alter the schema of domainsnapshot to add inactive XML of a snapshot.
    As, snapshot already has active XML configuration of domain, the inactive
    XMl is embedded in <inactiveDomain> tags. Sample XML is:
    
      <domainsnapshot>
        ....
        ....
        <domain>
        </domain>
        <inactiveDomain>
          <domain>
          </domain>
        </inactiveDomain>
      </domainsnapshot>
    
    Alter the domainsnapshotxml2xmltest to validate the format.
    
    Signed-off-by: Kothapally Madhu Pavan <kmp@linux.vnet.ibm.com>
    Message-Id: <1509353516-52208-8-git-send-email-kmp@linux.vnet.ibm.com>

--- tests/domainsnapshotxml2xmltest.c
+++ tests/domainsnapshotxml2xmltest.c
@@ -205,6 +205,7 @@ mymain(void)
     DO_TEST_OUT("all_parameters", "9d37b878-a7cc-9f9a-b78f-49b3abad25a8", true);
     DO_TEST_OUT("disk_snapshot_redefine", "c7a5fdbd-edaf-9455-926a-d65c16db1809", true);
     DO_TEST_OUT("full_domain", "c7a5fdbd-edaf-9455-926a-d65c16db1809", true);
+    DO_TEST_OUT("full_domain_withinactive", "c7a5fdbd-edaf-9455-926a-d65c16db1809", true);
     DO_TEST_OUT("noparent_nodescription_noactive", NULL, false);
     DO_TEST_OUT("noparent_nodescription", NULL, true);
     DO_TEST_OUT("noparent", "9d37b878-a7cc-9f9a-b78f-49b3abad25a8", false);
