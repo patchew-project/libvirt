d3b096dc09901cdbca0e4286e17a5d6e54550b40
John Ferlan <jferlan@redhat.com>
2017-11-08 08:15:57 -0500

    qemu: Refactor qemuBuildSCSIiSCSIHostdevDrvStr slightly
    Rather than building the "file" string in qemuBuildSCSIHostdevDrvStr
    build it in the called helper.
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20171108131601.26985-5-jferlan@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -4895,15 +4895,22 @@ static char *
 qemuBuildSCSIiSCSIHostdevDrvStr(virDomainHostdevDefPtr dev)
 {
     char *source = NULL;
+    char *netsource = NULL;
     virDomainHostdevSubsysSCSIPtr scsisrc = &dev->source.subsys.u.scsi;
     virDomainHostdevSubsysSCSIiSCSIPtr iscsisrc = &scsisrc->u.iscsi;
     qemuDomainStorageSourcePrivatePtr srcPriv =
         QEMU_DOMAIN_STORAGE_SOURCE_PRIVATE(iscsisrc->src);
 
     /* Rather than pull what we think we want - use the network disk code */
-    source = qemuBuildNetworkDriveStr(iscsisrc->src, srcPriv ?
-                                      srcPriv->secinfo : NULL);
+    netsource = qemuBuildNetworkDriveStr(iscsisrc->src, srcPriv ?
+                                         srcPriv->secinfo : NULL);
+    if (!netsource)
+        goto cleanup;
+    if (virAsprintf(&source, "file=%s,if=none,format=raw", netsource) < 0)
+        goto cleanup;
 
+ cleanup:
+    VIR_FREE(netsource);
     return source;
 }
 
@@ -4956,7 +4963,7 @@ qemuBuildSCSIHostdevDrvStr(virDomainHostdevDefPtr dev)
     if (scsisrc->protocol == VIR_DOMAIN_HOSTDEV_SCSI_PROTOCOL_TYPE_ISCSI) {
         if (!(source = qemuBuildSCSIiSCSIHostdevDrvStr(dev)))
             goto error;
-        virBufferAsprintf(&buf, "file=%s,if=none,format=raw", source);
+        virBufferAsprintf(&buf, "%s", source);
     } else {
         if (!(source = qemuBuildSCSIHostHostdevDrvStr(dev)))
             goto error;
