2e649047daf3e6458fa328bb842d38274fc9e8a0
Pino Toscano <ptoscano@redhat.com>
2017-11-26 23:25:44 +0100

    conf: convert sclp/sclplm <console> as <serial>
    In case we are allowed to break the ABI of a s390/s390x guest, then
    convert the first sclp/sclplm console from <console> to <serial>, just
    like it is done on other architectures.
    
    Signed-off-by: Pino Toscano <ptoscano@redhat.com>
    Reviewed-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Bjoern Walk <bwalk@linux.vnet.ibm.com>
    Message-Id: <20171126222549.13681-24-abologna@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -3986,9 +3986,40 @@ virDomainDefPostParseMemory(virDomainDefPtr def,
 }
 
 
+static void
+virDomainChrConsoleTargetTypeToSerial(virDomainChrConsoleTargetType type,
+                                      int *targetType,
+                                      int *targetModel)
+{
+    switch (type) {
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_SCLP:
+        *targetType = VIR_DOMAIN_CHR_SERIAL_TARGET_TYPE_SCLP;
+        *targetModel = VIR_DOMAIN_CHR_SERIAL_TARGET_MODEL_SCLPCONSOLE;
+        break;
+
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_SCLPLM:
+        *targetType = VIR_DOMAIN_CHR_SERIAL_TARGET_TYPE_SCLP;
+        *targetModel = VIR_DOMAIN_CHR_SERIAL_TARGET_MODEL_SCLPLMCONSOLE;
+        break;
+
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_NONE:
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_SERIAL:
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_XEN:
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_UML:
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_VIRTIO:
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_LXC:
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_OPENVZ:
+    case VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_LAST:
+        *targetType = VIR_DOMAIN_CHR_SERIAL_TARGET_TYPE_NONE;
+        *targetModel = VIR_DOMAIN_CHR_SERIAL_TARGET_MODEL_NONE;
+        break;
+    }
+}
+
+
 static int
 virDomainDefAddConsoleCompat(virDomainDefPtr def,
-                             unsigned int parseFlags ATTRIBUTE_UNUSED)
+                             unsigned int parseFlags)
 {
     size_t i;
 
@@ -4005,6 +4036,10 @@ virDomainDefAddConsoleCompat(virDomainDefPtr def,
      *
      * We then fill def->consoles[0] with a stub just so we get sequencing
      * correct for consoles > 0
+     *
+     * sclp/sclplm consoles (in s390 and s390x guests) are converted to serial
+     * only when we can update the ABI of the guest, to avoid breaking
+     * migrations to old libvirt.
      */
 
     /* Only the first console (if there are any) can be of type serial,
@@ -4021,7 +4056,10 @@ virDomainDefAddConsoleCompat(virDomainDefPtr def,
     }
     if (def->nconsoles > 0 && def->os.type == VIR_DOMAIN_OSTYPE_HVM &&
         (def->consoles[0]->targetType == VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_SERIAL ||
-         def->consoles[0]->targetType == VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_NONE)) {
+         def->consoles[0]->targetType == VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_NONE ||
+         ((def->consoles[0]->targetType == VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_SCLP ||
+           def->consoles[0]->targetType == VIR_DOMAIN_CHR_CONSOLE_TARGET_TYPE_SCLPLM) &&
+          (parseFlags & VIR_DOMAIN_DEF_PARSE_ABI_UPDATE)))) {
 
         /* If there isn't a corresponding serial port:
          *  - create one and set, the console to be an alias for it
@@ -4041,7 +4079,9 @@ virDomainDefAddConsoleCompat(virDomainDefPtr def,
 
             /* modify it to be a serial port */
             def->serials[0]->deviceType = VIR_DOMAIN_CHR_DEVICE_TYPE_SERIAL;
-            def->serials[0]->targetType = VIR_DOMAIN_CHR_SERIAL_TARGET_TYPE_NONE;
+            virDomainChrConsoleTargetTypeToSerial(def->serials[0]->targetType,
+                                                  &def->serials[0]->targetType,
+                                                  &def->serials[0]->targetModel);
             def->serials[0]->target.port = 0;
         } else {
             /* if the console source doesn't match */
