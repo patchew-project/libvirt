9805f9e5b8ab45a1967ab9c667867a93b2be07a5
Andrea Bolognani <abologna@redhat.com>
2017-12-13 17:10:21 +0100

    util: Don't report CPU frequency for ARM hosts
    Some ARM platforms, such as the original Raspberry Pi, report the
    CPU frequency in the BogoMIPS field of /proc/cpuinfo, so libvirt
    parsed that field and returned it through its API.
    
    However, not only many more boards don't report any value there,
    but several - including ARMv8-based server hardware, and even the
    more recent Raspberry Pi 3 - use this field as originally intended:
    to report the BogoMIPS value instead of the CPU frequency.
    
    Since we have no way of detecting how the field is being used,
    it's better to report no information at all rather than something
    ludicrous like "your shiny 96-core aarch64 virtualization host's
    CPUs are running at a whopping 100 MHz".
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Message-Id: <20171213161021.28525-5-abologna@redhat.com>

--- src/util/virhostcpu.c
+++ src/util/virhostcpu.c
@@ -543,12 +543,14 @@ virHostCPUParseFrequency(FILE *cpuinfo,
     const char *prefix = NULL;
     char line[1024];
 
+    /* No sensible way to retrieve CPU frequency */
+    if (ARCH_IS_ARM(arch))
+        return 0;
+
     if (ARCH_IS_X86(arch))
         prefix = "cpu MHz";
     else if (ARCH_IS_PPC(arch))
         prefix = "clock";
-    else if (ARCH_IS_ARM(arch))
-        prefix = "BogoMIPS";
 
     if (!prefix) {
         VIR_WARN("Parser for /proc/cpuinfo needs to be adapted for your architecture");
