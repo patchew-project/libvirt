a2c2ec2311aeca0d2c767033dc25518ce1132fb1
Michal Privoznik <mprivozn@redhat.com>
2018-01-02 18:12:01 +0100

    vshCommandOpt: Allow caller avoiding assert()
    In the future, completer callbacks will receive partially parsed
    command (and thus possibly incomplete). However, we still want
    them to use command options fetching APIs we already have (e.g.
    vshCommandOpt*()) and at the same time don't report any errors
    (nor call any asserts).
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <51284359ef94b281ac6c81017bdc11f4900682d7.1514911024.git.mprivozn@redhat.com>

--- tools/vsh.c
+++ tools/vsh.c
@@ -815,8 +815,8 @@ vshCommandFree(vshCmd *cmd)
  * to the option if found, 0 with *OPT set to NULL if the name is
  * valid and the option is not required, -1 with *OPT set to NULL if
  * the option is required but not present, and assert if NAME is not
- * valid (which indicates a programming error).  No error messages are
- * issued if a value is returned.
+ * valid (which indicates a programming error) unless cmd->skipChecks
+ * is set. No error messages are issued if a value is returned.
  */
 static int
 vshCommandOpt(const vshCmd *cmd, const char *name, vshCmdOpt **opt,
@@ -829,7 +829,8 @@ vshCommandOpt(const vshCmd *cmd, const char *name, vshCmdOpt **opt,
     /* See if option is valid and/or required.  */
     *opt = NULL;
     while (valid) {
-        assert(valid->name);
+        if (!cmd->skipChecks)
+            assert(valid->name);
         if (STREQ(name, valid->name))
             break;
         valid++;
