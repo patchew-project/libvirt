e06f620b37a59fd9739ac8da9ca46eb8385e4758
Daniel P. Berrangé <berrange@redhat.com>
2018-01-25 09:38:16 +0000

    conf: expand network device callbacks to cover bandwidth updates
    Currently the QEMU driver will call directly into the network driver
    impl to modify network device bandwidth for interfaces with
    type=network. This introduces a callback system to allow us to decouple
    the QEMU driver from the network driver.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20180125093825.5834-6-berrange@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -3453,10 +3453,21 @@ typedef int
 (*virDomainNetReleaseActualDeviceImpl)(virDomainDefPtr dom,
                                        virDomainNetDefPtr iface);
 
+typedef bool
+(*virDomainNetBandwidthChangeAllowedImpl)(virDomainNetDefPtr iface,
+                                          virNetDevBandwidthPtr newBandwidth);
+
+typedef int
+(*virDomainNetBandwidthUpdateImpl)(virDomainNetDefPtr iface,
+                                   virNetDevBandwidthPtr newBandwidth);
+
+
 void
 virDomainNetSetDeviceImpl(virDomainNetAllocateActualDeviceImpl allocate,
                           virDomainNetNotifyActualDeviceImpl notify,
-                          virDomainNetReleaseActualDeviceImpl release);
+                          virDomainNetReleaseActualDeviceImpl release,
+                          virDomainNetBandwidthChangeAllowedImpl bandwidthChangeAllowed,
+                          virDomainNetBandwidthUpdateImpl bandwidthUpdate);
 
 int
 virDomainNetAllocateActualDevice(virDomainDefPtr dom,
@@ -3473,6 +3484,15 @@ virDomainNetReleaseActualDevice(virDomainDefPtr dom,
                                 virDomainNetDefPtr iface)
     ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
 
+bool
+virDomainNetBandwidthChangeAllowed(virDomainNetDefPtr iface,
+                              virNetDevBandwidthPtr newBandwidth)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
+
+int
+virDomainNetBandwidthUpdate(virDomainNetDefPtr iface,
+                            virNetDevBandwidthPtr newBandwidth)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
 
 
 #endif /* __DOMAIN_CONF_H */
