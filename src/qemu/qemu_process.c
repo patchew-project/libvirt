d7937ef511e7639e6802a66e87b0866348b64e2a
Daniel P. Berrangé <berrange@redhat.com>
2018-02-05 15:28:19 +0000

    conf: introduce callback registration for domain net device allocation
    Currently virt drivers will call directly into the network driver impl
    to allocate domain interface devices where type=network. This introduces
    a callback system to allow us to decouple the virt drivers from the
    network driver.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20180205152829.12577-6-berrange@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -2914,7 +2914,7 @@ qemuProcessNotifyNets(virDomainDefPtr def)
         if (virDomainNetGetActualType(net) == VIR_DOMAIN_NET_TYPE_DIRECT)
            ignore_value(virNetDevMacVLanReserveName(net->ifname, false));
 
-        networkNotifyActualDevice(def, net);
+        virDomainNetNotifyActualDevice(def, net);
     }
 }
 
@@ -5010,7 +5010,7 @@ qemuProcessNetworkPrepareDevices(virDomainDefPtr def)
          * network's pool of devices, or resolve bridge device name
          * to the one defined in the network definition.
          */
-        if (networkAllocateActualDevice(def, net) < 0)
+        if (virDomainNetAllocateActualDevice(def, net) < 0)
             goto cleanup;
 
         actualType = virDomainNetGetActualType(net);
@@ -6597,7 +6597,7 @@ void qemuProcessStop(virQEMUDriverPtr driver,
 
         /* kick the device out of the hostdev list too */
         virDomainNetRemoveHostdev(def, net);
-        networkReleaseActualDevice(vm->def, net);
+        virDomainNetReleaseActualDevice(vm->def, net);
     }
 
  retry:
