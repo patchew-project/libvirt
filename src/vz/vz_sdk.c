patchew-project/libvirt

commit 5529b057e79dcca11ea09149c8ba83e589d65cf0

cfg: forbid includes of headers in network and storage drivers again
Loadable drivers must never depend on each other. Over time some usage
mistakenly crept in for the storage and network drivers, but now this is
eliminated the syntax-check rules can enforce this separation once more.

Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/vz/vz_sdk.c
@@ -33,7 +33,6 @@
 #include "virhostcpu.h"
 #include "virsocketaddr.h"
 
-#include "storage/storage_driver.h"
 #include "vz_sdk.h"
 
 #define VIR_FROM_THIS VIR_FROM_PARALLELS

cfg.mk
@@ -769,7 +769,7 @@ sc_prohibit_gettext_markup:
 # lower-level code must not include higher-level headers.
 cross_dirs=$(patsubst $(srcdir)/src/%.,%,$(wildcard $(srcdir)/src/*/.))
 cross_dirs_re=($(subst / ,/|,$(cross_dirs)))
-mid_dirs=access|admin|conf|cpu|locking|logging|network|node_device|rpc|security|storage
+mid_dirs=access|admin|conf|cpu|locking|logging|rpc|security
 sc_prohibit_cross_inclusion:
 	@for dir in $(cross_dirs); do \
 	  case $$dir in \

src/qemu/qemu_command.c
@@ -51,7 +51,6 @@
 #include "snapshot_conf.h"
 #include "storage_conf.h"
 #include "secret_conf.h"
-#include "network/bridge_driver.h"
 #include "virnetdevtap.h"
 #include "virnetdevopenvswitch.h"
 #include "device_conf.h"

src/qemu/qemu_domain.c
@@ -55,8 +55,6 @@
 #include "logging/log_manager.h"
 #include "locking/domain_lock.h"
 
-#include "storage/storage_driver.h"
-
 #ifdef MAJOR_IN_MKDEV
 # include <sys/mkdev.h>
 #elif MAJOR_IN_SYSMACROS

src/qemu/qemu_migration.c
@@ -58,7 +58,6 @@
 #include "virtypedparam.h"
 #include "virprocess.h"
 #include "nwfilter_conf.h"
-#include "storage/storage_driver.h"
 
 #define VIR_FROM_THIS VIR_FROM_QEMU
 

commit 6958293137c954245bf2f2e7d3d1c2bb2094efba

vz: build fix after excluding cross driver headers
Build is broken by 5529b057 [1].

[1] cfg: forbid includes of headers in network and storage drivers again

Signed-off-by: Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
Message-Id: <1519637151-828210-2-git-send-email-nshirokovskiy@virtuozzo.com>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

src/vz/vz_sdk.c
@@ -29,6 +29,7 @@
 #include "virlog.h"
 #include "datatypes.h"
 #include "domain_conf.h"
+#include "storage_conf.h"
 #include "virtime.h"
 #include "virhostcpu.h"
 #include "virsocketaddr.h"