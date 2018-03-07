25b503e54c5fc66eb42b1bbffd4129e0b20c2cf9
Peter Krempa <pkrempa@redhat.com>
2018-03-07 15:42:36 +0100

    tests: qemuxml2xml: Add status XML with outgoing migration with NBD
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <fd965300275a65393aa13d6e89be853eba2df233.1520433584.git.pkrempa@redhat.com>

--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -1224,6 +1224,7 @@ mymain(void)
     DO_TEST_STATUS("blockjob-mirror");
     DO_TEST_STATUS("vcpus-multi");
     DO_TEST_STATUS("modern");
+    DO_TEST_STATUS("migration-out-nbd");
 
     if (getenv("LIBVIRT_SKIP_CLEANUP") == NULL)
         virFileDeleteTree(fakerootdir);
