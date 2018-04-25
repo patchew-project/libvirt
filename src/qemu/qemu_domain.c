6e11d86c7cb1e4271c6980341c17d3ee47bf2879
Peter Krempa <pkrempa@redhat.com>
2018-04-25 17:15:20 +0200

    qemu: domain: Store whether a virStorageSource is a host CDROM drive
    Use virFileIsCdrom to detect whether a block device is a cdrom drive and
    store it in virStorageSource. This will be necessary to correctly create
    the 'host_cdrom' backend in qemu when using -blockdev.
    
    We assume that host_cdrom makes only sense when used directly as a raw
    image, but if a backing chain would be put in front of it, libvirt will
    use 'host_device' in that case.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <2de2db4e9795c5d51049c093d925b65de5ca6c2b.1524668969.git.pkrempa@redhat.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -7545,6 +7545,14 @@ qemuDomainDetermineDiskChain(virQEMUDriverPtr driver,
             VIR_ALLOC(src->backingStore) < 0)
             goto cleanup;
 
+        /* host cdrom requires special treatment in qemu, so we need to check
+         * whether a block device is a cdrom */
+        if (disk->device == VIR_DOMAIN_DISK_DEVICE_CDROM &&
+            src->format == VIR_STORAGE_FILE_RAW &&
+            virStorageSourceIsBlockLocal(src) &&
+            virFileIsCdrom(src->path) == 1)
+            src->hostcdrom = true;
+
         ret = 0;
         goto cleanup;
     }
