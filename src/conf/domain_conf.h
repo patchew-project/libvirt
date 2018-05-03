75e475ec89fe198577b91b239d5b4d4349eb329e
John Ferlan <jferlan@redhat.com>
2018-05-03 19:37:42 -0400

    conf: Add VM Generation ID parse/format support
    The VM Generation ID is a mechanism to provide a unique 128-bit,
    cryptographically random, and integer value identifier known as
    the GUID (Globally Unique Identifier) to the guest OS. The value
    is used to help notify the guest operating system when the virtual
    machine is executed with a different configuration.
    
    This patch adds support for a new "genid" XML element similar to
    the "uuid" element. The "genid" element can have two forms "<genid/>"
    or "<genid>$GUID</genid>". If the $GUID is not provided, libvirt
    will generate one and save it in the XML.
    
    Since adding support for a generated GUID (or UUID like) value to
    be displayed modifying the xml2xml test to include virrandommock.so
    is necessary since it will generate a "known" value.
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20180503233747.17505-2-jferlan@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -2323,6 +2323,11 @@ struct _virDomainDef {
     virDomainVirtType virtType;
     int id;
     unsigned char uuid[VIR_UUID_BUFLEN];
+
+    unsigned char genid[VIR_UUID_BUFLEN];
+    bool genidRequested;
+    bool genidGenerated;
+
     char *name;
     char *title;
     char *description;
