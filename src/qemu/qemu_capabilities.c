b1d18e8afa5ce5f144efbea2636cbb79cf9b0ade
John Ferlan <jferlan@redhat.com>
2018-05-11 07:17:49 -0400

    qemu: Add VM Generation ID device capability
    Add the query of the device objects for the vmgenid device
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20180511111753.9504-3-jferlan@redhat.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -476,6 +476,9 @@ VIR_ENUM_IMPL(virQEMUCaps, QEMU_CAPS_LAST,
               "nbd-tls",
               "tpm-crb",
               "pr-manager-helper",
+
+              /* 295 */
+              "vmgenid",
     );
 
 
@@ -1103,6 +1106,7 @@ struct virQEMUCapsStringFlags virQEMUCapsObjectTypes[] = {
     { "virtio-tablet-ccw", QEMU_CAPS_DEVICE_VIRTIO_TABLET_CCW },
     { "pcie-pci-bridge", QEMU_CAPS_DEVICE_PCIE_PCI_BRIDGE },
     { "pr-manager-helper", QEMU_CAPS_PR_MANAGER_HELPER },
+    { "vmgenid", QEMU_CAPS_DEVICE_VMGENID },
 };
 
 static struct virQEMUCapsStringFlags virQEMUCapsDevicePropsVirtioBalloon[] = {
--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -461,6 +461,9 @@ typedef enum { /* virQEMUCapsFlags grouping marker for syntax-check */
     QEMU_CAPS_DEVICE_TPM_CRB, /* -device tpm-crb */
     QEMU_CAPS_PR_MANAGER_HELPER, /* -object pr-manager-helper */
 
+    /* 295 */
+    QEMU_CAPS_DEVICE_VMGENID, /* -device vmgenid */
+
     QEMU_CAPS_LAST /* this must always be the last item */
 } virQEMUCapsFlags;
 
