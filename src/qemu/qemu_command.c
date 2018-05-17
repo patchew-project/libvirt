06ed56e75181cc8f28f01fea87691d99e00b7638
John Ferlan <jferlan@redhat.com>
2018-05-17 08:42:45 -0400

    qemu: Add VM Generation ID to qemu command line
    https://bugzilla.redhat.com/show_bug.cgi?id=1149445
    
    If the domain requests usage of the genid functionality,
    then add the QEMU '-device vmgenid' to the command line
    providing either the supplied or generated GUID value.
    
    Add tests for both a generated and supplied GUID value.
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20180517124247.10149-5-jferlan@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -6003,6 +6003,27 @@ qemuBuildSmbiosCommandLine(virCommandPtr cmd,
 
 
 static int
+qemuBuildVMGenIDCommandLine(virCommandPtr cmd,
+                            const virDomainDef *def)
+{
+    virBuffer opts = VIR_BUFFER_INITIALIZER;
+    char guid[VIR_UUID_STRING_BUFLEN];
+
+    if (!def->genidRequested)
+        return 0;
+
+    virUUIDFormat(def->genid, guid);
+    virBufferAsprintf(&opts, "vmgenid,guid=%s,id=vmgenid0", guid);
+
+    virCommandAddArg(cmd, "-device");
+    virCommandAddArgBuffer(cmd, &opts);
+
+    virBufferFreeAndReset(&opts);
+    return 0;
+}
+
+
+static int
 qemuBuildSgaCommandLine(virCommandPtr cmd,
                         const virDomainDef *def,
                         virQEMUCapsPtr qemuCaps)
@@ -9993,6 +10014,9 @@ qemuBuildCommandLine(virQEMUDriverPtr driver,
     virCommandAddArgList(cmd, "-uuid", uuid, NULL);
 
     if (qemuBuildSmbiosCommandLine(cmd, driver, def) < 0)
+        goto error;
+
+    if (qemuBuildVMGenIDCommandLine(cmd, def) < 0)
         goto error;
 
     /*
