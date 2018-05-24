16fcc8ca9240db07797a747642690efb73bddd01
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-05-24 16:25:57 -0400

    conf: Add support for external swtpm TPM emulator to domain XML
    This patch adds support for an external swtpm TPM emulator. The XML for
    this type of TPM looks as follows:
    
     <tpm model='tpm-tis'>
       <backend type='emulator'/>
     </tpm>
    
    The XML will currently only define a TPM 1.2.
    
    Extend the documentation.
    
    Add a test case testing the XML parser and formatter.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>
    Message-Id: <20180524202614.3180183-2-stefanb@linux.vnet.ibm.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -9495,6 +9495,7 @@ qemuBuildTPMBackendStr(const virDomainDef *def,
         VIR_FREE(cancel_path);
 
         break;
+    case VIR_DOMAIN_TPM_TYPE_EMULATOR:
     case VIR_DOMAIN_TPM_TYPE_LAST:
         goto error;
     }
