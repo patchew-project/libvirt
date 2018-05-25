1499425029848a1a09bde0e7ba437d5cfc28219c
Roland Schulz <schullzroll@gmail.com>
2018-05-25 09:12:37 +0200

    tools: virsh: Add virshCellnoCompleter.
    Signed-off-by: Roland Schulz <schullzroll@gmail.com>
    Message-Id: <20180525071237.13570-1-schullzroll@gmail.com>

--- tools/virsh-completer.c
+++ tools/virsh-completer.c
@@ -756,3 +756,62 @@ virshNodedevEventNameCompleter(vshControl *ctl ATTRIBUTE_UNUSED,
     virStringListFree(ret);
     return NULL;
 }
+
+
+char **
+virshCellnoCompleter(vshControl *ctl,
+                     const vshCmd *cmd ATTRIBUTE_UNUSED,
+                     unsigned int flags)
+{
+    xmlXPathContextPtr ctxt = NULL;
+    virshControlPtr priv = ctl->privData;
+    unsigned int ncells = 0;
+    xmlNodePtr *cells = NULL;
+    xmlDocPtr doc = NULL;
+    size_t i = 0;
+    char *cap_xml = NULL;
+    char *cellid = NULL;
+    char **ret = NULL;
+
+    virCheckFlags(0, NULL);
+
+    if (!priv->conn || virConnectIsAlive(priv->conn) <= 0)
+        goto error;
+
+    if (!(cap_xml = virConnectGetCapabilities(priv->conn)))
+        goto error;
+
+    if (!(doc = virXMLParseStringCtxt(cap_xml, _("capabilities"), &ctxt)))
+        goto error;
+
+    ncells = virXPathNodeSet("/capabilities/host/topology/cells/cell", ctxt, &cells);
+    if (ncells <= 0)
+        goto error;
+
+    if (VIR_ALLOC_N(ret, ncells + 1))
+        goto error;
+
+    for (i = 0; i < ncells; i++) {
+        VIR_FREE(cellid);
+        cellid = virXMLPropString(cells[i], "id");
+        if (VIR_STRDUP(ret[i], cellid) < 0)
+            goto error;
+    }
+
+ cleanup:
+    xmlXPathFreeContext(ctxt);
+    VIR_FREE(cells);
+    xmlFreeDoc(doc);
+    VIR_FREE(cap_xml);
+    VIR_FREE(cellid);
+
+    return ret;
+
+ error:
+    if (ret) {
+        for (i = 0; i < ncells; i++)
+            VIR_FREE(ret[i]);
+    }
+    VIR_FREE(ret);
+    goto cleanup;
+}
