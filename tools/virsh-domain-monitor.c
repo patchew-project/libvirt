c88e239a05b6459b2d17d09a6208f51117320fcf
Tomáš Golembiovský <tgolembi@redhat.com>
2018-06-08 14:23:12 +0200

    qemu: add entry for balloon stat stat-disk-caches
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>
    Message-Id: <053baf25c5ef6b4f3d1638a0827442cae6dd993c.1528460572.git.tgolembi@redhat.com>

--- tools/virsh-domain-monitor.c
+++ tools/virsh-domain-monitor.c
@@ -362,6 +362,8 @@ cmdDomMemStat(vshControl *ctl, const vshCmd *cmd)
             vshPrint(ctl, "actual %llu\n", stats[i].val);
         if (stats[i].tag == VIR_DOMAIN_MEMORY_STAT_RSS)
             vshPrint(ctl, "rss %llu\n", stats[i].val);
+        if (stats[i].tag == VIR_DOMAIN_MEMORY_STAT_DISK_CACHES)
+            vshPrint(ctl, "disk_caches %llu\n", stats[i].val);
         if (stats[i].tag == VIR_DOMAIN_MEMORY_STAT_LAST_UPDATE)
             vshPrint(ctl, "last_update %llu\n", stats[i].val);
     }
