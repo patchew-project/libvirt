4633360cbd0a6610f038508ff541cac80ecb8b8d
Erik Skultety <eskultet@redhat.com>
2018-06-28 14:14:59 +0200

    conf: Introduce virDomainVideoDefClear helper
    Future patches rely on the ability to reset the contents of the
    virDomainVideoDef structure rather than re-allocating it.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>
    Message-Id: <eb61913ced759e070501630864dc7c601579740d.1530187618.git.eskultet@redhat.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -2537,7 +2537,8 @@ virDomainVideoDefNew(void)
 }
 
 
-void virDomainVideoDefFree(virDomainVideoDefPtr def)
+void
+virDomainVideoDefClear(virDomainVideoDefPtr def)
 {
     if (!def)
         return;
@@ -2547,6 +2548,17 @@ void virDomainVideoDefFree(virDomainVideoDefPtr def)
     VIR_FREE(def->accel);
     VIR_FREE(def->virtio);
     VIR_FREE(def->driver);
+
+    memset(def, 0, sizeof(*def));
+}
+
+
+void virDomainVideoDefFree(virDomainVideoDefPtr def)
+{
+    if (!def)
+        return;
+
+    virDomainVideoDefClear(def);
     VIR_FREE(def);
 }
 
