ba9ccdb0b8ad15b2bd3f20e56e810a35ef9185d0
Michal Privoznik <mprivozn@redhat.com>
2018-07-04 11:23:43 +0200

    viriscsitest: Introduce testIscsiadmCbData struct
    Some tests will want to pass their own callback data into the
    testIscsiadmCbData callback. Introduce testIscsiadmCbData struct
    to give this some form and order.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <724635c2ce2fa648acd8eb138d249c77f06df7c5.1530695759.git.mprivozn@redhat.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- tests/viriscsitest.c
+++ tests/viriscsitest.c
@@ -60,6 +60,10 @@ const char *iscsiadmSendtargetsOutput =
     "10.20.30.40:3260,1 iqn.2008-04.example:example1:iscsi.bar\n"
     "10.20.30.40:3260,1 iqn.2009-04.example:example1:iscsi.seven\n";
 
+struct testIscsiadmCbData {
+    bool output_version;
+};
+
 static void testIscsiadmCb(const char *const*args,
                            const char *const*env ATTRIBUTE_UNUSED,
                            const char *input ATTRIBUTE_UNUSED,
@@ -68,12 +72,13 @@ static void testIscsiadmCb(const char *const*args,
                            int *status,
                            void *opaque)
 {
-    int *output_version = opaque;
+    struct testIscsiadmCbData *data = opaque;
+
     if (args[0] && STREQ(args[0], ISCSIADM) &&
         args[1] && STREQ(args[1], "--mode") &&
         args[2] && STREQ(args[2], "session") &&
         args[3] == NULL) {
-        if (*output_version == 1)
+        if (data->output_version)
             ignore_value(VIR_STRDUP(*output, iscsiadmSessionOutputNonFlash));
         else
             ignore_value(VIR_STRDUP(*output, iscsiadmSessionOutput));
@@ -113,11 +118,13 @@ static int
 testISCSIGetSession(const void *data)
 {
     const struct testSessionInfo *info = data;
-    int ver = info->output_version;
+    struct testIscsiadmCbData cbData = { 0 };
     char *actual_session = NULL;
     int ret = -1;
 
-    virCommandSetDryRun(NULL, testIscsiadmCb, &ver);
+    cbData.output_version = info->output_version;
+
+    virCommandSetDryRun(NULL, testIscsiadmCb, &cbData);
 
     actual_session = virISCSIGetSession(info->device_path, true);
 
