d006ea4a2cf62bafeed128da978b191d9dbd6af2
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2018-07-07 22:06:54 -0300

    esx_driver: Use VIR_DRV_CONN_CHECK_SERVER macro
    ESX needs a server to connect to, so use this macro in order to reuse
    code.
    
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>
    Message-Id: <20180708010656.9047-3-marcos.souza.org@gmail.com>

--- src/esx/esx_driver.c
+++ src/esx/esx_driver.c
@@ -854,12 +854,7 @@ esxConnectOpen(virConnectPtr conn, virConnectAuthPtr auth,
                  conn->uri->path, conn->uri->scheme);
     }
 
-    /* Require server part */
-    if (!conn->uri->server) {
-        virReportError(VIR_ERR_INVALID_ARG, "%s",
-                       _("URI is missing the server part"));
-        return VIR_DRV_OPEN_ERROR;
-    }
+    VIR_DRV_CONN_CHECK_SERVER;
 
     /* Require auth */
     if (!auth || !auth->cb) {
