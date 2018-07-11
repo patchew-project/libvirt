f02772b9300b4bc4dddcfe9fc80ea667533bad06
Pavel Hrdina <phrdina@redhat.com>
2018-07-11 10:22:50 +0200

    tests: Add additional hugepage tests to cover current state
    Both of these cases were working before commit
    <fa6bdf6afa878b8d7c5ed71664ee72be8967cdc5> tried to fix the case were
    there is no guest NUMA configured but there is "nodeset='0'" attribute.
    The case where "nodeset='1'" without any guest NUMA topology is covered
    by "hugepage-pages8" test case.
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Message-Id: <bb4f5fcb059c30a49e70d3646ef645ef96cf2fc7.1531297333.git.phrdina@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -978,6 +978,8 @@ mymain(void)
     DO_TEST_FAILURE("hugepages-pages8",
                     QEMU_CAPS_DEVICE_PC_DIMM, QEMU_CAPS_OBJECT_MEMORY_FILE,
                     QEMU_CAPS_OBJECT_MEMORY_FILE_DISCARD);
+    DO_TEST_FAILURE("hugepages-pages9", NONE);
+    DO_TEST_FAILURE("hugepages-pages10", NONE);
     DO_TEST("hugepages-memaccess", QEMU_CAPS_OBJECT_MEMORY_FILE,
             QEMU_CAPS_OBJECT_MEMORY_RAM, QEMU_CAPS_DEVICE_PC_DIMM,
             QEMU_CAPS_NUMA);
--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -338,6 +338,8 @@ mymain(void)
     DO_TEST("hugepages-pages5", NONE);
     DO_TEST("hugepages-pages6", NONE);
     DO_TEST("hugepages-pages7", NONE);
+    DO_TEST("hugepages-pages9", NONE);
+    DO_TEST("hugepages-pages10", NONE);
     DO_TEST("hugepages-shared", NONE);
     DO_TEST("hugepages-memaccess", NONE);
     DO_TEST("hugepages-memaccess2", NONE);
