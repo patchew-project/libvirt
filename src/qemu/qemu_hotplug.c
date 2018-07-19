patchew-project/libvirt

commit 105bcdde76bc8c64f2d9aca9db684186a5e96e63

qemu: hotplug: Fix detach of disk with managed persistent reservations
In commit 8bebb2b735d I've refactored how the detach of disk with a
managed persistent reservations object is handled. After the commit if
any disk with a managed PR object would be removed libvirt would also
attempt to remove the shared 'pr-manager-helper' object potentially used
by other disks.

Thankfully this should not have practical impact as qemu should reject
deletion of the object if it was still used and the rest of the code is
correct.

Fix this by removing the disk from the definition earlier and checking
if the shared/managed pr-manager-helper object is still needed.

This basically splits the detach code for the managed PR object from the
unmanaged ones. The same separation will follow for the attachment code
as well as it greatly simplifies -blockdev support for this.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>

src/qemu/qemu_hotplug.c
@@ -3839,6 +3839,8 @@ qemuDomainRemoveDiskDevice(virQEMUDriverPtr driver,
     char *drivestr;
     char *objAlias = NULL;
     char *encAlias = NULL;
+    bool prManaged = priv->prDaemonRunning;
+    bool prUsed = false;
 
     VIR_DEBUG("Removing disk %s from domain %p %s",
               disk->info.alias, vm, vm->def->name);
@@ -3876,6 +3878,16 @@ qemuDomainRemoveDiskDevice(virQEMUDriverPtr driver,
         }
     }
 
+    for (i = 0; i < vm->def->ndisks; i++) {
+        if (vm->def->disks[i] == disk) {
+            virDomainDiskRemove(vm->def, i);
+            break;
+        }
+    }
+
+    /* check if the last disk with managed PR was just removed */
+    prUsed = virDomainDefHasManagedPR(vm->def);
+
     qemuDomainObjEnterMonitor(driver, vm);
 
     qemuMonitorDriveDel(priv->mon, drivestr);
@@ -3892,11 +3904,15 @@ qemuDomainRemoveDiskDevice(virQEMUDriverPtr driver,
     VIR_FREE(encAlias);
 
     /* If it fails, then so be it - it was a best shot */
-    if (disk->src->pr)
+    if (disk->src->pr &&
+        !virStoragePRDefIsManaged(disk->src->pr))
         ignore_value(qemuMonitorDelObject(priv->mon, disk->src->pr->mgralias));
 
     if (disk->src->haveTLS)
         ignore_value(qemuMonitorDelObject(priv->mon, disk->src->tlsAlias));
+
+    if (prManaged && !prUsed)
+        ignore_value(qemuMonitorDelObject(priv->mon, qemuDomainGetManagedPRAlias()));
 
     if (qemuDomainObjExitMonitor(driver, vm) < 0)
         return -1;
@@ -3906,16 +3922,7 @@ qemuDomainRemoveDiskDevice(virQEMUDriverPtr driver,
     event = virDomainEventDeviceRemovedNewFromObj(vm, disk->info.alias);
     qemuDomainEventQueue(driver, event);
 
-    for (i = 0; i < vm->def->ndisks; i++) {
-        if (vm->def->disks[i] == disk) {
-            virDomainDiskRemove(vm->def, i);
-            break;
-        }
-    }
-
-    /* check if the last disk with managed PR was just removed */
-    if (priv->prDaemonRunning &&
-        !virDomainDefHasManagedPR(vm->def))
+    if (prManaged && !prUsed)
         qemuProcessKillManagedPRDaemon(vm);
 
     qemuDomainReleaseDeviceAddress(vm, &disk->info, src);

commit 93710410cd16475b4b6b3b95ed26c686ce0d3540

qemu: hotplug: Don't leak 'disk' if VM crashes during unplug finishing
qemuDomainRemoveDiskDevice would leak the disk to be removed if the VM
crashed since it was removed from the definition but not freed.

Broken in commit 105bcdde76b which moved the removal from the definition
earlier.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Message-Id: <10962a1bd06aed83157f6b612a3d98fa1b7a806f.1531829580.git.pkrempa@redhat.com>
Reviewed-by: J�n Tomko <jtomko@redhat.com>

src/qemu/qemu_hotplug.c
@@ -3864,6 +3864,7 @@ qemuDomainRemoveDiskDevice(virQEMUDriverPtr driver,
     bool prUsed = false;
     const char *authAlias = NULL;
     const char *encAlias = NULL;
+    int ret = -1;
 
     VIR_DEBUG("Removing disk %s from domain %p %s",
               disk->info.alias, vm, vm->def->name);
@@ -3918,7 +3919,7 @@ qemuDomainRemoveDiskDevice(virQEMUDriverPtr driver,
         ignore_value(qemuMonitorDelObject(priv->mon, qemuDomainGetManagedPRAlias()));
 
     if (qemuDomainObjExitMonitor(driver, vm) < 0)
-        return -1;
+        goto cleanup;
 
     virDomainAuditDisk(vm, disk->src, NULL, "detach", true);
 
@@ -3938,8 +3939,11 @@ qemuDomainRemoveDiskDevice(virQEMUDriverPtr driver,
     ignore_value(qemuRemoveSharedDevice(driver, &dev, vm->def->name));
     virDomainUSBAddressRelease(priv->usbaddrs, &disk->info);
 
+    ret = 0;
+
+ cleanup:
     virDomainDiskDefFree(disk);
-    return 0;
+    return ret;
 }
 
 