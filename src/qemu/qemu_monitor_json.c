c3c05ebb88f4bc3b7650bb5418bad11705ce86e9
Peter Krempa <pkrempa@redhat.com>
2018-08-15 13:52:57 +0200

    qemu: monitor: Use qemuMonitorJSONBlockJobError in qemuMonitorJSONDrivePivot
    The API deals with a block job so use the common error reporting
    function for it.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <bb0ee6c5e0ec2a6727bfa7971ec82aaea529f50f.1534333853.git.pkrempa@redhat.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- src/qemu/qemu_monitor_json.c
+++ src/qemu/qemu_monitor_json.c
@@ -4806,7 +4806,7 @@ qemuMonitorJSONBlockStream(qemuMonitorPtr mon,
     if (qemuMonitorJSONCommand(mon, cmd, &reply) < 0)
         goto cleanup;
 
-    if (qemuMonitorJSONCheckError(cmd, reply) < 0)
+    if (qemuMonitorJSONBlockJobError(cmd, reply, device) < 0)
         goto cleanup;
 
     ret = 0;
