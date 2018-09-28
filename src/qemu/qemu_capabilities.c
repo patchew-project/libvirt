010afc168b35d27dc5415d6a741a245ad2ad4081
Yi Min Zhao <zyimin@linux.ibm.com>
2018-09-28 16:46:17 +0800

    qemu: Enable PCI multi bus for S390 guests
    QEMU on s390 supports PCI multibus since forever.
    
    Signed-off-by: Yi Min Zhao <zyimin@linux.ibm.com>
    Reviewed-by: Boris Fiuczynski <fiuczy@linux.vnet.ibm.com>
    Reviewed-by: Stefan Zimmermann <stzi@linux.ibm.com>
    Reviewed-by: Bjoern Walk <bwalk@linux.vnet.ibm.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Andrea Bolognani <abologna@redhat.com>
    Message-Id: <20180928084626.76279-5-zyimin@linux.ibm.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -1700,6 +1700,10 @@ bool virQEMUCapsHasPCIMultiBus(virQEMUCapsPtr qemuCaps,
         return false;
     }
 
+    /* S390 supports PCI-multibus. */
+    if (ARCH_IS_S390(def->os.arch))
+        return true;
+
     /* If ARM 'virt' supports PCI, it supports multibus.
      * No extra conditions here for simplicity.
      */
