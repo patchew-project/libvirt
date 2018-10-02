99ec31d043519593aba8c9770ce4e1b78af540a8
Pavel Hrdina <phrdina@redhat.com>
2018-10-02 10:43:56 +0200

    vircgroup: introduce virCgroupV2GetAnyController
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Message-Id: <dfba1767870436691e2b43d7935cfadd6b334bfc.1538469654.git.phrdina@redhat.com>

--- src/util/vircgroupv2.c
+++ src/util/vircgroupv2.c
@@ -306,6 +306,13 @@ virCgroupV2HasController(virCgroupPtr group,
 }
 
 
+static int
+virCgroupV2GetAnyController(virCgroupPtr group)
+{
+    return ffs(group->unified.controllers) - 1;
+}
+
+
 virCgroupBackend virCgroupV2Backend = {
     .type = VIR_CGROUP_BACKEND_TYPE_V2,
 
@@ -319,6 +326,7 @@ virCgroupBackend virCgroupV2Backend = {
     .stealPlacement = virCgroupV2StealPlacement,
     .detectControllers = virCgroupV2DetectControllers,
     .hasController = virCgroupV2HasController,
+    .getAnyController = virCgroupV2GetAnyController,
 };
 
 
