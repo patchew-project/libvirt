e784b262a9ecfdd66b6e90693847271c27348c96
Eric Blake <eblake@redhat.com>
2018-10-12 00:10:04 -0500

    domain_conf: Expose virDomainStorageNetworkParseHost
    An upcoming patch wants to reuse XML parsing of both unix and tcp
    network host descriptions in the context of setting up a backup
    NBD server. Make that easier by refactoring the existing parser.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Message-Id: <20181012051011.1398546-3-eblake@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -3143,6 +3143,8 @@ int virDomainDiskInsert(virDomainDefPtr def,
     ATTRIBUTE_RETURN_CHECK;
 void virDomainDiskInsertPreAlloced(virDomainDefPtr def,
                                    virDomainDiskDefPtr disk);
+int virDomainStorageNetworkParseHost(xmlNodePtr hostnode,
+                                     virStorageNetHostDefPtr host);
 int virDomainDiskDefAssignAddress(virDomainXMLOptionPtr xmlopt,
                                   virDomainDiskDefPtr def,
                                   const virDomainDef *vmdef);
