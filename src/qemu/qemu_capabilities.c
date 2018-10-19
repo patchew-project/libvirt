1c3c5e4cd2bbedec07995d80b5dfd48e92ee2ded
Yi Min Zhao <zyimin@linux.ibm.com>
2018-10-19 11:40:08 +0800

    qemu: Introduce zPCI capability
    Let's introduce zPCI capability.
    
    Signed-off-by: Yi Min Zhao <zyimin@linux.ibm.com>
    Reviewed-by: Boris Fiuczynski <fiuczy@linux.ibm.com>
    Reviewed-by: Stefan Zimmermann <stzi@linux.ibm.com>
    Reviewed-by: Bjoern Walk <bwalk@linux.ibm.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Andrea Bolognani <abologna@redhat.com>
    Message-Id: <20181019034020.4757-3-zyimin@linux.ibm.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -508,6 +508,7 @@ VIR_ENUM_IMPL(virQEMUCaps, QEMU_CAPS_LAST,
               /* 315 */
               "vfio-pci.display",
               "blockdev",
+              "zpci",
     );
 
 
@@ -1092,6 +1093,7 @@ struct virQEMUCapsStringFlags virQEMUCapsObjectTypes[] = {
     { "vhost-vsock-device", QEMU_CAPS_DEVICE_VHOST_VSOCK },
     { "mch", QEMU_CAPS_DEVICE_MCH },
     { "sev-guest", QEMU_CAPS_SEV_GUEST },
+    { "zpci", QEMU_CAPS_DEVICE_ZPCI },
 };
 
 static struct virQEMUCapsStringFlags virQEMUCapsDevicePropsVirtioBalloon[] = {
