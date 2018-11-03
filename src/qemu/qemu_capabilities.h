cb89dd050defeef831cf76305cabfdf9fad32811
Chris Venteicher <cventeic@redhat.com>
2018-11-02 22:13:29 -0500

    qemu_capabilities: Introduce CPUModelInfo to virCPUDef function
    Move existing code to convert between cpu model info structures
    (qemuMonitorCPUModelInfoPtr into virCPUDef)
    into a reusable function.
    
    The new function is used in this and future patches.
    
    Signed-off-by: Chris Venteicher <cventeic@redhat.com>
    Message-Id: <20181103031338.11600-29-cventeic@redhat.com>

--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -572,6 +572,9 @@ int virQEMUCapsGetMachineTypesCaps(virQEMUCapsPtr qemuCaps,
 void virQEMUCapsFilterByMachineType(virQEMUCapsPtr qemuCaps,
                                     const char *machineType);
 
+virCPUDefPtr virQEMUCapsCPUModelInfoToCPUDef(bool migratable,
+                                             qemuMonitorCPUModelInfoPtr model);
+
 virFileCachePtr virQEMUCapsCacheNew(const char *libDir,
                                     const char *cacheDir,
                                     uid_t uid,
