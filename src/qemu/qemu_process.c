788f3754448ac07a56b445906c11f0e2a60a58fd
Erik Skultety <eskultet@redhat.com>
2018-11-29 15:20:17 +0100

    conf: gfx: egl-headless: Introduce a new <gl> subelement
    Unlike with SPICE and SDL which use the <gl> subelement to enable OpenGL
    acceleration, specifying egl-headless graphics in the XML has
    essentially the same meaning, thus in case of egl-headless we don't have
    a need for the 'enable' element attribute and we'll only be interested
    in the 'rendernode' one further down the road.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>
    Message-Id: <94237eaf99751a018651d7d99b8a47a9f472b94a.1543500852.git.eskultet@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -4788,11 +4788,21 @@ static int
 qemuProcessGraphicsSetupRenderNode(virDomainGraphicsDefPtr graphics,
                                    virQEMUCapsPtr qemuCaps)
 {
+    char **rendernode = NULL;
     /* Don't bother picking a DRM node if QEMU doesn't support it. */
-    if (!virQEMUCapsGet(qemuCaps, QEMU_CAPS_SPICE_RENDERNODE))
-        return 0;
+    if (graphics->type == VIR_DOMAIN_GRAPHICS_TYPE_SPICE) {
+        if (!virQEMUCapsGet(qemuCaps, QEMU_CAPS_SPICE_RENDERNODE))
+            return 0;
 
-    if (!(graphics->data.spice.rendernode = virHostGetDRMRenderNode()))
+        rendernode = &graphics->data.spice.rendernode;
+    } else {
+        if (!virQEMUCapsGet(qemuCaps, QEMU_CAPS_EGL_HEADLESS_RENDERNODE))
+            return 0;
+
+        rendernode = &graphics->data.egl_headless.rendernode;
+    }
+
+    if (!(*rendernode = virHostGetDRMRenderNode()))
         return -1;
 
     return 0;
