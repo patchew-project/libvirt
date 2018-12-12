2679b12f723ce00caa3c8566483e21cc676b02a5
Peter Krempa <pkrempa@redhat.com>
2018-12-12 18:08:18 +0100

    qemu: blockjob: Emit VIR_DOMAIN_EVENT_ID_BLOCK_JOB only for local disks
    The event reports the disk path to identify the disk which makes sense
    only for local disks. Additionally network backed disks like NBD don't
    need to have a path so the callback would return NULL.
    
    Report VIR_DOMAIN_EVENT_ID_BLOCK_JOB only for non-empty local disks.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <cbaebf1ab01f994249f0a0616a7ed3922cbc715f.1544634194.git.pkrempa@redhat.com>

--- src/qemu/qemu_blockjob.c
+++ src/qemu/qemu_blockjob.c
@@ -46,7 +46,7 @@ VIR_LOG_INIT("qemu.qemu_blockjob");
  * qemuBlockJobEmitEvents:
  *
  * Emits the VIR_DOMAIN_EVENT_ID_BLOCK_JOB and VIR_DOMAIN_EVENT_ID_BLOCK_JOB_2
- * for a block job.
+ * for a block job. The former event is emitted only for local disks.
  */
 static void
 qemuBlockJobEmitEvents(virQEMUDriverPtr driver,
@@ -58,8 +58,11 @@ qemuBlockJobEmitEvents(virQEMUDriverPtr driver,
     virObjectEventPtr event = NULL;
     virObjectEventPtr event2 = NULL;
 
-    event = virDomainEventBlockJobNewFromObj(vm, disk->src->path, type, status);
-    virObjectEventStateQueue(driver->domainEventState, event);
+    if (virStorageSourceIsLocalStorage(disk->src) &&
+        !virStorageSourceIsEmpty(disk->src)) {
+        event = virDomainEventBlockJobNewFromObj(vm, disk->src->path, type, status);
+        virObjectEventStateQueue(driver->domainEventState, event);
+    }
 
     event2 = virDomainEventBlockJob2NewFromObj(vm, disk->dst, type, status);
     virObjectEventStateQueue(driver->domainEventState, event2);
