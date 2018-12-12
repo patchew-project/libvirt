660b50761a2b8e99cbedacc52141a2c2535066e7
Peter Krempa <pkrempa@redhat.com>
2018-12-12 18:09:01 +0100

    qemu: blockjob: Add 'concluded' state for a block job
    This new state is entered when qemu finished the job but libvirt does
    not know whether it was successful or not.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <474f3cfc205c6f2f998b5d44465120c2f4391f34.1544634195.git.pkrempa@redhat.com>

--- src/qemu/qemu_blockjob.c
+++ src/qemu/qemu_blockjob.c
@@ -45,7 +45,7 @@ VIR_LOG_INIT("qemu.qemu_blockjob");
 /* Note that qemuBlockjobState and qemuBlockjobType values are formatted into
  * the status XML */
 VIR_ENUM_IMPL(qemuBlockjobState, QEMU_BLOCKJOB_STATE_LAST,
-              "completed", "failed", "cancelled", "ready", "new", "running");
+              "completed", "failed", "cancelled", "ready", "new", "running", "concluded");
 
 VIR_ENUM_IMPL(qemuBlockjob, QEMU_BLOCKJOB_TYPE_LAST,
               "", "pull", "copy", "commit", "active-commit", "");
