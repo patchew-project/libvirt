5a39bb97350ff91a147071d3468d86a805ed9c89
Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
2018-12-13 14:02:05 +0300

    qemu: don't log error for missing optional storage sources on start
    Because missing optional storage source is not error. The patch
    address only local files. Fixing other cases is a bit ugly.
    Below is example of error notice in log now:
    
    error: virStorageFileReportBrokenChain:427 :
       Cannot access storage file '/path/to/missing/optional/disk':
      	No such file or directory
    
    Signed-off-by: Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
    Message-Id: <1544698925-965336-3-git-send-email-nshirokovskiy@virtuozzo.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -6118,7 +6118,15 @@ qemuProcessPrepareHostStorage(virQEMUDriverPtr driver,
         if (!blockdev)
             virStorageSourceBackingStoreClear(disk->src);
 
-        if (qemuDomainDetermineDiskChain(driver, vm, disk, true) >= 0)
+        /*
+         * Go to applying startup policy for optional disk with nonexistent
+         * source file immediately as detemining chain will surely fail
+         * and we don't want noisy error notice in logs for this case.
+         */
+        if (qemuDomainDiskIsMissingLocalOptional(disk) && cold_boot)
+            VIR_INFO("optional disk '%s' source file is missing, "
+                     "skip checking disk chain", disk->dst);
+        else if (qemuDomainDetermineDiskChain(driver, vm, disk, true) >= 0)
             continue;
 
         if (qemuDomainCheckDiskStartupPolicy(driver, vm, idx, cold_boot) >= 0)
