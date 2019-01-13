663649bf757d5123ce38f894f5bf3db125aeca6b
Chris Venteicher <cventeic@redhat.com>
2019-01-12 18:50:12 -0600

    qemu_process: Store libDir in qemuProcessQMP struct
    Store libDir path in the qemuProcessQMP struct in anticipation of moving
    path construction code into qemuProcessQMPInit function.
    
    Signed-off-by: Chris Venteicher <cventeic@redhat.com>
    Message-Id: <20190113005032.17880-14-cventeic@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -8211,6 +8211,7 @@ qemuProcessQMPFree(qemuProcessQMPPtr proc)
 
     qemuProcessQMPStop(proc);
     VIR_FREE(proc->binary);
+    VIR_FREE(proc->libDir);
     VIR_FREE(proc->monpath);
     VIR_FREE(proc->monarg);
     VIR_FREE(proc->pidfile);
@@ -8231,7 +8232,8 @@ qemuProcessQMPNew(const char *binary,
     if (VIR_ALLOC(proc) < 0)
         goto error;
 
-    if (VIR_STRDUP(proc->binary, binary) < 0)
+    if (VIR_STRDUP(proc->binary, binary) < 0 ||
+        VIR_STRDUP(proc->libDir, libDir) < 0)
         goto error;
 
     proc->runUid = runUid;
@@ -8241,7 +8243,7 @@ qemuProcessQMPNew(const char *binary,
     /* the ".sock" sufix is important to avoid a possible clash with a qemu
      * domain called "capabilities"
      */
-    if (virAsprintf(&proc->monpath, "%s/%s", libDir,
+    if (virAsprintf(&proc->monpath, "%s/%s", proc->libDir,
                     "capabilities.monitor.sock") < 0)
         goto error;
     if (virAsprintf(&proc->monarg, "unix:%s,server,nowait", proc->monpath) < 0)
@@ -8253,7 +8255,7 @@ qemuProcessQMPNew(const char *binary,
      * -daemonize we need QEMU to be allowed to create them, rather
      * than libvirtd. So we're using libDir which QEMU can write to
      */
-    if (virAsprintf(&proc->pidfile, "%s/%s", libDir, "capabilities.pidfile") < 0)
+    if (virAsprintf(&proc->pidfile, "%s/%s", proc->libDir, "capabilities.pidfile") < 0)
         goto error;
 
     virPidFileForceCleanupPath(proc->pidfile);
