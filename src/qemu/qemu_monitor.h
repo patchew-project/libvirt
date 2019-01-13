946ca596c8cc3a0f2308fa862451aaf2afdc86ee
Chris Venteicher <cventeic@redhat.com>
2019-01-12 18:50:31 -0600

    qemu_driver: Support feature expansion via QEMU when baselining cpu
    Support using QEMU to do feature expansion when also using QEMU to
    compute hypervisor baseline.
    
    A QEMU process is already created to send the QMP messages to baseline
    using QEMU.
    The same QEMU process is used for the CPU feature expansion.
    
    QEMU only returns migratable features when expanding CPU model in
    architectures where QEMU is used for baseline so no attempt is made to
    ask for non-migratable features in expansions when using QEMU for
    baseline.
    
    Signed-off-by: Chris Venteicher <cventeic@redhat.com>
    Message-Id: <20190113005032.17880-33-cventeic@redhat.com>

--- src/qemu/qemu_monitor.h
+++ src/qemu/qemu_monitor.h
@@ -1078,6 +1078,10 @@ int qemuMonitorCPUModelInfoBoolPropAdd(qemuMonitorCPUModelInfoPtr model,
                                        bool prop_value)
     ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
 
+void qemuMonitorCPUModelInfoRemovePropByBoolValue(qemuMonitorCPUModelInfoPtr model,
+                                                  bool value)
+    ATTRIBUTE_NONNULL(1);
+
 int qemuMonitorGetCommands(qemuMonitorPtr mon,
                            char ***commands);
 int qemuMonitorGetEvents(qemuMonitorPtr mon,
