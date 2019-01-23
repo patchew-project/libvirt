4b1873a9024882425534400c505427f7f581ea1b
Cole Robinson <crobinso@redhat.com>
2019-01-23 16:32:50 -0500

    qemu: Support input model=virtio-{non-}transitional
    Add <input> model handling for virtio transitional devices. Ex:
    
      <input type='passthrough' bus='virtio' model='virtio-transitional'>
        ...
      </input>
    
    * "virtio-transitional" maps to qemu "virtio-input-host-pci-transitional"
    * "virtio-non-transitional" maps to qemu "virtio-input-host-non-transitional"
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>
    Message-Id: <3b52eb01e5c90f4c1ada71a5eefe63e5acd72c2e.1548278586.git.crobinso@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -516,8 +516,16 @@ qemuBuildVirtioDevStr(virBufferPtr buf,
             ntmodel_cap = QEMU_CAPS_DEVICE_VHOST_VSOCK_PCI_NON_TRANSITIONAL;
             break;
 
-        case VIR_DOMAIN_DEVICE_LEASE:
         case VIR_DOMAIN_DEVICE_INPUT:
+            if (device.data.input->type != VIR_DOMAIN_INPUT_TYPE_PASSTHROUGH)
+                return 0;
+            has_tmodel = device.data.input->model == VIR_DOMAIN_INPUT_MODEL_VIRTIO_TRANSITIONAL;
+            has_ntmodel = device.data.input->model == VIR_DOMAIN_INPUT_MODEL_VIRTIO_NON_TRANSITIONAL;
+            tmodel_cap = QEMU_CAPS_DEVICE_VIRTIO_INPUT_HOST_PCI_TRANSITIONAL;
+            ntmodel_cap = QEMU_CAPS_DEVICE_VIRTIO_INPUT_HOST_PCI_NON_TRANSITIONAL;
+            break;
+
+        case VIR_DOMAIN_DEVICE_LEASE:
         case VIR_DOMAIN_DEVICE_SOUND:
         case VIR_DOMAIN_DEVICE_VIDEO:
         case VIR_DOMAIN_DEVICE_WATCHDOG:
