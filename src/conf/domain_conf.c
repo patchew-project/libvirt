e16aa1c56de76238e68ba401c8099aadb533e800
Cole Robinson <crobinso@redhat.com>
2019-03-13 11:51:19 -0400

    vbox: Convert to net enum model
    Convert the vbox driver to net model enum, which requires adding
    enum values for Am79C970A, Am79C973, 82540EM, 82545EM, 82543GC. We
    preserve the same casing that vbox historically used for these model
    names.
    
    Remove the now unused virDomainNetStrcaseeqModelString
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>
    Message-Id: <9a04cae6cf2dbe3a933c0d4a1c6a77649c69f660.1552492022.git.crobinso@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -489,6 +489,11 @@ VIR_ENUM_IMPL(virDomainNetModel, VIR_DOMAIN_NET_MODEL_LAST,
               "vmxnet",
               "vmxnet2",
               "vmxnet3",
+              "Am79C970A",
+              "Am79C973",
+              "82540EM",
+              "82545EM",
+              "82543GC",
 );
 
 VIR_ENUM_IMPL(virDomainNetBackend, VIR_DOMAIN_NET_BACKEND_TYPE_LAST,
@@ -29428,15 +29433,6 @@ virDomainNetSetModelString(virDomainNetDefPtr net,
     if (VIR_STRDUP(net->modelstr, model) < 0)
         return -1;
     return 0;
-}
-
-int
-virDomainNetStrcaseeqModelString(const virDomainNetDef *net,
-                                 const char *model)
-{
-    if (net->model)
-        return STRCASEEQ(virDomainNetModelTypeToString(net->model), model);
-    return net->modelstr && STRCASEEQ(net->modelstr, model);
 }
 
 bool
