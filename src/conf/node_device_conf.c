patchew-project/libvirt

commit 3bd4ed4630e1445aee290ab6d91e21bdd07cabb3

xml: nodedev: add class info for pci capability
This info can be useful to filter devices visible
to mgmt clients so that they won't see devices that
unsafe/not meaningful to pass thru.

Provide class info the way it is provided by udev or
kernel that is as single 6-digit hexadecimal.

Class element is not optional. I guess this should not
break users that use virNodeDeviceCreateXML because
they probably specify only scsi_host capability on
input and then node device driver gets other capabilities
from udev after device appeared.

HAL driver does not get support for the new element in
this patch.

Signed-off-by: Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

docs/formatnode.html.in
@@ -70,6 +70,10 @@
           <dd>Describes a device on the host's PCI bus.  Sub-elements
             include:
             <dl>
+              <dt><code>class</code></dt>
+              <dd>Combined class, subclass and
+                programming interface codes as 6-digit hexadecimal number.
+                <span class="since">Since 5.2.0</span></dd>
               <dt><code>domain</code></dt>
               <dd>Which domain the device belongs to.</dd>
               <dt><code>bus</code></dt>
@@ -381,6 +385,7 @@
     &lt;name&gt;igb&lt;/name&gt;
   &lt;/driver&gt;
   &lt;capability type='pci'&gt;
+    &lt;class&gt;0x020000&lt;/class&gt;
     &lt;domain&gt;0&lt;/domain&gt;
     &lt;bus&gt;2&lt;/bus&gt;
     &lt;slot&gt;0&lt;/slot&gt;

docs/schemas/nodedev.rng
@@ -133,6 +133,11 @@
       <value>pci</value>
     </attribute>
 
+    <element name='class'>
+      <data type="string">
+        <param name="pattern">0x[0-9a-fA-F]{6}</param>
+      </data>
+    </element>
     <element name='domain'>
       <ref name='unsignedLong'/>
     </element>

src/conf/node_device_conf.c
@@ -208,6 +208,7 @@ virNodeDeviceCapPCIDefFormat(virBufferPtr buf,
 {
     size_t i;
 
+    virBufferAsprintf(buf, "<class>0x%.6x</class>\n", data->pci_dev.klass);
     virBufferAsprintf(buf, "<domain>%d</domain>\n",
                       data->pci_dev.domain);
     virBufferAsprintf(buf, "<bus>%d</bus>\n", data->pci_dev.bus);
@@ -1643,6 +1644,18 @@ virNodeDevCapPCIDevParseXML(xmlXPathContextPtr ctxt,
 
     orignode = ctxt->node;
     ctxt->node = node;
+
+    if (virNodeDevCapsDefParseHexId("string(./class[1])", ctxt,
+                                    &pci_dev->klass, def,
+                                    _("no PCI class supplied for '%s'"),
+                                    _("invalid PCI class supplied for '%s'")) < 0)
+        goto out;
+
+    if (pci_dev->klass > 0xffffff) {
+        virReportError(VIR_ERR_INTERNAL_ERROR,
+                       _("invalid PCI class supplied for '%s'"), def->name);
+        goto out;
+    }
 
     if (virNodeDevCapsDefParseULong("number(./domain[1])", ctxt,
                                     &pci_dev->domain, def,

tests/nodedevschemadata/pci_0000_02_10_7_sriov_pf_vfs_all.xml
@@ -2,6 +2,7 @@
   <name>pci_0000_02_10_7</name>
   <parent>pci_0000_00_04_0</parent>
   <capability type='pci'>
+    <class>0xffffff</class>
     <domain>0</domain>
     <bus>2</bus>
     <slot>16</slot>

tests/nodedevschemadata/pci_0000_00_02_0_header_type.xml
@@ -2,6 +2,7 @@
   <name>pci_0000_00_02_0</name>
   <parent>computer</parent>
   <capability type='pci'>
+    <class>0xffffff</class>
     <domain>0</domain>
     <bus>0</bus>
     <slot>2</slot>

tests/nodedevschemadata/pci_0000_00_1c_0_header_type.xml
@@ -2,6 +2,7 @@
   <name>pci_0000_00_1c_0</name>
   <parent>computer</parent>
   <capability type='pci'>
+    <class>0xffffff</class>
     <domain>0</domain>
     <bus>0</bus>
     <slot>28</slot>

tests/nodedevschemadata/pci_0000_02_10_7_mdev_types.xml
@@ -2,6 +2,7 @@
   <name>pci_0000_02_10_7</name>
   <parent>pci_0000_00_04_0</parent>
   <capability type='pci'>
+    <class>0xffffff</class>
     <domain>0</domain>
     <bus>2</bus>
     <slot>16</slot>

tests/nodedevschemadata/pci_0000_02_10_7_sriov.xml
@@ -2,6 +2,7 @@
   <name>pci_0000_02_10_7</name>
   <parent>pci_0000_00_04_0</parent>
   <capability type='pci'>
+    <class>0xffffff</class>
     <domain>0</domain>
     <bus>2</bus>
     <slot>16</slot>

commit ac16acf71c2766b55913eea9d57e17b93a8a0b4e

xml: nodedev: make pci capability class element optional
Commit 3bd4ed46 introduced this element as required which
breaks backcompat for test driver. Let's make the element optional.

Signed-off-by: Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
Message-Id: <1553153234-427807-1-git-send-email-nshirokovskiy@virtuozzo.com>
Reviewed-by: Erik Skultety <eskultet@redhat.com>

docs/formatnode.html.in
@@ -71,7 +71,7 @@
             include:
             <dl>
               <dt><code>class</code></dt>
-              <dd>Combined class, subclass and
+              <dd>Optional element for combined class, subclass and
                 programming interface codes as 6-digit hexadecimal number.
                 <span class="since">Since 5.2.0</span></dd>
               <dt><code>domain</code></dt>

docs/schemas/nodedev.rng
@@ -133,11 +133,13 @@
       <value>pci</value>
     </attribute>
 
-    <element name='class'>
-      <data type="string">
-        <param name="pattern">0x[0-9a-fA-F]{6}</param>
-      </data>
-    </element>
+    <optional>
+      <element name='class'>
+        <data type="string">
+          <param name="pattern">0x[0-9a-fA-F]{6}</param>
+        </data>
+      </element>
+    </optional>
     <element name='domain'>
       <ref name='unsignedLong'/>
     </element>

src/conf/node_device_conf.c
@@ -208,7 +208,8 @@ virNodeDeviceCapPCIDefFormat(virBufferPtr buf,
 {
     size_t i;
 
-    virBufferAsprintf(buf, "<class>0x%.6x</class>\n", data->pci_dev.klass);
+    if (data->pci_dev.klass >= 0)
+        virBufferAsprintf(buf, "<class>0x%.6x</class>\n", data->pci_dev.klass);
     virBufferAsprintf(buf, "<domain>%d</domain>\n",
                       data->pci_dev.domain);
     virBufferAsprintf(buf, "<bus>%d</bus>\n", data->pci_dev.bus);
@@ -1645,16 +1646,16 @@ virNodeDevCapPCIDevParseXML(xmlXPathContextPtr ctxt,
     orignode = ctxt->node;
     ctxt->node = node;
 
-    if (virNodeDevCapsDefParseHexId("string(./class[1])", ctxt,
-                                    &pci_dev->klass, def,
-                                    _("no PCI class supplied for '%s'"),
-                                    _("invalid PCI class supplied for '%s'")) < 0)
-        goto out;
-
-    if (pci_dev->klass > 0xffffff) {
-        virReportError(VIR_ERR_INTERNAL_ERROR,
-                       _("invalid PCI class supplied for '%s'"), def->name);
-        goto out;
+    if ((tmp = virXPathString("string(./class[1])", ctxt))) {
+        if (virStrToLong_i(tmp, NULL, 16, &pci_dev->klass) < 0 ||
+            pci_dev->klass > 0xffffff) {
+            virReportError(VIR_ERR_INTERNAL_ERROR,
+                           _("invalid PCI class supplied for '%s'"), def->name);
+            goto out;
+        }
+        VIR_FREE(tmp);
+    } else {
+        pci_dev->klass = -1;
     }
 
     if (virNodeDevCapsDefParseULong("number(./domain[1])", ctxt,

src/conf/node_device_conf.h
@@ -153,7 +153,7 @@ struct _virNodeDevCapPCIDev {
     unsigned int function;
     unsigned int product;
     unsigned int vendor;
-    unsigned int klass;
+    int klass;
     char *product_name;
     char *vendor_name;
     virPCIDeviceAddressPtr physical_function;

src/node_device/node_device_udev.c
@@ -402,7 +402,7 @@ udevProcessPCI(struct udev_device *device,
     privileged = driver->privileged;
     nodeDeviceUnlock();
 
-    if (udevGetUintProperty(device, "PCI_CLASS", &pci_dev->klass, 16) < 0)
+    if (udevGetIntProperty(device, "PCI_CLASS", &pci_dev->klass, 16) < 0)
         goto cleanup;
 
     if ((p = strrchr(def->sysfs_path, '/')) == NULL ||

tests/nodedevschemadata/pci_0000_02_10_7_sriov_pf_vfs_all.xml
@@ -2,7 +2,6 @@
   <name>pci_0000_02_10_7</name>
   <parent>pci_0000_00_04_0</parent>
   <capability type='pci'>
-    <class>0xffffff</class>
     <domain>0</domain>
     <bus>2</bus>
     <slot>16</slot>