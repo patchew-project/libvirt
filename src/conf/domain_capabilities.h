6786f58feb0708e7d32f356381ebbe034864bf86
Cole Robinson <crobinso@redhat.com>
2019-04-04 19:37:25 -0400

    conf: domcaps: Add virDomainCapsDeviceDefValidate
    This is an entrypoint to validate a virDomainDeviceDef against
    values filled into virDomainCaps.
    
    Currently it's just a stub
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>
    Message-Id: <f6fcd26435ede7c8136f8b8c9dbd4dc94ded3926.1554419814.git.crobinso@redhat.com>

--- src/conf/domain_capabilities.h
+++ src/conf/domain_capabilities.h
@@ -223,6 +223,10 @@ void virDomainCapsEnumClear(virDomainCapsEnumPtr capsEnum);
 
 char * virDomainCapsFormat(virDomainCapsPtr const caps);
 
+int virDomainCapsDeviceDefValidate(virDomainCapsPtr const caps,
+                                   const virDomainDeviceDef *dev,
+                                   const virDomainDef *def);
+
 void
 virSEVCapabilitiesFree(virSEVCapability *capabilities);
 
