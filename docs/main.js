0185945330b6e0a368e4640c669c2374cda762f3
Daniel P. Berrangé <berrange@redhat.com>
2019-04-05 14:30:31 +0100

    docs: add advanced search capabilities
    Allow targetting the search scope to the website, wiki or mailing lists
    only. When javascript is disabled this should gracefully fallback to
    only searching the website.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20190405133031.6034-3-berrange@redhat.com>

--- docs/main.js
+++ docs/main.js
@@ -22,4 +22,48 @@ function pageload() {
             }
         }
     });
+
+    /* Setting this class makes the advanced search options visible */
+    advancedSearch = document.getElementById("advancedsearch")
+    advancedSearch.className = "advancedsearch"
+
+    simpleSearch = document.getElementById("simplesearch")
+    simplesearch.addEventListener("submit", advancedsearch)
+}
+
+function advancedsearch(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    form = document.createElement("form");
+    form.setAttribute("method", "get");
+    form.setAttribute("action", "https://google.com/search");
+
+    newq = document.createElement("input");
+    newq.setAttribute("type", "hidden");
+    newq.setAttribute("name", "q");
+    form.appendChild(newq);
+
+    q = document.getElementById("searchq");
+    whats = document.getElementsByName("what");
+    what = "website";
+    for (var i = 0; i < whats.length; i++) {
+	if (whats[i].checked) {
+	    what = whats[i].value;
+	    break;
+	}
+    }
+
+    if (what == "website") {
+	newq.value = "site:libvirt.org " + q.value;
+    } else if (what == "wiki") {
+	newq.value = "site:wiki.libvirt.org " + q.value;
+    } else if (what == "lists") {
+	newq.value = "site:redhat.com inurl:/archives/libvir " + q.value;
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+
+    return false;
 }
