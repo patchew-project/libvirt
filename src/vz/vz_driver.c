aa3975168203d4b781d671e2cb8fd2705806a4c8
Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
2019-04-08 11:42:52 +0300

    vz: fixes: snapshot: Switch type of virDomainSnapshotObj.def
    Fix for commit 1ab05da22 refactoring snapshot code.
    
    Signed-off-by: Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
    Message-Id: <1554712973-336801-3-git-send-email-nshirokovskiy@virtuozzo.com>
    Reviewed-by: Cole Robinson <crobinso@redhat.com>

--- src/vz/vz_driver.c
+++ src/vz/vz_driver.c
@@ -2266,7 +2266,8 @@ vzDomainSnapshotGetXMLDesc(virDomainSnapshotPtr snapshot, unsigned int flags)
 
     virUUIDFormat(snapshot->domain->uuid, uuidstr);
 
-    xml = virDomainSnapshotDefFormat(uuidstr, snap->def, privconn->driver->caps,
+    xml = virDomainSnapshotDefFormat(uuidstr, virDomainSnapshotObjGetDef(snap),
+                                     privconn->driver->caps,
                                      privconn->driver->xmlopt,
                                      virDomainSnapshotFormatConvertXMLFlags(flags));
 
