2b7a458e467fb155ab395aadf1eeb126739afd36
Daniel P. Berrangé <berrange@redhat.com>
2019-04-17 18:19:15 +0100

    network: remove the virDomainNetBandwidthChangeAllowed callback
    The current qemu driver code for changing bandwidth on a NIC first asks
    the network driver if the change is supported, then changes the
    bandwidth on the VIF, and then tells the network driver to update the
    bandwidth on the bridge.
    
    This is potentially racing if a parallel API call causes the network
    driver to allocate bandwidth on the bridge between the check and the
    update phases.
    
    Change the code to just try to apply the network bridge update
    immediately and rollback at the end if something failed.
    
    Reviewed-by: Laine Stump <laine@laine.org>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20190417171929.9399-16-berrange@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -3579,10 +3579,6 @@ typedef int
                                        virDomainDefPtr dom,
                                        virDomainNetDefPtr iface);
 
-typedef bool
-(*virDomainNetBandwidthChangeAllowedImpl)(virDomainNetDefPtr iface,
-                                          virNetDevBandwidthPtr newBandwidth);
-
 typedef int
 (*virDomainNetBandwidthUpdateImpl)(virDomainNetDefPtr iface,
                                    virNetDevBandwidthPtr newBandwidth);
@@ -3592,7 +3588,6 @@ void
 virDomainNetSetDeviceImpl(virDomainNetAllocateActualDeviceImpl allocate,
                           virDomainNetNotifyActualDeviceImpl notify,
                           virDomainNetReleaseActualDeviceImpl release,
-                          virDomainNetBandwidthChangeAllowedImpl bandwidthChangeAllowed,
                           virDomainNetBandwidthUpdateImpl bandwidthUpdate);
 
 int
@@ -3611,11 +3606,6 @@ int
 virDomainNetReleaseActualDevice(virConnectPtr conn,
                                 virDomainDefPtr dom,
                                 virDomainNetDefPtr iface)
-    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
-
-bool
-virDomainNetBandwidthChangeAllowed(virDomainNetDefPtr iface,
-                              virNetDevBandwidthPtr newBandwidth)
     ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
 
 int
