ebb3441615ef289102c507a708e766f8320b8c0d
Ilias Stamatis <stamatis.iliass@gmail.com>
2019-04-24 18:57:57 +0200

    test_driver: provide virDomainGetTime implementation
    Implement testDomainGetTime by returning a fixed timestamp.
    
    Signed-off-by: Ilias Stamatis <stamatis.iliass@gmail.com>
    Message-Id: <20190424165756.4579-1-stamatis.iliass@gmail.com>

--- src/test/test_driver.c
+++ src/test/test_driver.c
@@ -1943,6 +1943,18 @@ testDomainGetState(virDomainPtr domain,
     return 0;
 }
 
+static int
+testDomainGetTime(virDomainPtr dom ATTRIBUTE_UNUSED,
+                  long long *seconds,
+                  unsigned int *nseconds,
+                  unsigned int flags ATTRIBUTE_UNUSED)
+{
+    *seconds = 627319920;
+    *nseconds = 0;
+
+    return 0;
+}
+
 #define TEST_SAVE_MAGIC "TestGuestMagic"
 
 static int
@@ -6786,6 +6798,7 @@ static virHypervisorDriver testHypervisorDriver = {
     .domainSetMemory = testDomainSetMemory, /* 0.1.4 */
     .domainGetInfo = testDomainGetInfo, /* 0.1.1 */
     .domainGetState = testDomainGetState, /* 0.9.2 */
+    .domainGetTime = testDomainGetTime, /* 5.3.0 */
     .domainSave = testDomainSave, /* 0.3.2 */
     .domainSaveFlags = testDomainSaveFlags, /* 0.9.4 */
     .domainRestore = testDomainRestore, /* 0.3.2 */
