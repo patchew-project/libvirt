560ced5d83fa095e7f715a960a739cb8ba8d7e21
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2019-05-16 09:38:55 +0200

    virt-aa-helper: clean old reference to libvirt-sandbox
    It came up that libvirt-sandbox is a failed (and removed) experiment.
    Let us clean up and remove it from the virt-aa-helper source as well.
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>
    Message-Id: <20190516073855.15199-1-christian.ehrhardt@canonical.com>

--- src/security/virt-aa-helper.c
+++ src/security/virt-aa-helper.c
@@ -511,8 +511,7 @@ valid_path(const char *path, const bool readonly)
     /* override the above with these */
     const char * const override[] = {
         "/sys/devices/pci",                /* for hostdev pci devices */
-        "/sys/kernel/config/target/vhost", /* for hostdev vhost_scsi devices */
-        "/etc/libvirt-sandbox/services/"   /* for virt-sandbox service config */
+        "/sys/kernel/config/target/vhost"  /* for hostdev vhost_scsi devices */
     };
 
     const int nropaths = ARRAY_CARDINALITY(restricted);
