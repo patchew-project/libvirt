53f0a14c9e8d26132bcf5532a9755025ecf3d265
Peter Krempa <pkrempa@redhat.com>
2019-07-12 18:05:47 +0200

    qemu: blockjob: Register new and running blockjobs in the global table
    Add the job structure to the table when instantiating a new job and
    remove it when it terminates/fails.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <f8b9364bd47451014dcca7b7c808359018246dcf.1562947284.git.pkrempa@redhat.com>

--- src/qemu/qemu_migration.c
+++ src/qemu/qemu_migration.c
@@ -921,7 +921,7 @@ qemuMigrationSrcNBDStorageCopyOne(virQEMUDriverPtr driver,
     if (!(diskAlias = qemuAliasDiskDriveFromDisk(disk)))
         goto cleanup;
 
-    if (!(job = qemuBlockJobDiskNew(disk, QEMU_BLOCKJOB_TYPE_COPY, diskAlias)))
+    if (!(job = qemuBlockJobDiskNew(vm, disk, QEMU_BLOCKJOB_TYPE_COPY, diskAlias)))
         goto cleanup;
 
     qemuBlockJobSyncBegin(job);
@@ -949,7 +949,7 @@ qemuMigrationSrcNBDStorageCopyOne(virQEMUDriverPtr driver,
     ret = 0;
 
  cleanup:
-    qemuBlockJobStartupFinalize(job);
+    qemuBlockJobStartupFinalize(vm, job);
     VIR_FREE(diskAlias);
     return ret;
 }
