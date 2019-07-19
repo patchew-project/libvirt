5870041cf7f2bdd23233c496dd49d4b03b93dd40
Michal Privoznik <mprivozn@redhat.com>
2019-07-19 10:48:25 +0200

    tools: s/Nodedev/NodeDevice/
    The proper name is [vir|virsh]NodeDevice* and not Nodedev.
    Fortunately, there are only handful of offenders.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <079406baf747eafc12bacd71f0aeb893768a29a3.1563525985.git.mprivozn@redhat.com>

--- tools/virsh-completer.c
+++ tools/virsh-completer.c
@@ -953,9 +953,9 @@ virshDomainInterfaceStateCompleter(vshControl *ctl,
 
 
 char **
-virshNodedevEventNameCompleter(vshControl *ctl ATTRIBUTE_UNUSED,
-                               const vshCmd *cmd ATTRIBUTE_UNUSED,
-                               unsigned int flags)
+virshNodeDeviceEventNameCompleter(vshControl *ctl ATTRIBUTE_UNUSED,
+                                  const vshCmd *cmd ATTRIBUTE_UNUSED,
+                                  unsigned int flags)
 {
     size_t i = 0;
     char **ret = NULL;
@@ -967,7 +967,7 @@ virshNodedevEventNameCompleter(vshControl *ctl ATTRIBUTE_UNUSED,
         return NULL;
 
     for (i = 0; i < VIR_NODE_DEVICE_EVENT_ID_LAST; i++) {
-        if (VIR_STRDUP(tmp[i], virshNodedevEventCallbacks[i].name) < 0)
+        if (VIR_STRDUP(tmp[i], virshNodeDeviceEventCallbacks[i].name) < 0)
             return NULL;
     }
 
@@ -977,9 +977,9 @@ virshNodedevEventNameCompleter(vshControl *ctl ATTRIBUTE_UNUSED,
 
 
 char **
-virshNodedevCapabilityNameCompleter(vshControl *ctl,
-                                    const vshCmd *cmd,
-                                    unsigned int flags)
+virshNodeDeviceCapabilityNameCompleter(vshControl *ctl,
+                                       const vshCmd *cmd,
+                                       unsigned int flags)
 {
     VIR_AUTOSTRINGLIST tmp = NULL;
     const char *cap_str = NULL;
