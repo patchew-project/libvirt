25d399dcc3e10dc892a2c11e507df5c2fe2c2f90
Ilias Stamatis <stamatis.iliass@gmail.com>
2019-08-07 18:56:35 +0200

    test_driver: implement virDomainManagedSaveGetXMLDesc
    The managedSave APIs according to the documentation are supposed to
    operate on a disk file. However, this might not be appropriate in the
    case of the test driver since:
    
    * It's better if the test driver keeps all its state in memory only and
    doesn't affect the host in any way.
    
    * The test driver, apart from "emulating" the domains, it additionally
    "emulates" a fake physical host. Every time we start a new test
    connection that sort of means that a new physical host is created as
    well. And this fake host isn't necessarily the same.
    
    What we can do instead is operating on the already existing domain
    definitions. So along as a connection remains open, a domain can
    preserve the managed state between different shutdown / create calls.
    When the test connection closes this means that the fake host is
    destroyed as well, hence no other state is preserved after that.
    
    This way we also make sure that we don't touch the real host's
    filesystem.
    
    Signed-off-by: Ilias Stamatis <stamatis.iliass@gmail.com>
    Message-Id: <20190807165635.30528-2-stamatis.iliass@gmail.com>

--- src/test/test_driver.c
+++ src/test/test_driver.c
@@ -7606,6 +7606,33 @@ testDomainManagedSave(virDomainPtr dom, unsigned int flags)
 }
 
 
+static char *
+testDomainManagedSaveGetXMLDesc(virDomainPtr dom,
+                                unsigned int flags)
+{
+    virDomainObjPtr vm;
+    testDriverPtr privconn = dom->conn->privateData;
+    char *ret = NULL;
+
+    virCheckFlags(VIR_DOMAIN_SAVE_IMAGE_XML_SECURE, NULL);
+
+    if (!(vm = testDomObjFromDomain(dom)))
+        return NULL;
+
+    if (vm->hasManagedSave == false) {
+        virReportError(VIR_ERR_OPERATION_INVALID, "%s",
+                       _("domain does not have managed save image"));
+        goto cleanup;
+    }
+
+    ret = virDomainDefFormat(vm->def, privconn->caps, VIR_DOMAIN_DEF_FORMAT_SECURE);
+
+ cleanup:
+    virDomainObjEndAPI(&vm);
+    return ret;
+}
+
+
 static int
 testDomainHasManagedSaveImage(virDomainPtr dom, unsigned int flags)
 {
@@ -9027,6 +9054,7 @@ static virHypervisorDriver testHypervisorDriver = {
     .domainSendProcessSignal = testDomainSendProcessSignal, /* 5.5.0 */
     .connectGetCPUModelNames = testConnectGetCPUModelNames, /* 1.1.3 */
     .domainManagedSave = testDomainManagedSave, /* 1.1.4 */
+    .domainManagedSaveGetXMLDesc = testDomainManagedSaveGetXMLDesc, /* 5.7.0 */
     .domainHasManagedSaveImage = testDomainHasManagedSaveImage, /* 1.1.4 */
     .domainManagedSaveRemove = testDomainManagedSaveRemove, /* 1.1.4 */
     .domainMemoryStats = testDomainMemoryStats, /* 5.7.0 */
