7ee78d4921988b26d393bffcc71d70e4d2d2f855
Peter Krempa <pkrempa@redhat.com>
2019-09-16 14:17:04 +0200

    util: xml: Add wrapper for xmlXPathNewContext
    The wrapper reports libvirt errors for the libxml2 function so that
    the same does not have to be repeated over and over.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <778d3e675eb92479e01e74a5fe35402b499f0bb3.1568636165.git.pkrempa@redhat.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -3612,10 +3612,8 @@ virQEMUCapsLoadCache(virArch hostArch,
     if (!(doc = virXMLParseFile(filename)))
         goto cleanup;
 
-    if (!(ctxt = xmlXPathNewContext(doc))) {
-        virReportOOMError();
+    if (!(ctxt = virXMLXPathContextNew(doc)))
         goto cleanup;
-    }
 
     ctxt->node = xmlDocGetRootElement(doc);
 
