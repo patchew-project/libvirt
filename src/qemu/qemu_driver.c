c350e83ff3c5240b2b2477e6f55aee13aa54635f
Peter Krempa <pkrempa@redhat.com>
2019-09-19 19:13:25 +0200

    qemu: driver: Remove unused cleanup labels in stats gathering functions
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <b5a378fd6ca2491ae1d7018cd2da7003cc5339cf.1568911535.git.pkrempa@redhat.com>

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -21537,22 +21537,18 @@ qemuDomainGetStatsBlockExportHeader(virDomainDiskDefPtr disk,
                                     size_t recordnr,
                                     virTypedParamListPtr params)
 {
-    int ret = -1;
-
     if (virTypedParamListAddS(params, disk->dst, "block.%zu.name", recordnr) < 0)
-        goto cleanup;
+        return -1;
 
     if (virStorageSourceIsLocalStorage(src) && src->path &&
         virTypedParamListAddS(params, src->path, "block.%zu.path", recordnr) < 0)
-        goto cleanup;
+        return -1;
 
     if (src->id &&
         virTypedParamListAddUI(params, src->id, "block.%zu.backingIndex", recordnr) < 0)
-        goto cleanup;
+        return -1;
 
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 
@@ -21796,20 +21792,16 @@ qemuDomainGetStatsPerf(virQEMUDriverPtr driver ATTRIBUTE_UNUSED,
 {
     size_t i;
     qemuDomainObjPrivatePtr priv = dom->privateData;
-    int ret = -1;
 
     for (i = 0; i < VIR_PERF_EVENT_LAST; i++) {
         if (!virPerfEventIsEnabled(priv->perf, i))
              continue;
 
         if (qemuDomainGetStatsPerfOneEvent(priv->perf, i, params) < 0)
-            goto cleanup;
+            return -1;
     }
 
-    ret = 0;
-
- cleanup:
-    return ret;
+    return 0;
 }
 
 typedef int
