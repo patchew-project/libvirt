791609574e452ca143552e59167ca08e451d8a0c
Michal Privoznik <mprivozn@redhat.com>
2019-09-26 18:12:29 +0200

    qemu_capabilities: Introduce QEMU_CAPS_DRIVE_NVME
    This capability tracks if qemu is capable of:
    
      -drive file.driver=nvme
    
    The feature was added in QEMU's commit of v2.12.0-rc0~104^2~2.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <59e3230b5d2dba73cdd76b034aef5644c035e34e.1569514291.git.mprivozn@redhat.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -540,6 +540,9 @@ VIR_ENUM_IMPL(virQEMUCaps,
               "dbus-vmstate",
               "vhost-user-gpu",
               "vhost-user-vga",
+
+              /* 340 */
+              "drive-nvme",
     );
 
 
@@ -1287,6 +1290,7 @@ static struct virQEMUCapsStringFlags virQEMUCapsQMPSchemaQueries[] = {
     { "query-display-options/ret-type/+egl-headless/rendernode", QEMU_CAPS_EGL_HEADLESS_RENDERNODE },
     { "nbd-server-add/arg-type/bitmap", QEMU_CAPS_NBD_BITMAP },
     { "blockdev-add/arg-type/+file/drop-cache", QEMU_CAPS_MIGRATION_FILE_DROP_CACHE },
+    { "blockdev-add/arg-type/+nvme", QEMU_CAPS_DRIVE_NVME },
 };
 
 typedef struct _virQEMUCapsObjectTypeProps virQEMUCapsObjectTypeProps;
