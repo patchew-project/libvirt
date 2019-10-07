b8efbfd2a973b241c50a9ecf7cf5057e64555631
Cole Robinson <crobinso@redhat.com>
2019-10-07 17:49:41 -0400

    security: selinux: Add is_toplevel to SetImageLabelInternal
    This will simplify future patches and make the logic easier to follow
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>
    Message-Id: <726ba82b9504505face29a83451f27a046635bcd.1570482718.git.crobinso@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- src/security/security_selinux.c
+++ src/security/security_selinux.c
@@ -1824,6 +1824,7 @@ virSecuritySELinuxSetImageLabelInternal(virSecurityManagerPtr mgr,
     virSecurityDeviceLabelDefPtr parent_seclabel = NULL;
     char *use_label = NULL;
     bool remember;
+    bool is_toplevel = parent == src;
     int ret;
 
     if (!src->path || !virStorageSourceIsLocalStorage(src))
@@ -1845,7 +1846,7 @@ virSecuritySELinuxSetImageLabelInternal(virSecurityManagerPtr mgr,
      * but the top layer, or read only image, or disk explicitly
      * marked as shared.
      */
-    remember = src == parent && !src->readonly && !src->shared;
+    remember = is_toplevel && !src->readonly && !src->shared;
 
     disk_seclabel = virStorageSourceGetSecurityLabelDef(src,
                                                         SECURITY_SELINUX_NAME);
@@ -1862,7 +1863,7 @@ virSecuritySELinuxSetImageLabelInternal(virSecurityManagerPtr mgr,
             return 0;
 
         use_label = parent_seclabel->label;
-    } else if (parent == src) {
+    } else if (is_toplevel) {
         if (src->shared) {
             use_label = data->file_context;
         } else if (src->readonly) {
