8b009cf89bd446170aec0485bfacbf0d9207d769
Jiri Denemark <jdenemar@redhat.com>
2019-11-11 21:35:33 +0100

    qemuxml2*test: Add tests for Icelake-Server, -pconfig
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
    Message-Id: <b2fe41b424efecdab7f97143ccbce52c51277f8f.1573504055.git.jdenemar@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -3027,6 +3027,9 @@ mymain(void)
     DO_TEST_CAPS_LATEST("vhost-user-vga");
     DO_TEST_CAPS_LATEST("vhost-user-gpu-secondary");
 
+    DO_TEST_CAPS_VER("cpu-Icelake-Server-pconfig", "3.1.0");
+    DO_TEST_CAPS_LATEST("cpu-Icelake-Server-pconfig");
+
     if (getenv("LIBVIRT_SKIP_CLEANUP") == NULL)
         virFileDeleteTree(fakerootdir);
 
