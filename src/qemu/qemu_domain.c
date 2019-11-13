patchew-project/libvirt

commit 224d269f19f0a6c496dd2218f934a54742d51708

qemuDomainGetHostdevPath: Drop @freeTmpPath
The @freeTmpPath boolean is used to determine if @tmpPath holds
an allocated memory or is a pointer to a constant string and
therefore if it needs to be freed or not when returning from the
function. Well, we can unify the way we set @tmpPath so that it
always holds an allocated memory and thus always must be freed.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Cole Robinson <crobinso@redhat.com>

src/qemu/qemu_domain.c
@@ -12758,7 +12758,6 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
     virSCSIDevicePtr scsi = NULL;
     virSCSIVHostDevicePtr host = NULL;
     char *tmpPath = NULL;
-    bool freeTmpPath = false;
     bool includeVFIO = false;
     char **tmpPaths = NULL;
     int *tmpPerms = NULL;
@@ -12781,7 +12780,6 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
 
                 if (!(tmpPath = virPCIDeviceGetIOMMUGroupDev(pci)))
                     goto cleanup;
-                freeTmpPath = true;
 
                 perm = VIR_CGROUP_DEVICE_RW;
                 if (teardown) {
@@ -12802,7 +12800,8 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
             if (!usb)
                 goto cleanup;
 
-            tmpPath = (char *)virUSBDeviceGetPath(usb);
+            if (VIR_STRDUP(tmpPath, virUSBDeviceGetPath(usb)) < 0)
+                goto cleanup;
             perm = VIR_CGROUP_DEVICE_RW;
             break;
 
@@ -12823,7 +12822,8 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
                 if (!scsi)
                     goto cleanup;
 
-                tmpPath = (char *)virSCSIDeviceGetPath(scsi);
+                if (VIR_STRDUP(tmpPath, virSCSIDeviceGetPath(scsi)) < 0)
+                    goto cleanup;
                 perm = virSCSIDeviceGetReadonly(scsi) ?
                     VIR_CGROUP_DEVICE_READ : VIR_CGROUP_DEVICE_RW;
             }
@@ -12835,7 +12835,8 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
                 if (!(host = virSCSIVHostDeviceNew(hostsrc->wwpn)))
                     goto cleanup;
 
-                tmpPath = (char *)virSCSIVHostDeviceGetPath(host);
+                if (VIR_STRDUP(tmpPath, virSCSIVHostDeviceGetPath(host)) < 0)
+                    goto cleanup;
                 perm = VIR_CGROUP_DEVICE_RW;
             }
             break;
@@ -12845,7 +12846,6 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
             if (!(tmpPath = virMediatedDeviceGetIOMMUGroupDev(mdevsrc->uuidstr)))
                 goto cleanup;
 
-            freeTmpPath = true;
             includeVFIO = true;
             perm = VIR_CGROUP_DEVICE_RW;
             break;
@@ -12895,8 +12895,7 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
     virUSBDeviceFree(usb);
     virSCSIDeviceFree(scsi);
     virSCSIVHostDeviceFree(host);
-    if (freeTmpPath)
-        VIR_FREE(tmpPath);
+    VIR_FREE(tmpPath);
     return ret;
 }
 

commit 9d6e343865e26ba7b9c2d89d0364b3000c6a935f

Remove VIR_STRDUP usage that snuck in
Fixes: 224d269f19f0a6c496dd2218f934a54742d51708

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Message-Id: <32cff73d2a51c273608d73fe8ae7a6fe15232502.1573578043.git.jtomko@redhat.com>

src/qemu/qemu_domain.c
@@ -12843,8 +12843,7 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
             if (!usb)
                 goto cleanup;
 
-            if (VIR_STRDUP(tmpPath, virUSBDeviceGetPath(usb)) < 0)
-                goto cleanup;
+            tmpPath = g_strdup(virUSBDeviceGetPath(usb));
             perm = VIR_CGROUP_DEVICE_RW;
             break;
 
@@ -12865,8 +12864,7 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
                 if (!scsi)
                     goto cleanup;
 
-                if (VIR_STRDUP(tmpPath, virSCSIDeviceGetPath(scsi)) < 0)
-                    goto cleanup;
+                tmpPath = g_strdup(virSCSIDeviceGetPath(scsi));
                 perm = virSCSIDeviceGetReadonly(scsi) ?
                     VIR_CGROUP_DEVICE_READ : VIR_CGROUP_DEVICE_RW;
             }
@@ -12878,8 +12876,7 @@ qemuDomainGetHostdevPath(virDomainDefPtr def,
                 if (!(host = virSCSIVHostDeviceNew(hostsrc->wwpn)))
                     goto cleanup;
 
-                if (VIR_STRDUP(tmpPath, virSCSIVHostDeviceGetPath(host)) < 0)
-                    goto cleanup;
+                tmpPath = g_strdup(virSCSIVHostDeviceGetPath(host));
                 perm = VIR_CGROUP_DEVICE_RW;
             }
             break;