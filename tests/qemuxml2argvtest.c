f5b673d01b78d7bc6d73b10d564be284a9cc4d72
Daniel Henrique Barboza <danielhb413@gmail.com>
2019-11-21 19:19:16 -0300

    virhostdev.c: check all IOMMU devs in virHostdevPreparePCIDevices
    virHostdevPreparePCIDevices verifies if a PCI hostdev "A" is
    in use by another domain by checking the 'activePCIHostdevs'
    list. It also verifies if other domains are using any other
    hostdev that belongs to the same IOMMU of "A".
    
    This is not enough to cover all the cases. Suppose a PCI hostdev
    "B" that shares the IOMMU with "A". We are currently able to abort
    guest launch if "B" is being used by another domain, but if "B" is
    not being used by any domain, we'll proceed guest execution. What
    happens then is: if "A" is being used in managed mode, the guest
    will fail to launch because Libvirt didn't detach "B" from the
    host. If "A" is being used in un-managed mode, the guest might fail
    or succeed to launch depending on whether both "A" and "B" were
    detached manually prior to guest start.
    
    Libvirt is now able to deal with these scenarios in a homogeneous
    fashion, providing the same behavior for both managed and
    un-managed mode while allowing parcial assignment for both cases as
    well. This patch changes virHostdevPreparePCIDevices to check all
    the PCI hostdevs of the domain against all the PCI hostdevs of the
    IOMMU, failing to launch if the domain does not contain all the
    PCI hostdevs declared, in both managed and un-managed cases.
    
    After this patch, any existing domains that were using PCI hostdevs
    with un-managed mode, and were getting away with partial assignment
    without declaring all the IOMMU PCI hostdevs in the domain XML, will
    be forced to do so. The missing PCI hostdevs can be declared with
    "<address type='none'/>" to keep them invisible to the guest, making
    the guest oblivious of this change. This is an annoyance for such
    domains, but the payoff is more consistency of behavior between
    managed and un-managed mode and more clarity on the domain XML,
    forcing the admin to declare all PCI hostdevs of the IOMMU and
    knowing exactly what will be detached from the host.
    
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20191121221917.15969-4-danielhb413@gmail.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -1330,6 +1330,10 @@ mymain(void)
             QEMU_CAPS_KVM,
             QEMU_CAPS_DEVICE_VFIO_PCI);
 
+    DO_TEST("hostdev-pci-multifunction-partial-fail",
+            QEMU_CAPS_KVM,
+            QEMU_CAPS_DEVICE_VFIO_PCI);
+
     DO_TEST("serial-file-log",
             QEMU_CAPS_CHARDEV_FILE_APPEND,
             QEMU_CAPS_DEVICE_ISA_SERIAL,
