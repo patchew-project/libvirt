bde8cd62926f8eb4e02f75d899bb172e6cfc1dc2
Peter Krempa <pkrempa@redhat.com>
2019-12-03 18:17:35 +0100

    Add 'backup' block job type
    A backup job may consist of many backup sub-blockjobs. Add the new
    blockjob type and add all type converter strings.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <93c801f8310554606ddbac84dfd2919864945c19.1575392645.git.pkrempa@redhat.com>

--- src/qemu/qemu_blockjob.c
+++ src/qemu/qemu_blockjob.c
@@ -65,6 +65,7 @@ VIR_ENUM_IMPL(qemuBlockjob,
               "copy",
               "commit",
               "active-commit",
+              "backup",
               "",
               "create",
               "broken");
@@ -1276,6 +1277,8 @@ qemuBlockJobEventProcessConcludedTransition(qemuBlockJobDataPtr job,
             qemuBlockJobProcessEventConcludedCopyAbort(driver, vm, job, asyncJob);
         break;
 
+    case QEMU_BLOCKJOB_TYPE_BACKUP:
+        break;
 
     case QEMU_BLOCKJOB_TYPE_BROKEN:
     case QEMU_BLOCKJOB_TYPE_NONE:
