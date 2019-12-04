2296f59e14f5846ffcd6c9968d8b6853100418a0
Daniel P. Berrangé <berrange@redhat.com>
2019-12-04 14:20:56 +0000

    conf: drop virCapsPtr param from post parse data alloc callback
    The only user of this callback did not require the virCapsPtr parameter.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20191204142113.2961028-14-berrange@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -5498,7 +5498,7 @@ virDomainDeviceDefPostParseOne(virDomainDeviceDefPtr dev,
     int ret;
 
     if (!parseOpaque && xmlopt->config.domainPostParseDataAlloc) {
-        if (xmlopt->config.domainPostParseDataAlloc(def, caps, flags,
+        if (xmlopt->config.domainPostParseDataAlloc(def, flags,
                                                     xmlopt->config.priv,
                                                     &data) < 0)
             return -1;
@@ -5833,7 +5833,7 @@ virDomainDefPostParse(virDomainDefPtr def,
 
     if (!data.parseOpaque &&
         xmlopt->config.domainPostParseDataAlloc) {
-        ret = xmlopt->config.domainPostParseDataAlloc(def, caps, parseFlags,
+        ret = xmlopt->config.domainPostParseDataAlloc(def, parseFlags,
                                                       xmlopt->config.priv,
                                                       &data.parseOpaque);
 
