f0b6b251ccc9c053e23407a41bf7311537141484
Michael Weiser <michael.weiser@gmx.de>
2019-12-30 19:07:36 +0100

    qemu: Warn of restore with managed save being risky
    Internal snapshots of a non-running domain do not carry any memory state
    and restoring such a snapshot will not replace existing saved memory
    state. This allows a scenario, where a user first suspends a domain into
    managedsave, restores a non-running snapshot and then resumes the domain
    from managedsave. After that, the guest system will run with its
    previous memory state atop a different disk state. The most obvious
    possible fallout from this is extensive file system corruption. Swap
    content and RAID bitmaps might also be off.
    
    This has been discussed[1] and fixed[2] from the end-user perspective for
    virt-manager.
    
    This patch marks the restore operation as risky at the libvirt level,
    requiring the user to remove the saved memory state first or force the
    operation.
    
    [1] https://www.redhat.com/archives/virt-tools-list/2019-November/msg00011.html
    [2] https://www.redhat.com/archives/virt-tools-list/2019-December/msg00049.html
    
    Signed-off-by: Michael Weiser <michael.weiser@gmx.de>
    Cc: Cole Robinson <crobinso@redhat.com>
    Message-Id: <20191230180736.3203497-1-michael.weiser@gmx.de>
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -16652,6 +16652,15 @@ qemuDomainRevertToSnapshot(virDomainSnapshotPtr snapshot,
                            _("must respawn qemu to start inactive snapshot"));
             goto endjob;
         }
+        if (vm->hasManagedSave &&
+            !(snapdef->state == VIR_DOMAIN_SNAPSHOT_RUNNING ||
+              snapdef->state == VIR_DOMAIN_SNAPSHOT_PAUSED)) {
+            virReportError(VIR_ERR_SNAPSHOT_REVERT_RISKY, "%s",
+                           _("revert to snapshot while there is a managed "
+                             "saved state will cause corruption when run, "
+                             "remove saved state first"));
+            goto endjob;
+        }
     }
 
     if (snap->def->dom) {
