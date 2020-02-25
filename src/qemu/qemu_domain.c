7c589ea590780ed630d0b4037c150030b0b7efc6
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-02-25 10:55:09 +0100

    domain: save/restore the state of dbus-daemon running
    This avoids trying to start a dbus-daemon when its already running.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20200225095513.969053-6-marcandre.lureau@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -2883,6 +2883,9 @@ qemuDomainObjPrivateXMLFormat(virBufferPtr buf,
                           virDomainChrTypeToString(priv->monConfig->type));
     }
 
+    if (priv->dbusDaemonRunning)
+        virBufferAddLit(buf, "<dbusDaemon/>\n");
+
     if (priv->namespaces) {
         ssize_t ns = -1;
 
@@ -3634,6 +3637,8 @@ qemuDomainObjPrivateXMLParse(xmlXPathContextPtr ctxt,
                        _("failed to parse agent timeout"));
         goto error;
     }
+
+    priv->dbusDaemonRunning = virXPathBoolean("boolean(./dbusDaemon)", ctxt) > 0;
 
     if ((node = virXPathNode("./namespaces", ctxt))) {
         xmlNodePtr next;
