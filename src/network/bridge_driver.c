patchew-project/libvirt

commit 17f430eb5cfaaa3388077f2b0856f011f0b2a4c3

qemu: test if bandwidth has 'floor' factored out to separate function
This compound condition will be useful in several places so it
makes sense to give it a name for better readability.

Signed-off-by: Pavel Mores <pmores@redhat.com>
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/network/bridge_driver.c
@@ -5065,7 +5065,7 @@ networkCheckBandwidth(virNetworkObjPtr obj,
 
     virMacAddrFormat(ifaceMac, ifmac);
 
-    if (ifaceBand && ifaceBand->in && ifaceBand->in->floor &&
+    if (virNetDevBandwidthHasFloor(ifaceBand) &&
         !(netBand && netBand->in)) {
         virReportError(VIR_ERR_OPERATION_UNSUPPORTED,
                        _("Invalid use of 'floor' on interface with MAC "
@@ -5079,8 +5079,9 @@ networkCheckBandwidth(virNetworkObjPtr obj,
         /* no QoS required, claim success */
         return 1;
     }
-    if (((!ifaceBand || !ifaceBand->in || !ifaceBand->in->floor) &&
-         (!oldBandwidth || !oldBandwidth->in || !oldBandwidth->in->floor))) {
+    if (!virNetDevBandwidthHasFloor(ifaceBand) &&
+        !virNetDevBandwidthHasFloor(oldBandwidth)) {
+
         VIR_DEBUG("No old/new interface bandwidth floor");
         /* no QoS required, claim success */
         return 1;

src/conf/netdev_bandwidth_conf.c
@@ -297,3 +297,10 @@ virDomainClearNetBandwidth(virDomainObjPtr vm)
             virNetDevBandwidthClear(vm->def->nets[i]->ifname);
     }
 }
+
+
+bool
+virNetDevBandwidthHasFloor(const virNetDevBandwidth *b)
+{
+    return b && b->in && b->in->floor != 0;
+}

src/conf/netdev_bandwidth_conf.h
@@ -57,3 +57,6 @@ static inline bool virNetDevSupportBandwidth(virDomainNetType type)
     }
     return false;
 }
+
+
+bool virNetDevBandwidthHasFloor(const virNetDevBandwidth *b);

src/libvirt_private.syms
@@ -733,6 +733,7 @@ virInterfaceDefParseString;
 # conf/netdev_bandwidth_conf.h
 virDomainClearNetBandwidth;
 virNetDevBandwidthFormat;
+virNetDevBandwidthHasFloor;
 virNetDevBandwidthParse;
 
 

commit e5c0b1be580a63a70279fd32965cfdedfd3d5c8e

bridge: include netdev_bandwidth_conf.h
This file uses the virNetDevBandwidth*Floor helpers
without including the correct include,
relying on virnetworkportdef.h to include it.

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Fixes: 17f430eb5cfaaa3388077f2b0856f011f0b2a4c3
Message-Id: <252034300f2710b843410d8442740fca545d74cb.1582389117.git.jtomko@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/network/bridge_driver.c
@@ -69,6 +69,8 @@
 #include "virnetworkportdef.h"
 #include "virutil.h"
 
+#include "netdev_bandwidth_conf.h"
+
 #define VIR_FROM_THIS VIR_FROM_NETWORK
 #define MAX_BRIDGE_ID 256
 