bd8c48d516d4a5373c740ca37f06aca5ef499ee9
Gaurav Agrawal <agrawalgaurav@gnome.org>
2020-03-12 23:47:25 +0530

    qemu: convert DomainLogContext class to use GObject
    Message-Id: <20200312181726.6547-1-agrawalgaurav@gnome.org>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -1930,7 +1930,7 @@ static void
 qemuProcessMonitorLogFree(void *opaque)
 {
     qemuDomainLogContextPtr logCtxt = opaque;
-    virObjectUnref(logCtxt);
+    g_clear_object(&logCtxt);
 }
 
 
@@ -1984,7 +1984,7 @@ qemuConnectMonitor(virQEMUDriverPtr driver, virDomainObjPtr vm, int asyncJob,
                           driver);
 
     if (mon && logCtxt) {
-        virObjectRef(logCtxt);
+        g_object_ref(logCtxt);
         qemuMonitorSetDomainLog(mon,
                                 qemuProcessMonitorReportLogError,
                                 logCtxt,
