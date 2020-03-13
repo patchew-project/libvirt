patchew-project/libvirt

commit 45464db8ba502764cf37ec9335770248bdb3d9a8

conf: do not generate machine names ending with a dash
As of systemd commit:

commit d65652f1f21a4b0c59711320f34266c635393c89
Author:     Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
CommitDate: 2018-12-10 09:56:56 +0100

    Partially unify hostname_is_valid() and dns_name_is_valid()

Dashes are no longer allowed at the end of machine names.

Trim the trailing dashes from the generated name before passing
it to machined.

https://bugzilla.redhat.com/show_bug.cgi?id=1790409

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Erik Skultety <eskultet@redhat.com>

src/conf/domain_conf.c
@@ -30472,6 +30472,9 @@ virDomainMachineNameAppendValid(virBufferPtr buf,
 
         virBufferAddChar(buf, *name);
     }
+
+    /* trailing dashes are not allowed */
+    virBufferTrimChars(buf, "-");
 }
 
 #undef HOSTNAME_CHARS

tests/virsystemdtest.c
@@ -740,6 +740,10 @@ mymain(void)
                  "qemu-7-123456789012345678901234567890123456789012345678901234567");
     TEST_MACHINE("123456789012345678901234567890123456789012345678901234567890", 8,
                  "qemu-8-123456789012345678901234567890123456789012345678901234567");
+    TEST_MACHINE("kstest-network-device-default-httpks_(c9eed63e-981e-48ec-acdc-56b3f8c5f678)", 100,
+                 "qemu-100-kstest-network-device-default-httpksc9eed63e-981e-48ec");
+    TEST_MACHINE("kstest-network-device-default-httpks_(c9eed63e-981e-48ec--cdc-56b3f8c5f678)", 10,
+                 "qemu-10-kstest-network-device-default-httpksc9eed63e-981e-48ec");
 
 # define TESTS_PM_SUPPORT_HELPER(name, function) \
     do { \

commit 44b8fd833c7ae658b3a5d8dbde8d3282b9278ee7

conf: Don't generate machine names with a dot
According to the linked BZ, machined expects either valid
hostname or valid FQDN (see systemd commit
v239-3092-gd65652f1f2). While in case of multiple dots, a
trailing one doesn't violate FQDN, it does violate the rule in
case of something simple, like "domain.". But it's safe to remove
it in both cases.

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1808499
Fixes: 45464db8ba502764cf37ec9335770248bdb3d9a8

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Message-Id: <5348817cbd0e972efd495648f7374d6171212ee4.1584022628.git.mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/conf/domain_conf.c
@@ -30933,7 +30933,7 @@ virDomainMachineNameAppendValid(virBufferPtr buf,
         if (strlen(virBufferCurrentContent(buf)) >= 64)
             break;
 
-        if (*name == '.') {
+        if (*name == '.' || *name == '-') {
             if (!skip_dot)
                 virBufferAddChar(buf, *name);
             skip_dot = true;
@@ -30948,8 +30948,8 @@ virDomainMachineNameAppendValid(virBufferPtr buf,
         virBufferAddChar(buf, *name);
     }
 
-    /* trailing dashes are not allowed */
-    virBufferTrimChars(buf, "-");
+    /* trailing dashes or dots are not allowed */
+    virBufferTrimChars(buf, "-.");
 }
 
 #undef HOSTNAME_CHARS

tests/virsystemdtest.c
@@ -746,7 +746,8 @@ mymain(void)
     TEST_MACHINE("kstest-network-device-default-httpks_(c9eed63e-981e-48ec-acdc-56b3f8c5f678)", 100,
                  "qemu-100-kstest-network-device-default-httpksc9eed63e-981e-48ec");
     TEST_MACHINE("kstest-network-device-default-httpks_(c9eed63e-981e-48ec--cdc-56b3f8c5f678)", 10,
-                 "qemu-10-kstest-network-device-default-httpksc9eed63e-981e-48ec");
+                 "qemu-10-kstest-network-device-default-httpksc9eed63e-981e-48ec-c");
+    TEST_MACHINE("demo.-.test.", 11, "qemu-11-demo.test");
 
 # define TESTS_PM_SUPPORT_HELPER(name, function) \
     do { \