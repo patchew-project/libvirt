patchew-project/libvirt

commit 756b46ddd243c3293f7d8193916f0c048d8d6347

storage: encryption: Add deep copy function for storage encryption

src/util/virstorageencryption.c
@@ -67,6 +67,46 @@ virStorageEncryptionFree(virStorageEncryptionPtr enc)
 }
 
 static virStorageEncryptionSecretPtr
+virStorageEncryptionSecretCopy(const virStorageEncryptionSecret *src)
+{
+    virStorageEncryptionSecretPtr ret;
+
+    if (VIR_ALLOC(ret) < 0)
+        return NULL;
+
+    memcpy(ret, src, sizeof(*src));
+
+    return ret;
+}
+
+virStorageEncryptionPtr
+virStorageEncryptionCopy(const virStorageEncryption *src)
+{
+    virStorageEncryptionPtr ret;
+    size_t i;
+
+    if (VIR_ALLOC(ret) < 0)
+        return NULL;
+
+    if (VIR_ALLOC_N(ret->secrets, src->nsecrets) < 0)
+        goto error;
+
+    ret->nsecrets = src->nsecrets;
+    ret->format = src->format;
+
+    for (i = 0; i < src->nsecrets; i++) {
+        if (!(ret->secrets[i] = virStorageEncryptionSecretCopy(src->secrets[i])))
+            goto error;
+    }
+
+    return ret;
+
+ error:
+    virStorageEncryptionFree(ret);
+    return NULL;
+}
+
+static virStorageEncryptionSecretPtr
 virStorageEncryptionSecretParse(xmlXPathContextPtr ctxt,
                                 xmlNodePtr node)
 {

src/util/virstorageencryption.h
@@ -61,6 +61,9 @@ struct _virStorageEncryption {
     virStorageEncryptionSecretPtr *secrets;
 };
 
+virStorageEncryptionPtr virStorageEncryptionCopy(const virStorageEncryption *src)
+    ATTRIBUTE_NONNULL(1);
+
 void virStorageEncryptionFree(virStorageEncryptionPtr enc);
 
 virStorageEncryptionPtr virStorageEncryptionParseNode(xmlDocPtr xml,

commit 7f149f357089b097ea7d7a404d090f6bf326f158

virStorageEncryptionSecretCopy: Properly copy internals
virStorageEncryptionSecretPtr may have a string inside it, thus we must
copy the string too. Use virSecretLookupDefCopy to do that.

Likely caused by 756b46ddd24.

https://bugzilla.redhat.com/show_bug.cgi?id=1814923

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Message-Id: <ad3c915ae5012059efaa3014c5fd3f75e5de165e.1584636370.git.pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/util/virstorageencryption.c
@@ -85,12 +85,10 @@ virStorageEncryptionFree(virStorageEncryptionPtr enc)
 static virStorageEncryptionSecretPtr
 virStorageEncryptionSecretCopy(const virStorageEncryptionSecret *src)
 {
-    virStorageEncryptionSecretPtr ret;
+    virStorageEncryptionSecretPtr ret = g_new0(virStorageEncryptionSecret, 1);
 
-    if (VIR_ALLOC(ret) < 0)
-        return NULL;
-
-    memcpy(ret, src, sizeof(*src));
+    ret->type = src->type;
+    virSecretLookupDefCopy(&ret->seclookupdef, &src->seclookupdef);
 
     return ret;
 }