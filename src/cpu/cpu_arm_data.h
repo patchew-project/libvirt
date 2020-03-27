5727a523971e2a905c56758f46c165411bdcfa9f
Zhenyu Zheng <zheng.zhenyu@foxmail.com>
2020-03-27 16:52:19 +0800

    cpu: Introduce virCPUarmData to virCPUData
    Introduce virCPUarmData to virCPUData
    
    Signed-off-by: Zhenyu Zheng <zhengzhenyulixi@gmail.com>
    Message-Id: <20200327085220.1811-3-zheng.zhenyu@foxmail.com>

---
+++ src/cpu/cpu_arm_data.h
@@ -0,0 +1,32 @@
+/*
+ * cpu_arm_data.h: 64-bit arm CPU specific data
+ *
+ * Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library;  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+
+#define VIR_CPU_ARM_DATA_INIT { 0 }
+
+typedef struct _virCPUarmData virCPUarmData;
+struct _virCPUarmData {
+    char *vendor_id;
+    char *model_name;
+    char *features;
+};
