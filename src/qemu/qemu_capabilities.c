f76b60ec08b8eb067cf50bf58d44c056262b5ef1
Tobin Feldman-Fitzthum <tobin@linux.vnet.ibm.com>
2020-03-27 20:22:26 -0400

    use new functions when probing
    Probe for TCG with QMP right after probing for KVM.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@linux.vnet.ibm.com>
    Message-Id: <20200328002227.36555-5-tobin@linux.vnet.ibm.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -5046,12 +5046,10 @@ virQEMUCapsInitQMPMonitor(virQEMUCapsPtr qemuCaps,
     /* Some capabilities may differ depending on KVM state */
     if (virQEMUCapsProbeQMPKVMState(qemuCaps, mon) < 0)
         return -1;
+    if (virQEMUCapsProbeQMPTCGState(qemuCaps, mon) < 0)
+        return -1;
 
-    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_KVM))
-        type = VIR_DOMAIN_VIRT_KVM;
-    else
-        type = VIR_DOMAIN_VIRT_QEMU;
-
+    type = virQEMUCapsGetVirtType(qemuCaps);
     accel = virQEMUCapsGetAccel(qemuCaps, type);
 
     if (virQEMUCapsProbeQMPEvents(qemuCaps, mon) < 0)
@@ -5544,10 +5542,7 @@ virQEMUCapsCacheLookupDefault(virFileCachePtr cache,
         goto cleanup;
     }
 
-    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_KVM))
-        capsType = VIR_DOMAIN_VIRT_KVM;
-    else
-        capsType = VIR_DOMAIN_VIRT_QEMU;
+    capsType = virQEMUCapsGetVirtType(qemuCaps);
 
     if (virttype == VIR_DOMAIN_VIRT_NONE)
         virttype = capsType;
