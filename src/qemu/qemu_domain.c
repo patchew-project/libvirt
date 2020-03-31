77f02bd7f65620749d4cf4d8cc957f51b42b4ea7
Michal Privoznik <mprivozn@redhat.com>
2020-03-31 18:24:16 +0200

    conf: Move virDomainGenerateMachineName to hypervisor/
    The virDomainGenerateMachineName() function doesn't belong in
    src/conf/ really, because it has nothing to do with domain XML
    parsing. It landed there because of lack of better place in the
    past. But now that we have src/hypervisor/ the function should
    live there. At the same time, the function name is changed to
    match new location.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <26958b0c7e09be68f55c00156bbb4d3549f0aedb.1585671623.git.mprivozn@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -44,6 +44,7 @@
 #include "virfile.h"
 #include "domain_addr.h"
 #include "domain_capabilities.h"
+#include "domain_driver.h"
 #include "domain_event.h"
 #include "virtime.h"
 #include "virnetdevopenvswitch.h"
@@ -16544,9 +16545,9 @@ qemuDomainGetMachineName(virDomainObjPtr vm)
     }
 
     if (!ret)
-        ret = virDomainGenerateMachineName("qemu", cfg->root,
-                                           vm->def->id, vm->def->name,
-                                           driver->privileged);
+        ret = virDomainDriverGenerateMachineName("qemu", cfg->root,
+                                                 vm->def->id, vm->def->name,
+                                                 driver->privileged);
 
     return ret;
 }
