patchew-project/libvirt

commit bb4f3543bbf3ebbffa833ae7df55c298920243eb

qemu: command: Format rerror/werror with -device instead of -drive
Use the new proper location for the read/write error policy selection.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_command.c
@@ -1667,7 +1667,8 @@ qemuBuildDriveStr(virDomainDiskDefPtr disk,
 
     /* werror/rerror are really frontend attributes, but older
      * qemu requires them on -drive instead of -device */
-    qemuBuildDiskFrontendAttributeErrorPolicy(disk, &opt);
+    if (!virQEMUCapsGet(qemuCaps, QEMU_CAPS_USB_STORAGE_WERROR))
+        qemuBuildDiskFrontendAttributeErrorPolicy(disk, &opt);
 
 
     /* While this is a frontend attribute, it only makes sense to be used when
@@ -2128,6 +2129,9 @@ qemuBuildDiskDeviceStr(const virDomainDef *def,
         goto error;
 
     qemuBuildDiskFrontendAttributes(disk, &opt);
+
+    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_USB_STORAGE_WERROR))
+        qemuBuildDiskFrontendAttributeErrorPolicy(disk, &opt);
 
     if (virBufferCheckError(&opt) < 0)
         goto error;

tests/qemuxml2argvdata/disk-drive-error-policy.x86_64-latest.args
@@ -25,17 +25,17 @@ file=/tmp/lib/domain--1-QEMUGuest1/master-key.aes \
 -boot strict=on \
 -device piix3-usb-uhci,id=usb,bus=pci.0,addr=0x1.0x2 \
 -drive file=/dev/HostVG/QEMUGuest1,format=qcow2,if=none,id=drive-ide0-0-0,\
-werror=stop,rerror=stop,cache=none \
+cache=none \
 -device ide-hd,bus=ide.0,unit=0,drive=drive-ide0-0-0,id=ide0-0-0,bootindex=1,\
-write-cache=on \
+write-cache=on,werror=stop,rerror=stop \
 -drive file=/dev/HostVG/QEMUGuest2,format=qcow2,if=none,id=drive-ide0-0-1,\
-werror=enospc,cache=none \
+cache=none \
 -device ide-hd,bus=ide.0,unit=1,drive=drive-ide0-0-1,id=ide0-0-1,\
-write-cache=on \
+write-cache=on,werror=enospc \
 -drive file=/dev/HostVG/QEMUGuest3,format=qcow2,if=none,id=drive-ide0-1-0,\
-werror=report,rerror=ignore,cache=none \
+cache=none \
 -device ide-hd,bus=ide.1,unit=0,drive=drive-ide0-1-0,id=ide0-1-0,\
-write-cache=on \
+write-cache=on,werror=report,rerror=ignore \
 -sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,\
 resourcecontrol=deny \
 -msg timestamp=on

commit be07f895a7349301aa25b5ffa7836edd93d33064

qemu: use QEMU_CAPS_STORAGE_WERROR for disk-error attributes
When moving the formatting of this attributes from -drive
to -device, the QEMU_CAPS_USB_STORAGE_WERROR capability
was used, because usb-storage was the last device to gain
this capability.

However this lead to the assumption that QEMU binaries
without the usb-storage device do not support this,
leading to breakage on s390x with blockdev.

Fixes: bb4f3543bbf3ebbffa833ae7df55c298920243eb
https://bugzilla.redhat.com/show_bug.cgi?id=1819250

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Message-Id: <615595fdbebdb6c71b9f11bc975acae69772bfe4.1585733685.git.jtomko@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/qemu/qemu_command.c
@@ -1790,7 +1790,7 @@ qemuBuildDriveStr(virDomainDiskDefPtr disk,
 
     /* werror/rerror are really frontend attributes, but older
      * qemu requires them on -drive instead of -device */
-    if (!virQEMUCapsGet(qemuCaps, QEMU_CAPS_USB_STORAGE_WERROR))
+    if (!virQEMUCapsGet(qemuCaps, QEMU_CAPS_STORAGE_WERROR))
         qemuBuildDiskFrontendAttributeErrorPolicy(disk, &opt);
 
     if (disk->src->readonly)
@@ -2241,7 +2241,7 @@ qemuBuildDiskDeviceStr(const virDomainDef *def,
 
     qemuBuildDiskFrontendAttributes(disk, &opt);
 
-    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_USB_STORAGE_WERROR))
+    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_STORAGE_WERROR))
         qemuBuildDiskFrontendAttributeErrorPolicy(disk, &opt);
 
     return virBufferContentAndReset(&opt);

tests/qemuxml2argvdata/disk-error-policy-s390x.s390x-2.12.0.args
@@ -26,17 +26,17 @@ file=/tmp/lib/domain--1-guest/master-key.aes \
 -no-shutdown \
 -boot strict=on \
 -drive file=/var/images/image1,format=qcow2,if=none,id=drive-virtio-disk0,\
-werror=stop,rerror=stop,cache=none \
+cache=none \
 -device virtio-blk-ccw,scsi=off,devno=fe.0.0000,drive=drive-virtio-disk0,\
-id=virtio-disk0,bootindex=1,write-cache=on \
+id=virtio-disk0,bootindex=1,write-cache=on,werror=stop,rerror=stop \
 -drive file=/var/images/image2,format=qcow2,if=none,id=drive-virtio-disk1,\
-werror=enospc,cache=none \
+cache=none \
 -device virtio-blk-ccw,scsi=off,devno=fe.0.0001,drive=drive-virtio-disk1,\
-id=virtio-disk1,write-cache=on \
+id=virtio-disk1,write-cache=on,werror=enospc \
 -drive file=/var/images/image3,format=qcow2,if=none,id=drive-virtio-disk2,\
-werror=report,rerror=ignore,cache=none \
+cache=none \
 -device virtio-blk-ccw,scsi=off,devno=fe.0.0002,drive=drive-virtio-disk2,\
-id=virtio-disk2,write-cache=on \
+id=virtio-disk2,write-cache=on,werror=report,rerror=ignore \
 -sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,\
 resourcecontrol=deny \
 -msg timestamp=on

tests/qemuxml2argvdata/disk-error-policy-s390x.s390x-latest.args
@@ -27,17 +27,17 @@ file=/tmp/lib/domain--1-guest/master-key.aes \
 -no-shutdown \
 -boot strict=on \
 -drive file=/var/images/image1,format=qcow2,if=none,id=drive-virtio-disk0,\
-werror=stop,rerror=stop,cache=none \
+cache=none \
 -device virtio-blk-ccw,scsi=off,devno=fe.0.0000,drive=drive-virtio-disk0,\
-id=virtio-disk0,bootindex=1,write-cache=on \
+id=virtio-disk0,bootindex=1,write-cache=on,werror=stop,rerror=stop \
 -drive file=/var/images/image2,format=qcow2,if=none,id=drive-virtio-disk1,\
-werror=enospc,cache=none \
+cache=none \
 -device virtio-blk-ccw,scsi=off,devno=fe.0.0001,drive=drive-virtio-disk1,\
-id=virtio-disk1,write-cache=on \
+id=virtio-disk1,write-cache=on,werror=enospc \
 -drive file=/var/images/image3,format=qcow2,if=none,id=drive-virtio-disk2,\
-werror=report,rerror=ignore,cache=none \
+cache=none \
 -device virtio-blk-ccw,scsi=off,devno=fe.0.0002,drive=drive-virtio-disk2,\
-id=virtio-disk2,write-cache=on \
+id=virtio-disk2,write-cache=on,werror=report,rerror=ignore \
 -sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,\
 resourcecontrol=deny \
 -msg timestamp=on