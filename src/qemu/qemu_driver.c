patchew-project/libvirt

commit 2ace7a87a8aced68c2504fd4dd4e2df4302c3eeb

qemuDomainSnapshotDiskPrepareOne: Don't load the relative path with blockdev
Since we are refreshing the relative paths when doing the blockjobs we
no longer need to load them upfront when doing the snapshot.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_driver.c
@@ -15167,8 +15167,9 @@ qemuDomainSnapshotDiskPrepareOne(virQEMUDriverPtr driver,
         dd->initialized = true;
 
         /* relative backing store paths need to be updated so that relative
-         * block commit still works */
-        if (reuse) {
+         * block commit still works. With blockdev we must update it when doing
+         * commit anyways so it's skipped here */
+        if (reuse && !blockdev) {
             if (supportsBacking) {
                 g_autofree char *backingStoreStr = NULL;
 

commit 710bcd21cee80f463af69905db3c2b51da0a00eb

qemuDomainSnapshotDiskPrepareOne: Fix logic of relative backing store update
Commit 2ace7a87a8aced68c250 introduced a logic bug by an improperly
modified condition where we'd skip to the else branch when reusing of
external images was requested and blockdev is available.

The original intentions were to skip the backing store update with
blockdev.

Fix it by only asserting the boolean which was used to track whether we
support update of the backing store only when blockdev is not present
along with the appropriate rename.

https://bugzilla.redhat.com/show_bug.cgi?id=1820016

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Message-Id: <1c247857ad77862ad4e370fbb0e7b313bc94d174.1585806242.git.pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_driver.c
@@ -15129,7 +15129,7 @@ qemuDomainSnapshotDiskPrepareOne(virQEMUDriverPtr driver,
 {
     virDomainDiskDefPtr persistdisk;
     bool supportsCreate;
-    bool supportsBacking;
+    bool updateRelativeBacking = false;
 
     dd->disk = disk;
 
@@ -15158,19 +15158,22 @@ qemuDomainSnapshotDiskPrepareOne(virQEMUDriverPtr driver,
     }
 
     supportsCreate = virStorageFileSupportsCreate(dd->src);
-    supportsBacking = virStorageFileSupportsBackingChainTraversal(dd->src);
 
-    if (supportsCreate || supportsBacking) {
+    /* relative backing store paths need to be updated so that relative
+     * block commit still works. With blockdev we must update it when doing
+     * commit anyways so it's skipped here */
+    if (!blockdev &&
+        virStorageFileSupportsBackingChainTraversal(dd->src))
+        updateRelativeBacking = true;
+
+    if (supportsCreate || updateRelativeBacking) {
         if (qemuDomainStorageFileInit(driver, vm, dd->src, NULL) < 0)
             return -1;
 
         dd->initialized = true;
 
-        /* relative backing store paths need to be updated so that relative
-         * block commit still works. With blockdev we must update it when doing
-         * commit anyways so it's skipped here */
-        if (reuse && !blockdev) {
-            if (supportsBacking) {
+        if (reuse) {
+            if (updateRelativeBacking) {
                 g_autofree char *backingStoreStr = NULL;
 
                 if (virStorageFileGetBackingStoreStr(dd->src, &backingStoreStr) < 0)