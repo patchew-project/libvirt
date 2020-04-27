be5344b1d1d1c69859b12c5df89b5d3039a7b0b4
Han Han <hhan@redhat.com>
2020-04-27 10:01:06 +0800

    qemu_capabilities: Introduce iSER transport flag
    Introduce the flag QEMU_CAPS_ISCSI_TRANSPORT_ISER for iSER(iSCSI
    Extensions for RDMA) transport of iscsi backend.
    
    Signed-off-by: Han Han <hhan@redhat.com>
    Message-Id: <20200427020110.174406-2-hhan@redhat.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -574,6 +574,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
               "virtio.packed",
               "pcie-root-port.hotplug",
               "aio.io_uring",
+              "iscsi.transport.iser",
     );
 
 
@@ -1463,6 +1464,7 @@ static struct virQEMUCapsStringFlags virQEMUCapsQMPSchemaQueries[] = {
     { "query-named-block-nodes/arg-type/flat", QEMU_CAPS_QMP_QUERY_NAMED_BLOCK_NODES_FLAT },
     { "blockdev-snapshot/$allow-write-only-overlay", QEMU_CAPS_BLOCKDEV_SNAPSHOT_ALLOW_WRITE_ONLY },
     { "blockdev-add/arg-type/+file/aio/^io_uring", QEMU_CAPS_AIO_IO_URING },
+    { "blockdev-add/arg-type/+iscsi/transport/^iser", QEMU_CAPS_ISCSI_TRANSPORT_ISER },
 };
 
 typedef struct _virQEMUCapsObjectTypeProps virQEMUCapsObjectTypeProps;
