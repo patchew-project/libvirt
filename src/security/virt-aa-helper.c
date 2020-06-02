977c9c8b77e756ca54cfadcc486b17a91c7c0843
Daniel Henrique Barboza <danielhb413@gmail.com>
2020-06-02 10:31:01 -0300

    qemu_tpm, security, tests: change 'switch' clauses for 'if'
    This trivial rework is aimed to reduce the amount of line changes
    made by the next patch, when 'def->tpm' will become a 'def->tpms'
    array.
    
    Instead of using a 'switch' where only the VIR_DOMAIN_TPM_TYPE_EMULATOR
    label does something, use an 'if' clause instead.
    
    Tested-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20200602133107.1987182-5-danielhb413@gmail.com>

--- src/security/virt-aa-helper.c
+++ src/security/virt-aa-helper.c
@@ -1210,8 +1210,7 @@ get_files(vahControl * ctl)
         char *shortName = NULL;
         const char *tpmpath = NULL;
 
-        switch (ctl->def->tpm->type) {
-        case VIR_DOMAIN_TPM_TYPE_EMULATOR:
+        if (ctl->def->tpm->type == VIR_DOMAIN_TPM_TYPE_EMULATOR) {
             shortName = virDomainDefGetShortName(ctl->def);
 
             switch (ctl->def->tpm->version) {
@@ -1244,10 +1243,6 @@ get_files(vahControl * ctl)
                 RUNSTATEDIR, shortName);
 
             VIR_FREE(shortName);
-            break;
-        case VIR_DOMAIN_TPM_TYPE_PASSTHROUGH:
-        case VIR_DOMAIN_TPM_TYPE_LAST:
-            break;
         }
     }
 
