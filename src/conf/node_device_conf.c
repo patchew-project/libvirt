5401434600a6539699cb9f78d1c70f5630ec9c9a
Jonathon Jongsma <jjongsma@redhat.com>
2020-06-16 09:27:53 -0500

    nodedev: store mdev UUID in mdev caps
    In order to allow libvirt to create and start new mediated devices, we
    need to be able to verify that the device has been started. In order to
    do this, we'll need to save the UUID of newly-discovered devices within
    the virNodeDevCapMdev structure. This allows us to search the device
    list by UUID and verify whether the expected device has been started.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>
    Message-Id: <20200616142759.4625-6-jjongsma@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/node_device_conf.c
+++ src/conf/node_device_conf.c
@@ -2214,6 +2214,7 @@ virNodeDevCapsDefFree(virNodeDevCapsDefPtr caps)
         break;
     case VIR_NODE_DEV_CAP_MDEV:
         VIR_FREE(data->mdev.type);
+        VIR_FREE(data->mdev.uuid);
         for (i = 0; i < data->mdev.nattributes; i++)
             virMediatedDeviceAttrFree(data->mdev.attributes[i]);
         VIR_FREE(data->mdev.attributes);
