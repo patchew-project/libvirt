feaeff5f5e8772921f6924bedb8aa3780e9ae8d6
Laine Stump <laine@redhat.com>
2020-06-24 23:34:11 -0400

    nwfilter: convert remaining VIR_FREE() to g_free()
    Signed-off-by: Laine Stump <laine@redhat.com>
    Message-Id: <20200625033414.1819594-23-laine@redhat.com>

--- src/nwfilter/nwfilter_ebiptables_driver.c
+++ src/nwfilter/nwfilter_ebiptables_driver.c
@@ -3518,7 +3518,7 @@ ebiptablesApplyNewRules(const char *ifname,
 
  cleanup:
     for (i = 0; i < nsubchains; i++)
-        VIR_FREE(subchains[i]);
+        g_free(subchains[i]);
 
     return ret;
 }
