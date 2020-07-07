patchew-project/libvirt

commit 7fa7f7eeb6e969e002845928e155914da2fc8cd0

util: add access check for hooks to fix running as non-root
Since feb83c1e710b9ea8044a89346f4868d03b31b0f1 libvirtd will abort on
startup if run as non-root

  2020-07-01 16:30:30.738+0000: 1647444: error : virDirOpenInternal:2869 : cannot open directory '/etc/libvirt/hooks/daemon.d': Permission denied

The root cause flaw is that non-root libvirtd is using /etc/libvirt for
its hooks. Traditionally that has been harmless though since we checked
whether we could access the hook file and degraded gracefully. We need
the same access check for iterating over the hook directory.

Long term we should make it possible to have an unprivileged hook dir
under $HOME.

Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/util/virhook.c
@@ -171,6 +171,12 @@ virHookCheck(int no, const char *driver)
     }
 
     dir_path = g_strdup_printf("%s.d", path);
+
+    if (!virFileIsExecutable(dir_path)) {
+        VIR_DEBUG("Hook dir %s is not accessible", dir_path);
+        return 1;
+    }
+
     if ((ret = virDirOpenIfExists(&dir, dir_path)) < 0)
         return -1;
 
@@ -415,6 +421,10 @@ virHookCall(int driver,
     }
 
     dir_path = g_strdup_printf("%s.d", path);
+
+    if (!virFileIsExecutable(dir_path))
+        return script_ret;
+
     if ((ret = virDirOpenIfExists(&dir, dir_path)) < 0)
         return -1;
 

commit 911fe8a6fbf666070ef92533f3ad949b749cb6f7

util: fix accessibility check for hook directory
virFileIsAccessible does not return true on accessible
directories. Check whether it set EISDIR and only
then assume the directory is inaccessible.

Return 0 (not found) instead of 1 (found),
since the bridge driver taints the network based on
this return value, not whether the hook actually ran.

Remove the bogus check from virHookCall, since it already
checks the virHooksFound bitmap that was filled before
by virHookCheck.

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Fixes: 7fa7f7eeb6e969e002845928e155914da2fc8cd0
Closes: https://gitlab.com/libvirt/libvirt/-/issues/47
Message-Id: <a94ea6b47ecf71bd8af1784350867c404187bbc1.1594124053.git.jtomko@redhat.com>
Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>

src/util/virhook.c
@@ -172,9 +172,9 @@ virHookCheck(int no, const char *driver)
 
     dir_path = g_strdup_printf("%s.d", path);
 
-    if (!virFileIsExecutable(dir_path)) {
+    if (!virFileIsExecutable(dir_path) && errno != EISDIR) {
         VIR_DEBUG("Hook dir %s is not accessible", dir_path);
-        return 1;
+        return 0;
     }
 
     if ((ret = virDirOpenIfExists(&dir, dir_path)) < 0)
@@ -421,9 +421,6 @@ virHookCall(int driver,
     }
 
     dir_path = g_strdup_printf("%s.d", path);
-
-    if (!virFileIsExecutable(dir_path))
-        return script_ret;
 
     if ((ret = virDirOpenIfExists(&dir, dir_path)) < 0)
         return -1;