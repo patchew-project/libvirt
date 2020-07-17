patchew-project/libvirt

commit 5331c4804f4f419b9e75741084f926e52413d3a1

qemu: fix missing error reports in capabilities probing
The "virsh domcapabilities --arch ppc64" command will fail with no
error message set if qemu-system-ppc64 is not currently installed.

This is because virQEMUCapsCacheLookup() does not report any error
message if not capabilities can be obtained from the cache. Almost
all methods calling this expected an error to be set on failure.

Once that's fixed though, we see a further bug which is that
virQEMUCapsCacheLookupDefault() is passing a NULL binary path to
virQEMUCapsCacheLookup(), so we need to catch that too.

Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/qemu/qemu_domain.c
@@ -6131,8 +6131,10 @@ qemuDomainPostParseDataAlloc(const virDomainDef *def,
     virQEMUDriverPtr driver = opaque;
 
     if (!(*parseOpaque = virQEMUCapsCacheLookup(driver->qemuCapsCache,
-                                                def->emulator)))
+                                                def->emulator))) {
+        virResetLastError();
         return 1;
+    }
 
     return 0;
 }

src/qemu/qemu_capabilities.c
@@ -5634,6 +5634,11 @@ virQEMUCapsCacheLookup(virFileCachePtr cache,
     priv->microcodeVersion = virHostCPUGetMicrocodeVersion();
 
     ret = virFileCacheLookup(cache, binary);
+    if (!ret) {
+        virReportError(VIR_ERR_INTERNAL_ERROR,
+                       _("no capabilities available for %s"), binary);
+        return NULL;
+    }
 
     VIR_DEBUG("Returning caps %p for %s", ret, binary);
     return ret;
@@ -5779,6 +5784,12 @@ virQEMUCapsCacheLookupDefault(virFileCachePtr cache,
     if (!binary) {
         probedbinary = virQEMUCapsGetDefaultEmulator(hostarch, arch);
         binary = probedbinary;
+    }
+    if (!binary) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                       _("unable to find any emulator to serve '%s' architecture"),
+                       archStr);
+        return NULL;
     }
 
     if (!(qemuCaps = virQEMUCapsCacheLookup(cache, binary)))

commit 43203e9f9896ec370003151091fb8033da417cba

qemuDomainPostParseDataAlloc: Don't reset error if looking up caps fails
When starting the QEMU driver we load all domain XMLs. This
means, that post parse callbacks are run for each XML, but they
are allowed to fail because we will run them again when starting
a domain. In the case I am fixing, we were unable to look up QEMU
capabilities (in both post parse runs) and reported appropriate
error, sort of. It can be found in the logs, but the caller
doesn't get it because after 5331c4804f4 it is reset. Therefore,
as reported here [1], if we are unable to start QEMU for caps
probing the following will happen:

  virsh start fedora
  error: Failed to start domain fedora
  error: An error occurred, but the cause is unknown

1: https://www.redhat.com/archives/libvir-list/2020-July/msg00673.html

Fixes: 5331c4804f4

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Message-Id: <cec1794b91d19e5ea0d8c5217f9a7746bf3377d7.1594970874.git.mprivozn@redhat.com>

src/qemu/qemu_domain.c
@@ -5307,7 +5307,6 @@ qemuDomainPostParseDataAlloc(const virDomainDef *def,
 
     if (!(*parseOpaque = virQEMUCapsCacheLookup(driver->qemuCapsCache,
                                                 def->emulator))) {
-        virResetLastError();
         return 1;
     }
 