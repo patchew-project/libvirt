patchew-project/libvirt

commit d942bf6e9e8e3991808ca5185098257e84acab5d

logical: Increase the size of the data to wipe
Since a sector size may be larger than 512 bytes, let's just increase
the size to wipe to 1MB rather than 2KB

src/storage/storage_backend_logical.c
@@ -100,7 +100,7 @@ virStorageBackendLogicalInitializeDevice(const char *path)
      * a whole disk as a PV. So we just blank them out regardless
      * rather than trying to figure out if we're a disk or partition
      */
-    if (virStorageBackendZeroPartitionTable(path, 4 * PV_BLANK_SECTOR_SIZE) < 0)
+    if (virStorageBackendZeroPartitionTable(path, 1024 * 1024) < 0)
         return -1;
 
     /*

commit bd42c2e5df96952b269a87beb37153478009b185

storage: logical: drop unused PV_BLANK_SECTOR_SIZE
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Fixes: d942bf6e9e8e3991808ca5185098257e84acab5d
Message-Id: <866643c139d39fef9c77661a4ba7617def0cd936.1595937055.git.jtomko@redhat.com>
Reviewed-by: Jonathon Jongsma <jjongsma@redhat.com>

src/storage/storage_backend_logical.c
@@ -40,8 +40,6 @@
 
 VIR_LOG_INIT("storage.storage_backend_logical");
 
-#define PV_BLANK_SECTOR_SIZE 512
-
 
 static int
 virStorageBackendLogicalSetActive(virStoragePoolObjPtr pool,