patchew-project/libvirt

commit bab9257a649386a908069ba890a23f2a5ff5b131

qemu: capabilities: Add QEMU_CAPS_BLOCKDEV_HOSTDEV_SCSI
We want to instantiate hostdevs via -blockdev too. Add a separate
capability for them for a clean transition. The new capability will be
enabled when QEMU_CAPS_BLOCKDEV is present once all code is prepared.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_capabilities.c
@@ -596,6 +596,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
               "intel-iommu.aw-bits",
               "spapr-tpm-proxy",
               "numa.hmat",
+              "blockdev-hostdev-scsi"
     );
 
 

src/qemu/qemu_capabilities.h
@@ -576,6 +576,7 @@ typedef enum { /* virQEMUCapsFlags grouping marker for syntax-check */
     QEMU_CAPS_INTEL_IOMMU_AW_BITS, /* intel-iommu.aw-bits */
     QEMU_CAPS_DEVICE_SPAPR_TPM_PROXY, /* -device spapr-tpm-proxy */
     QEMU_CAPS_NUMA_HMAT, /* -numa hmat */
+    QEMU_CAPS_BLOCKDEV_HOSTDEV_SCSI, /* -blockdev used for (i)SCSI hostdevs */
 
     QEMU_CAPS_LAST /* this must always be the last item */
 } virQEMUCapsFlags;

commit cfc4485a4590a71cef9a44a638d23baf4ed19864

qemu: capabilities: add missing comma
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Fixes: bab9257a649386a908069ba890a23f2a5ff5b131
Message-Id: <587a32672e539e0c120df4a7f488bbc4a839fd76.1596475010.git.jtomko@redhat.com>

src/qemu/qemu_capabilities.c
@@ -596,7 +596,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
               "intel-iommu.aw-bits",
               "spapr-tpm-proxy",
               "numa.hmat",
-              "blockdev-hostdev-scsi"
+              "blockdev-hostdev-scsi",
     );
 
 