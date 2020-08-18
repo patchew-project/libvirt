patchew-project/libvirt

commit f0611fe8830543d03d1871422f8c542453f0c8db

conf: Validate NUMA HMAT configuration
There are several restrictions, for instance @initiator and
@target have to refer to existing NUMA nodes (daa), @cache has to
refer to a defined cache level and so on.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>

src/conf/numa_conf.c
@@ -1363,6 +1363,105 @@ virDomainNumaDefFormatXML(virBufferPtr buf,
 }
 
 
+int
+virDomainNumaDefValidate(const virDomainNuma *def)
+{
+    size_t i;
+    size_t j;
+
+    if (!def)
+        return 0;
+
+    for (i = 0; i < def->nmem_nodes; i++) {
+        const virDomainNumaNode *node = &def->mem_nodes[i];
+        g_autoptr(virBitmap) levelsSeen = virBitmapNewEmpty();
+
+        for (j = 0; j < node->ncaches; j++) {
+            const virDomainNumaCache *cache = &node->caches[j];
+
+            /* Relax this if there's ever fourth layer of cache */
+            if (cache->level > 3) {
+                virReportError(VIR_ERR_XML_ERROR, "%s",
+                               _("Ain't nobody heard of that much cache level"));
+                return -1;
+            }
+
+            if (virBitmapIsBitSet(levelsSeen, cache->level)) {
+                virReportError(VIR_ERR_XML_ERROR,
+                               _("Cache level '%u' already defined"),
+                               cache->level);
+                return -1;
+            }
+
+            if (virBitmapSetBitExpand(levelsSeen, cache->level))
+                return -1;
+        }
+    }
+
+    for (i = 0; i < def->ninterconnects; i++) {
+        const virDomainNumaInterconnect *l = &def->interconnects[i];
+
+        if (l->initiator >= def->nmem_nodes) {
+            virReportError(VIR_ERR_XML_ERROR, "%s",
+                           _("'initiator' refers to a non-existent NUMA node"));
+            return -1;
+        }
+
+        if (l->target >= def->nmem_nodes) {
+            virReportError(VIR_ERR_XML_ERROR, "%s",
+                           _("'target' refers to a non-existent NUMA node"));
+            return -1;
+        }
+
+        if (!def->mem_nodes[l->initiator].cpumask) {
+            virReportError(VIR_ERR_XML_ERROR, "%s",
+                           _("NUMA nodes without CPUs can't be initiator"));
+            return -1;
+        }
+
+        if (l->cache > 0) {
+            for (j = 0; j < def->mem_nodes[l->target].ncaches; j++) {
+                const virDomainNumaCache *cache = def->mem_nodes[l->target].caches;
+
+                if (l->cache == cache->level)
+                    break;
+            }
+
+            if (j == def->mem_nodes[l->target].ncaches) {
+                virReportError(VIR_ERR_XML_ERROR, "%s",
+                               _("'cache' refers to a non-existent NUMA node cache"));
+                return -1;
+            }
+        }
+
+        for (j = 0; j < i; j++) {
+            const virDomainNumaInterconnect *ll = &def->interconnects[j];
+
+            if (l->type == ll->type &&
+                l->initiator == ll->initiator &&
+                l->target == ll->target &&
+                l->cache == ll->cache &&
+                l->accessType == ll->accessType) {
+                virReportError(VIR_ERR_XML_ERROR, "%s",
+                               _("Duplicate info for NUMA latencies"));
+                return -1;
+            }
+
+
+            if (l->initiator != l->target &&
+                l->initiator == ll->target &&
+                l->target == ll->initiator) {
+                virReportError(VIR_ERR_XML_ERROR, "%s",
+                               _("Link already defined"));
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
 unsigned int
 virDomainNumaGetCPUCountTotal(virDomainNumaPtr numa)
 {

src/conf/domain_conf.c
@@ -7321,6 +7321,9 @@ virDomainDefValidateInternal(const virDomainDef *def,
     if (virDomainDefCputuneValidate(def) < 0)
         return -1;
 
+    if (virDomainNumaDefValidate(def->numa) < 0)
+        return -1;
+
     return 0;
 }
 

src/conf/numa_conf.h
@@ -216,6 +216,7 @@ bool virDomainNumatuneNodeSpecified(virDomainNumaPtr numatune,
 
 int virDomainNumaDefParseXML(virDomainNumaPtr def, xmlXPathContextPtr ctxt);
 int virDomainNumaDefFormatXML(virBufferPtr buf, virDomainNumaPtr def);
+int virDomainNumaDefValidate(const virDomainNuma *def);
 
 unsigned int virDomainNumaGetCPUCountTotal(virDomainNumaPtr numa);
 

commit 7aef383f96216b67fbe36e09766c5f7cf3ed7f00

numa_conf: Properly check for caches in virDomainNumaDefValidate()
When adding support for HMAT, in f0611fe8830 I've introduced a
check which aims to validate /domain/cpu/numa/interconnects. As a
part of that, there is a loop which checks whether all <latency/>
with @cache attribute refer to an existing cache level. For
instance:

  <cpu mode='host-model' check='partial'>
    <numa>
      <cell id='0' cpus='0-5' memory='512000' unit='KiB' discard='yes'>
        <cache level='1' associativity='direct' policy='writeback'>
          <size value='8' unit='KiB'/>
          <line value='5' unit='B'/>
        </cache>
      </cell>
      <interconnects>
        <latency initiator='0' target='0' cache='1' type='access' value='5'/>
        <bandwidth initiator='0' target='0' type='access' value='204800' unit='KiB'/>
      </interconnects>
    </numa>
  </cpu>

This XML defines that accessing L1 cache of node #0 from node #0
has latency of 5ns.

However, the loop was not written properly. Well, the check in
it, as it was always checking for the first cache in the target
node and not the rest. Therefore, the following example errors
out:

  <cpu mode='host-model' check='partial'>
    <numa>
      <cell id='0' cpus='0-5' memory='512000' unit='KiB' discard='yes'>
        <cache level='3' associativity='direct' policy='writeback'>
          <size value='10' unit='KiB'/>
          <line value='8' unit='B'/>
        </cache>
        <cache level='1' associativity='direct' policy='writeback'>
          <size value='8' unit='KiB'/>
          <line value='5' unit='B'/>
        </cache>
      </cell>
      <interconnects>
        <latency initiator='0' target='0' cache='1' type='access' value='5'/>
        <bandwidth initiator='0' target='0' type='access' value='204800' unit='KiB'/>
      </interconnects>
    </numa>
  </cpu>

This errors out even though it is a valid configuration. The L1
cache under node #0 is still present.

Fixes: f0611fe8830

src/conf/numa_conf.c
@@ -1421,7 +1421,7 @@ virDomainNumaDefValidate(const virDomainNuma *def)
 
         if (l->cache > 0) {
             for (j = 0; j < def->mem_nodes[l->target].ncaches; j++) {
-                const virDomainNumaCache *cache = def->mem_nodes[l->target].caches;
+                const virDomainNumaCache *cache = &def->mem_nodes[l->target].caches[j];
 
                 if (l->cache == cache->level)
                     break;