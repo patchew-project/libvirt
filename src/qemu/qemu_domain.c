328d161186c531d7907a0ef28d2b86c480cf22fc
Masayoshi Mizuma <m.mizuma@jp.fujitsu.com>
2020-08-28 10:08:34 -0400

    qemu: Block blockjobs when transient disk option is enabled
    Block blockjobs when transient disk option is enabled so far.
    
    Signed-off-by: Masayoshi Mizuma <m.mizuma@jp.fujitsu.com>
    Message-Id: <20200828140836.14266-6-msys.mizuma@gmail.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -10782,6 +10782,13 @@ qemuDomainDiskBlockJobIsSupported(virDomainObjPtr vm,
         return false;
     }
 
+    if (disk->transient) {
+        virReportError(VIR_ERR_OPERATION_UNSUPPORTED,
+                       _("block jobs are not supported on transient disk '%s'"),
+                       disk->dst);
+        return false;
+    }
+
     return true;
 }
 
