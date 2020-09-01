patchew-project/libvirt

commit 7f3eb533f44742071366e07a35ab41070956707b

src: conditionalize use of net/if.h
The net/if.h is not portable so we must check for its
existance and avoid using it when missing. Some use
of net/if.h was redundant and could be removed.

Reviewed-by: Pavel Hrdina <phrdina@redhat.com>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/util/virnetdevbridge.c
@@ -31,7 +31,9 @@
 # include <sys/ioctl.h>
 #endif
 #include <sys/socket.h>
-#include <net/if.h>
+#ifdef HAVE_NET_IF
+# include <net/if.h>
+#endif
 #include <netinet/in.h>
 
 #ifdef __linux__

src/util/virnetdevtap.c
@@ -36,7 +36,9 @@
 #ifndef WIN32
 # include <sys/ioctl.h>
 #endif
-#include <net/if.h>
+#ifdef HAVE_NET_IF
+# include <net/if.h>
+#endif
 #include <fcntl.h>
 #ifdef __linux__
 # include <linux/if_tun.h>    /* IFF_TUN, IFF_NO_PI */

build-aux/syntax-check.mk
@@ -2338,4 +2338,4 @@ exclude_file_name_regexp--sc_prohibit_backslash_alignment = \
   ^build-aux/syntax-check\.mk$$
 
 exclude_file_name_regexp--sc_prohibit_always_true_header_tests = \
-  ^src/util/virfile\.c$$
+  ^src/util/(virfile|virnetdev|virnetdevip)\.[c,h]|$$

configure.ac
@@ -381,6 +381,7 @@ AC_CHECK_HEADERS([\
   mntent.h \
   net/ethernet.h \
   netinet/tcp.h \
+  net/if.h \
   pty.h \
   pwd.h \
   stdarg.h \

src/util/virnetdev.c
@@ -34,7 +34,6 @@
 #ifndef WIN32
 # include <sys/ioctl.h>
 #endif
-#include <net/if.h>
 #include <fcntl.h>
 
 #ifdef __linux__

src/util/virnetdev.h
@@ -18,7 +18,9 @@
 
 #pragma once
 
-#include <net/if.h>
+#ifdef HAVE_NET_IF_H
+# include <net/if.h>
+#endif
 
 #include "virbitmap.h"
 #include "virsocketaddr.h"

src/util/virnetdevip.c
@@ -36,7 +36,9 @@
 #ifndef WIN32
 # include <sys/ioctl.h>
 #endif
-#include <net/if.h>
+#ifdef HAVE_NET_IF_H
+# include <net/if.h>
+#endif
 #include <fcntl.h>
 
 #ifdef __linux__

commit 78e6c50a49c94b6ac690113793ca55ffd1c11f79

util: Check for HAVE_NET_IF_H correctly
There are two places where we try to check whether the host
system has net/if.h before including it. But the check is missing
'_H' suffix.

Fixes: 7f3eb533f44742071366e07a35ab41070956707b
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Message-Id: <ca9cda7f1274ca74a3d10c321d83b4d68985feda.1598968735.git.mprivozn@redhat.com>

src/util/virnetdevbridge.c
@@ -27,7 +27,7 @@
 #include "virstring.h"
 #include "virsocket.h"
 
-#ifdef HAVE_NET_IF
+#ifdef HAVE_NET_IF_H
 # include <net/if.h>
 #endif
 

src/util/virnetdevtap.c
@@ -36,7 +36,7 @@
 #ifndef WIN32
 # include <sys/ioctl.h>
 #endif
-#ifdef HAVE_NET_IF
+#ifdef HAVE_NET_IF_H
 # include <net/if.h>
 #endif
 #include <fcntl.h>