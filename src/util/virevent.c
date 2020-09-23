patchew-project/libvirt

commit 2e07a1e14635ad25c57b66c13488feff4c8d2b0c

event: add API for requiring an event loop impl to be registered
Reviewed-by: Cole Robinson <crobinso@redhat.com>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/util/virevent.c
@@ -26,6 +26,7 @@
 #include "virlog.h"
 #include "virerror.h"
 
+#define VIR_FROM_THIS VIR_FROM_EVENT
 
 VIR_LOG_INIT("util.event");
 
@@ -251,6 +252,27 @@ void virEventRegisterImpl(virEventAddHandleFunc addHandle,
     removeTimeoutImpl = removeTimeout;
 }
 
+
+/**
+ * virEventRequireImpl:
+ *
+ * Require that there is an event loop implementation
+ * registered.
+ *
+ * Returns: -1 if no event loop is registered, 0 otherwise
+ */
+int virEventRequireImpl(void)
+{
+    if (!addHandleImpl || !addTimeoutImpl) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                       _("An event loop implementation must be registered"));
+        return -1;
+    }
+
+    return 0;
+}
+
+
 /**
  * virEventRegisterDefaultImpl:
  *
@@ -275,6 +297,8 @@ void virEventRegisterImpl(virEventAddHandleFunc addHandle,
 int virEventRegisterDefaultImpl(void)
 {
     VIR_DEBUG("registering default event implementation");
+
+    virInitialize();
 
     virResetLastError();
 

po/POTFILES.in
@@ -240,6 +240,7 @@
 @SRCDIR@/src/util/virdnsmasq.c
 @SRCDIR@/src/util/virerror.c
 @SRCDIR@/src/util/virerror.h
+@SRCDIR@/src/util/virevent.c
 @SRCDIR@/src/util/vireventpoll.c
 @SRCDIR@/src/util/virfcp.c
 @SRCDIR@/src/util/virfdstream.c

src/util/virevent.h
@@ -21,3 +21,5 @@
 
 #pragma once
 #include "internal.h"
+
+int virEventRequireImpl(void);

commit f2cad16e857adc71976ada4acc09aa72f5b490e6

util: event: check return value of virInitialize
This function can possibly fail.

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Fixes: 2e07a1e14635ad25c57b66c13488feff4c8d2b0c
Message-Id: <da38a6c04d4f593b777dc33332953eb3b68299ec.1600810297.git.jtomko@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/util/virevent.c
@@ -305,7 +305,8 @@ int virEventRegisterDefaultImpl(void)
 {
     VIR_DEBUG("registering default event implementation");
 
-    virInitialize();
+    if (virInitialize() < 0)
+        return -1;
 
     virResetLastError();
 