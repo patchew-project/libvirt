6c8ab7581870c5e2f35df36578a98252942c3634
Brian Turek <brian.turek@gmail.com>
2020-10-02 11:55:01 +0100

    qemu: capabilities: add QEMU_CAPS_FSDEV_CREATEMODE
    The QEMU 9pfs 'fmode' and 'dmode' options exists since QEMU 2.10. Probe
    QEMU's command line set to check whether these options are really
    available, and if yes, enable this new QEMU_CAPS_FSDEV_CREATEMODE
    capability on libvirt side.
    
    Signed-off-by: Brian Turek <brian.turek@gmail.com>
    Message-Id: <20201002105504.82121-2-brian.turek@gmail.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -600,6 +600,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
 
               /* 380 */
               "usb-host.hostdevice",
+              "fsdev.createmode",
     );
 
 
@@ -3334,6 +3335,7 @@ static struct virQEMUCapsCommandLineProps virQEMUCapsCommandLine[] = {
     { "smp-opts", "dies", QEMU_CAPS_SMP_DIES },
     { "fsdev", "multidevs", QEMU_CAPS_FSDEV_MULTIDEVS },
     { "fw_cfg", "file", QEMU_CAPS_FW_CFG },
+    { "fsdev", "fmode", QEMU_CAPS_FSDEV_CREATEMODE }, /* Could have also checked fsdev->dmode */
 };
 
 static int
