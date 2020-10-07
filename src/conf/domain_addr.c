5eeae7e24e0731c9fb9445c3e9d193705218b30b
Ján Tomko <jtomko@redhat.com>
2020-10-07 21:22:29 +0200

    conf: virDomainUSBAddressHubNew: refactor
    Remove the pointless variable and pointer stealing.
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    Message-Id: <e1054aa02159abe7197e3e5132f0688cce2a0caa.1602098503.git.jtomko@redhat.com>
    Reviewed-by: Neal Gompa <ngompa13@gmail.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- src/conf/domain_addr.c
+++ src/conf/domain_addr.c
@@ -2029,7 +2029,7 @@ virDomainUSBAddressControllerModelToPorts(virDomainControllerDefPtr cont)
 static virDomainUSBAddressHubPtr
 virDomainUSBAddressHubNew(size_t nports)
 {
-    virDomainUSBAddressHubPtr hub = NULL, ret = NULL;
+    virDomainUSBAddressHubPtr hub;
 
     hub = g_new0(virDomainUSBAddressHub, 1);
 
@@ -2038,9 +2038,7 @@ virDomainUSBAddressHubNew(size_t nports)
     hub->ports = g_new0(virDomainUSBAddressHubPtr, nports);
     hub->nports = nports;
 
-    ret = g_steal_pointer(&hub);
-    virDomainUSBAddressHubFree(hub);
-    return ret;
+    return hub;
 }
 
 
