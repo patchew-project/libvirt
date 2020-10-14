eb8411f4fc09e24b501088966e14894215d43806
Jonathon Jongsma <jjongsma@redhat.com>
2020-10-14 12:08:27 -0500

    qemu: add vdpa support
    Enable <interface type='vdpa'> for qemu domains. This provides basic
    support and does not support hotplug or migration.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>
    Message-Id: <20201014170830.1496209-4-jjongsma@redhat.com>

--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -497,6 +497,7 @@ mymain(void)
     DO_TEST("net-mtu", NONE);
     DO_TEST("net-coalesce", NONE);
     DO_TEST("net-many-models", NONE);
+    DO_TEST("net-vdpa", QEMU_CAPS_NETDEV_VHOST_VDPA);
 
     DO_TEST("serial-tcp-tlsx509-chardev", NONE);
     DO_TEST("serial-tcp-tlsx509-chardev-notls", NONE);
