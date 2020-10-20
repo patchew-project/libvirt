b5400881eb2f0d370826cddaa4feb42ed51d4786
Daniel P. Berrangé <berrange@redhat.com>
2020-10-20 16:07:03 +0100

    qemu: honour fatal errors dealing with qemu slirp helper
    Currently all errors from qemuInterfacePrepareSlirp() are completely
    ignored by the callers. The intention is that missing qemu-slirp binary
    should cause the caller to fallback to the built-in slirp impl.
    
    Many of the possible errors though should indeed be considered fatal.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20201020150703.365650-1-berrange@redhat.com>

--- src/qemu/qemu_interface.h
+++ src/qemu/qemu_interface.h
@@ -56,5 +56,6 @@ int qemuInterfaceOpenVhostNet(virDomainDefPtr def,
                               int *vhostfd,
                               size_t *vhostfdSize) G_GNUC_NO_INLINE;
 
-qemuSlirpPtr qemuInterfacePrepareSlirp(virQEMUDriverPtr driver,
-                                       virDomainNetDefPtr net);
+int qemuInterfacePrepareSlirp(virQEMUDriverPtr driver,
+                              virDomainNetDefPtr net,
+                              qemuSlirpPtr *slirp);
