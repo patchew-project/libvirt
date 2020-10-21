45044698d7c3645b84592fd4f86439a1f48e2437
Jonathon Jongsma <jjongsma@redhat.com>
2020-10-21 16:50:06 -0500

    qemu: fix potential resource leak
    Coverity reported a potential resource leak. While it's probably not
    a real-world scenario, the code could technically jump to cleanup
    between the time that vdpafd is opened and when it is used. Ensure that
    it gets cleaned up in that case.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>
    Message-Id: <20201021215006.1936108-1-jjongsma@redhat.com>
    Reviewed-by: Laine Stump <laine@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -8135,6 +8135,7 @@ qemuBuildInterfaceCommandLine(virQEMUDriverPtr driver,
         addfdarg = g_strdup_printf("%s,opaque=%s", fdset,
                                    net->data.vdpa.devicepath);
         virCommandAddArgList(cmd, "-add-fd", addfdarg, NULL);
+        vdpafd = -1;
     }
 
     if (chardev)
@@ -8204,6 +8205,8 @@ qemuBuildInterfaceCommandLine(virQEMUDriverPtr driver,
     VIR_FREE(tapfdName);
     VIR_FREE(vhostfd);
     VIR_FREE(tapfd);
+    if (vdpafd >= 0)
+        VIR_FORCE_CLOSE(vdpafd);
     return ret;
 }
 
