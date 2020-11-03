4f3b1bfaef8db147ab4b264b8c04ccece1f24b39
Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
2020-11-03 15:00:04 +0300

    qemu: rename: support renaming checkpoints directory
    This is basically just saves checkpoints metadata on disk after name is changed
    in memory as path to domain checkpoints directory depends on name. After that
    old checkpoint directory is deleted with checkpoint metadata files.
    
    Signed-off-by: Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
    Message-Id: <1604404808-825155-7-git-send-email-nshirokovskiy@virtuozzo.com>
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -19126,6 +19126,24 @@ qemuDomainSnapshotWriteMetadataIter(void *payload,
 
 
 static int
+qemuDomainCheckpointWriteMetadataIter(void *payload,
+                                      const char *name G_GNUC_UNUSED,
+                                      void *opaque)
+{
+    struct qemuDomainMomentWriteMetadataData *data = opaque;
+    virQEMUDriverConfigPtr cfg =  virQEMUDriverGetConfig(data->driver);
+    int ret;
+
+    ret = qemuCheckpointWriteMetadata(data->vm, payload,
+                                      data->driver->xmlopt,
+                                      cfg->snapshotDir);
+
+    virObjectUnref(cfg);
+    return ret;
+}
+
+
+static int
 qemuDomainRenameCallback(virDomainObjPtr vm,
                          const char *new_name,
                          unsigned int flags,
@@ -19189,6 +19207,11 @@ qemuDomainRenameCallback(virDomainObjPtr vm,
     if (virDomainSnapshotForEach(vm->snapshots,
                                  qemuDomainSnapshotWriteMetadataIter,
                                  &data) < 0)
+        goto cleanup;
+
+    if (virDomainCheckpointForEach(vm->checkpoints,
+                                   qemuDomainCheckpointWriteMetadataIter,
+                                   &data) < 0)
         goto cleanup;
 
     if (virDomainDefSave(vm->def, driver->xmlopt, cfg->configDir) < 0)
