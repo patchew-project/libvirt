patchew-project/libvirt

commit f8333b3b0a7fdbc1f18ed501c043ac7618b86a16

qemu: Fix domfsinfo for non-PCI device information from guest agent
qemuAgentFSInfoToPublic() currently only sets the devAlias for PCI devices.
However, the QEMU guest agent could also provide the device name in the
"dev" field of the response for other devices instead (well, at least after
fixing another problem in the current QEMU guest agent...). So if creating
the devAlias from the PCI information failed, let's fall back to the name
provided by the guest agent. This helps to fix the empty "Target" fields
that occur when running "virsh domfsinfo" on s390x where CCW devices are
used for the guest instead of PCI devices.

Also add a proper debug message here in case we completely failed to set the
device alias, since this problem here was very hard to debug: The only two
error messages that I've seen were "Unable to get filesystem information"
and "Unable to encode message payload" - which only indicates that something
went wrong in the RPC call. No debug message indicated the real problem, so
I had to learn the hard way why the RPC call failed (it apparently does not
like devAlias left to be NULL) and where the real problem comes from.

Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=1755075
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
Signed-off-by: Thomas Huth <thuth@redhat.com>

src/qemu/qemu_driver.c
@@ -18812,14 +18812,17 @@ qemuAgentFSInfoToPublic(qemuAgentFSInfoPtr agent,
         qemuAgentDiskInfoPtr agentdisk = agent->disks[i];
         virDomainDiskDefPtr diskDef;
 
-        if (!(diskDef = virDomainDiskByAddress(vmdef,
-                                               &agentdisk->pci_controller,
-                                               agentdisk->bus,
-                                               agentdisk->target,
-                                               agentdisk->unit)))
-            continue;
-
-        ret->devAlias[i] = g_strdup(diskDef->dst);
+        diskDef = virDomainDiskByAddress(vmdef,
+                                         &agentdisk->pci_controller,
+                                         agentdisk->bus,
+                                         agentdisk->target,
+                                         agentdisk->unit);
+        if (diskDef != NULL)
+            ret->devAlias[i] = g_strdup(diskDef->dst);
+        else if (agentdisk->devnode != NULL)
+            ret->devAlias[i] = g_strdup(agentdisk->devnode);
+        else
+            VIR_DEBUG("Missing devnode name for '%s'.", ret->mountpoint);
     }
 
     return ret;

commit f9b417c4efb62900dedf0dadfeb8b073e8e1c9fb

qemu: domfsinfo should print a target name on s390x, not a complete device path
While fixing domfsinfo for non-PCI (i.e. CCW) devices on s390x,
I accidentally used the whole device path for the devAlias field.
However, it should only contain the base target name.

Currently we have the wrong output:

 $ virsh domfsinfo guestname
  Mountpoint   Name   Type   Target
 ---------------------------------------
  /            sda3   xfs    /dev/sda3
  /boot        sda1   xfs    /dev/sda1

It should look like this instead:

 $ virsh domfsinfo guestname
  Mountpoint   Name   Type   Target
 ------------------------------------
  /            sda3   xfs    sda
  /boot        sda1   xfs    sda

Thus we have to strip the "/dev/" prefix and the partition number
from the string.

Fixes: f8333b3b0a ("qemu: Fix domfsinfo for non-PCI device information ...")
Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=1858771
Reported-by: Sebastian Mitterle <smitterl@redhat.com>
Message-Id: <20201112224802.784132-1-thuth@redhat.com>

src/qemu/qemu_driver.c
@@ -18874,6 +18874,30 @@ qemuDomainGetFSInfoAgent(virQEMUDriverPtr driver,
     return ret;
 }
 
+/* Turn device node string like "/dev/vda1" into a target name like "vda" */
+static char *
+qemuAgentDevNodeToTarget(const char *devnode)
+{
+    char *str = g_strdup(devnode);
+    size_t len = strlen(str);
+
+    /* Remove the "/dev/" prefix from the string */
+    if (g_str_has_prefix(str, "/dev/")) {
+        len -= 5;
+        memmove(str, str + 5, len + 1);
+    }
+
+    /* Remove the partition number from the end of the string */
+    while (len > 0) {
+        len--;
+        if (!g_ascii_isdigit(str[len]))
+            break;
+        str[len] = 0;
+    }
+
+    return str;
+}
+
 static virDomainFSInfoPtr
 qemuAgentFSInfoToPublic(qemuAgentFSInfoPtr agent,
                         virDomainDefPtr vmdef)
@@ -18903,7 +18927,7 @@ qemuAgentFSInfoToPublic(qemuAgentFSInfoPtr agent,
         if (diskDef != NULL)
             ret->devAlias[i] = g_strdup(diskDef->dst);
         else if (agentdisk->devnode != NULL)
-            ret->devAlias[i] = g_strdup(agentdisk->devnode);
+            ret->devAlias[i] = qemuAgentDevNodeToTarget(agentdisk->devnode);
         else
             VIR_DEBUG("Missing devnode name for '%s'.", ret->mountpoint);
     }