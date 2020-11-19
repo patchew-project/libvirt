patchew-project/libvirt

commit e3c626a61d6c3d808555653684c0fb1e7c4d74ec

openvz: fixed two memory leaks on migration code
The first one occurs in openvzDomainMigratePrepare3Params() where in
case no remote uri is given, the distant hostname is used. The name is
obtained via virGetHostname() which require callers to free the
returned value.
The second leak lies in openvzDomainMigratePerform3Params(). There's a
virCommand used later. However, at the beginning of the function
virCheckFlags() is called which returns. So the command created was
leaked.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

src/openvz/openvz_driver.c
@@ -2286,7 +2286,8 @@ openvzDomainMigratePrepare3Params(virConnectPtr dconn,
     const char *uri_in = NULL;
     virDomainDefPtr def = NULL;
     virDomainObjPtr vm = NULL;
-    char *hostname = NULL;
+    char *my_hostname = NULL;
+    const char *hostname = NULL;
     virURIPtr uri = NULL;
     int ret = -1;
 
@@ -2321,10 +2322,10 @@ openvzDomainMigratePrepare3Params(virConnectPtr dconn,
     def = NULL;
 
     if (!uri_in) {
-        if ((hostname = virGetHostname()) == NULL)
+        if ((my_hostname = virGetHostname()) == NULL)
             goto error;
 
-        if (STRPREFIX(hostname, "localhost")) {
+        if (STRPREFIX(my_hostname, "localhost")) {
             virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                            _("hostname on destination resolved to localhost,"
                              " but migration requires an FQDN"));
@@ -2364,6 +2365,7 @@ openvzDomainMigratePrepare3Params(virConnectPtr dconn,
     }
 
  done:
+    VIR_FREE(my_hostname);
     virURIFree(uri);
     if (vm)
         virObjectUnlock(vm);
@@ -2385,7 +2387,7 @@ openvzDomainMigratePerform3Params(virDomainPtr domain,
     virDomainObjPtr vm = NULL;
     const char *uri_str = NULL;
     virURIPtr uri = NULL;
-    virCommandPtr cmd = virCommandNew(VZMIGRATE);
+    virCommandPtr cmd = NULL;
     int ret = -1;
 
     virCheckFlags(OPENVZ_MIGRATION_FLAGS, -1);
@@ -2412,6 +2414,7 @@ openvzDomainMigratePerform3Params(virDomainPtr domain,
     if (uri == NULL || uri->server == NULL)
         goto cleanup;
 
+    cmd = virCommandNew(VZMIGRATE);
     if (flags & VIR_MIGRATE_LIVE)
         virCommandAddArg(cmd, "--live");
     virCommandAddArg(cmd, uri->server);

commit eb343adbf17607f24965ed8828ffa8919b05e53f

openvzDomainMigratePrepare3Params: correctly use hostname
In case no uri_in was supplied, we forgot to set the hostname
to the current hostname and formatted a useless uri_out.

src/util/glibcompat.h:57:26: error: ‘%s’ directive argument is null [-Werror=format-overflow=]
  57 | # define g_strdup_printf vir_g_strdup_printf
src/openvz/openvz_driver.c:2136:16: note: in expansion of macro ‘g_strdup_printf’
2136 |     *uri_out = g_strdup_printf("ssh://%s", hostname);

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reported-by: Jaroslav Suchanek <jsuchane@redhat.com>
Fixes: e3c626a61d6c3d808555653684c0fb1e7c4d74ec
Message-Id: <cb8f925648ca78e83271e0c2d28cde7ff217f5ce.1605781932.git.jtomko@redhat.com>
Reviewed-by: Erik Skultety <eskultet@redhat.com>

src/openvz/openvz_driver.c
@@ -2113,6 +2113,8 @@ openvzDomainMigratePrepare3Params(virConnectPtr dconn,
                              " but migration requires an FQDN"));
             goto error;
         }
+
+        hostname = my_hostname;
     } else {
         uri = virURIParse(uri_in);
 