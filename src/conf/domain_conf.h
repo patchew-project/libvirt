5caa6af8b7418fe65a9d2e41a99cd0d5ae0048a8
Thomas Huth <thuth@redhat.com>
2020-11-25 12:06:47 +0100

    domain_conf: Allow to look up virtio-block devices by their CCW address
    On s390x, devices are attached to the channel IO subsytem by default,
    so we need to look up the devices via their CCW address there instead
    of using PCI.
    
    This fixes "virsh domfsinfo" on s390x for virtio-block devices (the first
    attempt from commit f8333b3b0a7 did it in the wrong way, reporting the
    device name on the guest side instead of the target name on the host side).
    
    Fixes: f8333b3b0a ("qemu: Fix domfsinfo for non-PCI device information ...")
    Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=1858771
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20201125110648.380802-3-thuth@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -3342,10 +3342,12 @@ void virDomainRNGDefFree(virDomainRNGDefPtr def);
 
 int virDomainDiskIndexByAddress(virDomainDefPtr def,
                                 virPCIDeviceAddressPtr pci_controller,
+                                virDomainDeviceCCWAddressPtr ccw_addr,
                                 unsigned int bus, unsigned int target,
                                 unsigned int unit);
 virDomainDiskDefPtr virDomainDiskByAddress(virDomainDefPtr def,
                                            virPCIDeviceAddressPtr pci_controller,
+                                           virDomainDeviceCCWAddressPtr ccw_addr,
                                            unsigned int bus,
                                            unsigned int target,
                                            unsigned int unit);
