63081976828ba5efeafc92546454d955d409c5b4
Shalini Chellathurai Saroja <shalini@linux.ibm.com>
2020-12-03 18:59:42 +0100

    node_device: mdev matrix support
    Allow mdev devices to be created on the matrix device.
    
    Signed-off-by: Shalini Chellathurai Saroja <shalini@linux.ibm.com>
    Reviewed-by: Bjoern Walk <bwalk@linux.ibm.com>
    Reviewed-by: Boris Fiuczynski <fiuczy@linux.ibm.com>
    Message-Id: <20201203175944.3867894-11-shalini@linux.ibm.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- src/node_device/node_device_udev.c
+++ src/node_device/node_device_udev.c
@@ -1235,8 +1235,12 @@ udevProcessAPQueue(struct udev_device *device,
 
 
 static int
-udevProcessAPMatrix(virNodeDeviceDefPtr def)
+udevProcessAPMatrix(struct udev_device *device,
+                    virNodeDeviceDefPtr def)
 {
+    virNodeDevCapDataPtr data = &def->caps->data;
+
+    data->ap_matrix.addr =  g_strdup(udev_device_get_sysname(device));
     def->name = g_strdup("ap_matrix");
 
     return 0;
@@ -1383,7 +1387,7 @@ udevGetDeviceDetails(struct udev_device *device,
     case VIR_NODE_DEV_CAP_AP_QUEUE:
         return udevProcessAPQueue(device, def);
     case VIR_NODE_DEV_CAP_AP_MATRIX:
-        return udevProcessAPMatrix(def);
+        return udevProcessAPMatrix(device, def);
     case VIR_NODE_DEV_CAP_MDEV_TYPES:
     case VIR_NODE_DEV_CAP_SYSTEM:
     case VIR_NODE_DEV_CAP_FC_HOST:
