e6baf1ec12bb9c765cfe0a19704ded89d4149795
Michal Privoznik <mprivozn@redhat.com>
2020-12-03 13:36:16 +0100

    qemu_capabilities: Introduce QEMU_CAPS_DEVICE_VIRTIO_{P}MEM_PCI
    In this commit two new capabilities are introduced that reflect
    device support by give qemu:
    
    QEMU_CAPS_DEVICE_VIRTIO_MEM_PCI, /* -device virtio-mem-pci */
    QEMU_CAPS_DEVICE_VIRTIO_PMEM_PCI, /* -device virtio-pmem-pci */
    
    Please note, virtio-pmem device was introduced sooner (QEMU 4.1)
    than virtio-mem (QEMU 5.1).
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <843824ab4546292c81f186b652873cca0da76947.1606998426.git.mprivozn@redhat.com>
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Tested-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Tested-by: Han Han <hhan@redhat.com>

--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -589,6 +589,8 @@ typedef enum { /* virQEMUCapsFlags grouping marker for syntax-check */
     QEMU_CAPS_SCSI_NCR53C90, /* built-in SCSI */
     QEMU_CAPS_SCSI_DC390, /* -device dc-390 */
     QEMU_CAPS_SCSI_AM53C974, /* -device am53c974 */
+    QEMU_CAPS_DEVICE_VIRTIO_MEM_PCI, /* -device virtio-mem-pci */
+    QEMU_CAPS_DEVICE_VIRTIO_PMEM_PCI, /* -device virtio-pmem-pci */
 
     QEMU_CAPS_LAST /* this must always be the last item */
 } virQEMUCapsFlags;
