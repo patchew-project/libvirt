228139ea79244449e710295e2654b205f249a961
Daniel Henrique Barboza <danielhb413@gmail.com>
2020-12-08 19:20:18 -0300

    domain_conf.c: move primary video check to validate callback
    This check isn't exclusive to XML parsing. Let's move it to
    virDomainDefVideoValidate() in domain_validate.c
    
    We don't have a failure test for this scenario, so a new test called
    'video-multiple-primaries' was added to test this failure case.
    
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20201208222030.1302229-3-danielhb413@gmail.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -7348,6 +7348,9 @@ virDomainDefValidateInternal(const virDomainDef *def,
     if (virDomainDefBootValidate(def) < 0)
         return -1;
 
+    if (virDomainDefVideoValidate(def) < 0)
+        return -1;
+
     if (virDomainNumaDefValidate(def->numa) < 0)
         return -1;
 
@@ -22141,14 +22144,9 @@ virDomainDefParseXML(xmlDocPtr xml,
             goto error;
 
         if (video->primary) {
-            if (def->nvideos != 0 && def->videos[0]->primary) {
-                virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
-                               _("Only one primary video device is supported"));
-                goto error;
-            }
-
             insertAt = 0;
         }
+
         if (VIR_INSERT_ELEMENT_INPLACE(def->videos,
                                        insertAt,
                                        def->nvideos,
