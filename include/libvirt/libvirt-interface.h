ed163773a8666bb3bb949bc5043d60b7e008fd74
Jonathon Jongsma <jjongsma@redhat.com>
2021-01-20 11:33:45 -0600

    libvirt-interface.h: remove space-padded alignment
    Improve consistency in headers by removing the old style space-padded
    alignment for function names and arguments.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>
    Message-Id: <20210120173349.3785246-5-jjongsma@redhat.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- include/libvirt/libvirt-interface.h
+++ include/libvirt/libvirt-interface.h
@@ -42,17 +42,17 @@ typedef struct _virInterface virInterface;
  */
 typedef virInterface *virInterfacePtr;
 
-virConnectPtr           virInterfaceGetConnect    (virInterfacePtr iface);
+virConnectPtr virInterfaceGetConnect(virInterfacePtr iface);
 
-int                     virConnectNumOfInterfaces (virConnectPtr conn);
-int                     virConnectListInterfaces  (virConnectPtr conn,
-                                                   char **const names,
-                                                   int maxnames);
+int virConnectNumOfInterfaces(virConnectPtr conn);
+int virConnectListInterfaces(virConnectPtr conn,
+                             char **const names,
+                             int maxnames);
 
-int                     virConnectNumOfDefinedInterfaces (virConnectPtr conn);
-int                     virConnectListDefinedInterfaces  (virConnectPtr conn,
-                                                          char **const names,
-                                                          int maxnames);
+int virConnectNumOfDefinedInterfaces(virConnectPtr conn);
+int virConnectListDefinedInterfaces(virConnectPtr conn,
+                                    char **const names,
+                                    int maxnames);
 /*
  * virConnectListAllInterfaces:
  *
@@ -63,45 +63,45 @@ typedef enum {
     VIR_CONNECT_LIST_INTERFACES_ACTIVE        = 1 << 1,
 } virConnectListAllInterfacesFlags;
 
-int                     virConnectListAllInterfaces (virConnectPtr conn,
-                                                     virInterfacePtr **ifaces,
-                                                     unsigned int flags);
+int virConnectListAllInterfaces(virConnectPtr conn,
+                                virInterfacePtr **ifaces,
+                                unsigned int flags);
 
-virInterfacePtr         virInterfaceLookupByName  (virConnectPtr conn,
-                                                   const char *name);
-virInterfacePtr         virInterfaceLookupByMACString (virConnectPtr conn,
-                                                       const char *mac);
+virInterfacePtr virInterfaceLookupByName(virConnectPtr conn,
+                                         const char *name);
+virInterfacePtr virInterfaceLookupByMACString(virConnectPtr conn,
+                                              const char *mac);
 
-const char*             virInterfaceGetName       (virInterfacePtr iface);
-const char*             virInterfaceGetMACString  (virInterfacePtr iface);
+const char* virInterfaceGetName(virInterfacePtr iface);
+const char* virInterfaceGetMACString(virInterfacePtr iface);
 
 typedef enum {
     VIR_INTERFACE_XML_INACTIVE = 1 << 0 /* dump inactive interface information */
 } virInterfaceXMLFlags;
 
-char *                  virInterfaceGetXMLDesc    (virInterfacePtr iface,
-                                                   unsigned int flags);
-virInterfacePtr         virInterfaceDefineXML     (virConnectPtr conn,
-                                                   const char *xmlDesc,
-                                                   unsigned int flags);
+char * virInterfaceGetXMLDesc(virInterfacePtr iface,
+                              unsigned int flags);
+virInterfacePtr virInterfaceDefineXML(virConnectPtr conn,
+                                      const char *xmlDesc,
+                                      unsigned int flags);
 
-int                     virInterfaceUndefine      (virInterfacePtr iface);
+int virInterfaceUndefine(virInterfacePtr iface);
 
-int                     virInterfaceCreate        (virInterfacePtr iface,
-                                                   unsigned int flags);
+int virInterfaceCreate(virInterfacePtr iface,
+                       unsigned int flags);
 
-int                     virInterfaceDestroy       (virInterfacePtr iface,
-                                                   unsigned int flags);
+int virInterfaceDestroy(virInterfacePtr iface,
+                        unsigned int flags);
 
-int                     virInterfaceRef           (virInterfacePtr iface);
-int                     virInterfaceFree          (virInterfacePtr iface);
+int virInterfaceRef(virInterfacePtr iface);
+int virInterfaceFree(virInterfacePtr iface);
 
-int                     virInterfaceChangeBegin   (virConnectPtr conn,
-                                                   unsigned int flags);
-int                     virInterfaceChangeCommit  (virConnectPtr conn,
-                                                   unsigned int flags);
-int                     virInterfaceChangeRollback(virConnectPtr conn,
-                                                   unsigned int flags);
+int virInterfaceChangeBegin(virConnectPtr conn,
+                            unsigned int flags);
+int virInterfaceChangeCommit(virConnectPtr conn,
+                             unsigned int flags);
+int virInterfaceChangeRollback(virConnectPtr conn,
+                               unsigned int flags);
 
 int virInterfaceIsActive(virInterfacePtr iface);
 
