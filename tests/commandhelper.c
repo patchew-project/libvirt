552386312bd3d9fbda879339b59b5e4de933f303
Tim Wiederhake <twiederh@redhat.com>
2021-01-29 17:16:29 +0100

    commandhelper: Convert main to g_auto*
    Signed-off-by: Tim Wiederhake <twiederh@redhat.com>
    Message-Id: <20210129161629.604823-20-twiederh@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>

--- tests/commandhelper.c
+++ tests/commandhelper.c
@@ -306,39 +306,31 @@ static int printInput(struct Arguments *args)
 }
 
 int main(int argc, char **argv) {
-    struct Arguments *args = parseArguments(argc, argv);
-    FILE *log = fopen(abs_builddir "/commandhelper.log", "w");
-    int ret = EXIT_FAILURE;
+    g_autoptr(Arguments) args = parseArguments(argc, argv);
+    g_autoptr(FILE) log = fopen(abs_builddir "/commandhelper.log", "w");
 
     if (!log || !args)
-        goto cleanup;
+        return EXIT_FAILURE;
 
     printArguments(log, argc, argv);
 
     if (printEnvironment(log) != 0)
-        goto cleanup;
+        return EXIT_FAILURE;
 
     if (printFds(log) != 0)
-        goto cleanup;
+        return EXIT_FAILURE;
 
     printDaemonization(log, args);
 
     if (printCwd(log) != 0)
-        goto cleanup;
+        return EXIT_FAILURE;
 
     fprintf(log, "UMASK:%04o\n", umask(0));
 
     if (printInput(args) != 0)
-        goto cleanup;
+        return EXIT_FAILURE;
 
-    ret = EXIT_SUCCESS;
-
- cleanup:
-    if (args)
-        free(args);
-    if (log)
-        fclose(log);
-    return ret;
+    return EXIT_SUCCESS;
 }
 
 #else
