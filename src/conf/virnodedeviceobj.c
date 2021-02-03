f1eb08ac02d7744dd14af7c4d3187fabdbb53c4b
Jonathon Jongsma <jjongsma@redhat.com>
2021-02-03 11:38:51 -0600

    nodedev: add persistence to virNodeDeviceObj
    Consistent with other objects (e.g. virDomainObj), add a field to
    indicate whether the node device is persistent or transient.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>
    Message-Id: <20210203173909.1273000-8-jjongsma@redhat.com>

--- src/conf/virnodedeviceobj.c
+++ src/conf/virnodedeviceobj.c
@@ -40,6 +40,7 @@ struct _virNodeDeviceObj {
     bool skipUpdateCaps;                /* whether to skip checking host caps,
                                            used by testdriver */
     bool active;
+    bool persistent;
 };
 
 struct _virNodeDeviceObjList {
@@ -999,4 +1000,17 @@ virNodeDeviceObjSetActive(virNodeDeviceObjPtr obj,
                           bool active)
 {
     obj->active = active;
+}
+
+bool
+virNodeDeviceObjIsPersistent(virNodeDeviceObjPtr obj)
+{
+    return obj->persistent;
+}
+
+void
+virNodeDeviceObjSetPersistent(virNodeDeviceObjPtr obj,
+                              bool persistent)
+{
+    obj->persistent = persistent;
 }
