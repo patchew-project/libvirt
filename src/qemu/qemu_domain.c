29e1edda8ce6f834b70a34a67a3e94202424dca2
Pavel Hrdina <phrdina@redhat.com>
2021-02-03 16:25:59 +0100

    qemu: implement vhost-user-blk support
    Implements QEMU support for vhost-user-blk together with live
    hotplug/unplug.
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Message-Id: <a5ce250573021453c2ade64f5a4c11758896960c.1612364698.git.phrdina@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -10572,6 +10572,11 @@ qemuDomainPrepareDiskSource(virDomainDiskDefPtr disk,
                             qemuDomainObjPrivatePtr priv,
                             virQEMUDriverConfigPtr cfg)
 {
+    /* Nothing to prepare as it will use -chardev instead
+     * of -blockdev/-drive option. */
+    if (disk->src->type == VIR_STORAGE_TYPE_VHOST_USER)
+        return 0;
+
     qemuDomainPrepareDiskCachemode(disk);
 
     /* set default format for storage pool based disks */
@@ -11058,6 +11063,13 @@ qemuDomainDiskBlockJobIsSupported(virDomainObjPtr vm,
     if (disk->transient) {
         virReportError(VIR_ERR_OPERATION_UNSUPPORTED,
                        _("block jobs are not supported on transient disk '%s'"),
+                       disk->dst);
+        return false;
+    }
+
+    if (virStorageSourceGetActualType(disk->src) == VIR_STORAGE_TYPE_VHOST_USER) {
+        virReportError(VIR_ERR_OPERATION_UNSUPPORTED,
+                       _("block jobs are not supported on vhostuser disk '%s'"),
                        disk->dst);
         return false;
     }
