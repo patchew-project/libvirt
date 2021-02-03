patchew-project/libvirt

commit 77291414c7a8745cf4d2b06d3c38d269cfbcfe32

vircgroup: refactor virCgroupEnableMissingControllers
Use virStringSplit() to get the list of directories needed to be
created. This improves readability of the code and stops passing
absolute path to virCgroupNewFromParent().

Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/util/vircgroup.c
@@ -1131,21 +1131,18 @@ virCgroupEnableMissingControllers(char *path,
                                   virCgroupPtr *group)
 {
     g_autoptr(virCgroup) parent = NULL;
-    char *offset = path;
+    VIR_AUTOSTRINGLIST tokens = virStringSplit(path, "/", 0);
+    size_t i;
 
-    if (virCgroupNew("/",
-                     controllers,
-                     &parent) < 0)
+    if (virCgroupNew("/", controllers, &parent) < 0)
         return -1;
 
-    for (;;) {
+    /* Skip the first token as it is empty string. */
+    for (i = 1; tokens[i]; i++) {
         g_autoptr(virCgroup) tmp = NULL;
-        char *t = strchr(offset + 1, '/');
-        if (t)
-            *t = '\0';
 
         if (virCgroupNewFromParent(parent,
-                                   path,
+                                   tokens[i],
                                    controllers,
                                    &tmp) < 0)
             return -1;
@@ -1153,17 +1150,10 @@ virCgroupEnableMissingControllers(char *path,
         if (virCgroupMakeGroup(parent, tmp, true, VIR_CGROUP_SYSTEMD) < 0)
             return -1;
 
-        if (t) {
-            *t = '/';
-            offset = t;
-            virCgroupFree(parent);
-            parent = g_steal_pointer(&tmp);
-        } else {
-            *group = g_steal_pointer(&tmp);
-            break;
-        }
+        parent = g_steal_pointer(&tmp);
     }
 
+    *group = g_steal_pointer(&parent);
     return 0;
 }
 

commit 63904d853a881dc33a10122244c44803b35da1e0

vircgroup: Don't leak @parent in virCgroupEnableMissingControllers()
A memory leak was identified in
virCgroupEnableMissingControllers():

==11680==    at 0x483EAE5: calloc (vg_replace_malloc.c:760)
==11680==    by 0x4E51780: g_malloc0 (in /usr/lib64/libglib-2.0.so.0.6701.0)
==11680==    by 0x4908618: virCgroupNew (vircgroup.c:701)
==11680==    by 0x49096F4: virCgroupEnableMissingControllers (vircgroup.c:1146)
==11680==    by 0x4909B17: virCgroupNewMachineSystemd (vircgroup.c:1228)
==11680==    by 0x4909E94: virCgroupNewMachine (vircgroup.c:1313)
==11680==    by 0x1694FDBC: qemuInitCgroup (qemu_cgroup.c:946)
==11680==    by 0x1695046B: qemuSetupCgroup (qemu_cgroup.c:1083)
==11680==    by 0x16A60126: qemuProcessLaunch (qemu_process.c:7077)
==11680==    by 0x16A61504: qemuProcessStart (qemu_process.c:7384)
==11680==    by 0x169B84C2: qemuDomainObjStart (qemu_driver.c:6590)
==11680==    by 0x169B8776: qemuDomainCreateWithFlags (qemu_driver.c:6641)

What happens is that new virCgroup is created and stored into
@parent. Then, if @tokens is not empty the for() loop is entered
into where another virCgroup is created and @parent is replaced
with this new virCgroup. But nothing freed the old @parent.

Fixes: 77291414c7a8745cf4d2b06d3c38d269cfbcfe32
Reported-by: Andrea Bolognani <abologna@redhat.com>
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Message-Id: <1d6d449cff3e97d7c511388fc41db1a7bd374d13.1612382812.git.mprivozn@redhat.com>
Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

src/util/vircgroup.c
@@ -1159,6 +1159,7 @@ virCgroupEnableMissingControllers(char *path,
         if (virCgroupMakeGroup(parent, tmp, true, VIR_CGROUP_SYSTEMD) < 0)
             return -1;
 
+        virCgroupFree(parent);
         parent = g_steal_pointer(&tmp);
     }
 