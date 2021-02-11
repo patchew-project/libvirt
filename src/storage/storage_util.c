1c185bd0a39c340f47a6fd56c950e7b1e42bda24
Peter Krempa <pkrempa@redhat.com>
2021-02-11 16:37:47 +0100

    storage: Format qcow2v3 volumes by default
    Format the new volumes with 'compat=1.1' since the minimum supported
    qemu version is now 1.5 rather the pre-historic compat=0.10.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <4dac94e44d9a9bf44b393e79e82449dfabad823a.1613057278.git.pkrempa@redhat.com>

--- src/storage/storage_util.c
+++ src/storage/storage_util.c
@@ -777,7 +777,7 @@ storageBackendCreateQemuImgOpts(virStorageEncryptionInfoDefPtr encinfo,
     if (info->compat)
         virBufferAsprintf(&buf, "compat=%s,", info->compat);
     else if (info->format == VIR_STORAGE_FILE_QCOW2)
-        virBufferAddLit(&buf, "compat=0.10,");
+        virBufferAddLit(&buf, "compat=1.1,");
 
     if (info->features && info->format == VIR_STORAGE_FILE_QCOW2) {
         if (virBitmapIsBitSet(info->features,
