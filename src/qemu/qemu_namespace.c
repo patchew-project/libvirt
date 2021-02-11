patchew-project/libvirt

commit 5c86fbb72d6e90025481db756d7ee04afb9b7f66

qemuDomainDetachDeviceUnlink: Unlink paths in one go
Simirarly to qemuDomainAttachDeviceMknodHelper() which was
modified just a couple of commits ago, modify the unlink helper
which is called on device detach so that it can unlink multiple
files in one go instead of forking off for every single one of
them.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_namespace.c
@@ -1282,47 +1282,24 @@ qemuNamespaceMknodPaths(virDomainObjPtr vm G_GNUC_UNUSED,
 
 
 static int
-qemuDomainDetachDeviceUnlinkHelper(pid_t pid G_GNUC_UNUSED,
-                                   void *opaque)
+qemuNamespaceUnlinkHelper(pid_t pid G_GNUC_UNUSED,
+                          void *opaque)
 {
-    const char *path = opaque;
-
-    VIR_DEBUG("Unlinking %s", path);
-    if (unlink(path) < 0 && errno != ENOENT) {
-        virReportSystemError(errno,
-                             _("Unable to remove device %s"), path);
-        return -1;
-    }
-
-    return 0;
-}
-
-
-static int
-qemuDomainDetachDeviceUnlink(virQEMUDriverPtr driver G_GNUC_UNUSED,
-                             virDomainObjPtr vm,
-                             const char *file,
-                             char * const *devMountsPath,
-                             size_t ndevMountsPath)
-{
+    char **paths = opaque;
     size_t i;
 
-    if (STRPREFIX(file, QEMU_DEVPREFIX)) {
-        for (i = 0; i < ndevMountsPath; i++) {
-            if (STREQ(devMountsPath[i], "/dev"))
-                continue;
-            if (STRPREFIX(file, devMountsPath[i]))
-                break;
-        }
+    for (i = 0; paths[i]; i++) {
+        const char *path = paths[i];
 
-        if (i == ndevMountsPath) {
-            if (virProcessRunInMountNamespace(vm->pid,
-                                              qemuDomainDetachDeviceUnlinkHelper,
-                                              (void *)file) < 0)
-                return -1;
+        VIR_DEBUG("Unlinking %s", path);
+        if (unlink(path) < 0 && errno != ENOENT) {
+            virReportSystemError(errno,
+                                 _("Unable to remove device %s"), path);
+            return -1;
         }
     }
 
+    g_strfreev(paths);
     return 0;
 }
 
@@ -1335,6 +1312,7 @@ qemuDomainNamespaceUnlinkPaths(virDomainObjPtr vm,
     qemuDomainObjPrivatePtr priv = vm->privateData;
     virQEMUDriverPtr driver = priv->driver;
     g_autoptr(virQEMUDriverConfig) cfg = NULL;
+    VIR_AUTOSTRINGLIST unlinkPaths = NULL;
     char **devMountsPath = NULL;
     size_t ndevMountsPath = 0;
     size_t i;
@@ -1351,10 +1329,27 @@ qemuDomainNamespaceUnlinkPaths(virDomainObjPtr vm,
         goto cleanup;
 
     for (i = 0; i < npaths; i++) {
-        if (qemuDomainDetachDeviceUnlink(driver, vm, paths[i],
-                                         devMountsPath, ndevMountsPath) < 0)
-            goto cleanup;
+        const char *file = paths[i];
+
+        if (STRPREFIX(file, QEMU_DEVPREFIX)) {
+            for (i = 0; i < ndevMountsPath; i++) {
+                if (STREQ(devMountsPath[i], "/dev"))
+                    continue;
+                if (STRPREFIX(file, devMountsPath[i]))
+                    break;
+            }
+
+            if (i == ndevMountsPath &&
+                virStringListAdd(&unlinkPaths, file) < 0)
+                return -1;
+        }
     }
+
+    if (unlinkPaths &&
+        virProcessRunInMountNamespace(vm->pid,
+                                      qemuNamespaceUnlinkHelper,
+                                      unlinkPaths) < 0)
+        return -1;
 
     ret = 0;
  cleanup:

commit 96a65347e3268c6ffb1b6f80e8f2a382807a30d6

qemuNamespaceUnlinkPaths: Fix wrong use of iterator variable
'i' is used in both outer and inner loop. Since 'devMountsPath' is now a
NULL-terminated list, we can use a GStrv to iterate it;

Additionally rewrite the conditional of adding to the 'unlinkPaths'
array so that it's more clear what's happening.

Fixes: 5c86fbb72d6e90025481db7
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Message-Id: <53854405b4397bd6cf93a72679b34c6fb30a0e81.1612599659.git.pkrempa@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/qemu/qemu_namespace.c
@@ -1362,14 +1362,20 @@ qemuNamespaceUnlinkPaths(virDomainObjPtr vm,
         const char *file = paths[i];
 
         if (STRPREFIX(file, QEMU_DEVPREFIX)) {
-            for (i = 0; i < ndevMountsPath; i++) {
-                if (STREQ(devMountsPath[i], "/dev"))
+            GStrv mount;
+            bool inSubmount = false;
+
+            for (mount = devMountsPath; *mount; mount++) {
+                if (STREQ(*mount, "/dev"))
                     continue;
-                if (STRPREFIX(file, devMountsPath[i]))
+
+                if (STRPREFIX(file, *mount)) {
+                    inSubmount = true;
                     break;
+                }
             }
 
-            if (i == ndevMountsPath &&
+            if (!inSubmount &&
                 virStringListAdd(&unlinkPaths, file) < 0)
                 return -1;
         }