5d310d5bc8c48c21635856a35e9c7421cb9f1f34
Michal Privoznik <mprivozn@redhat.com>
2021-02-18 14:31:05 +0100

    qemu: Refresh the actual size of virtio-mem on monitor reconnect
    If the QEMU driver restarts it loses the track of the actual size
    of virtio-mem (because it's runtime type of information and thus
    not stored in XML) and therefore, we have to refresh it when
    reconnecting to the domain monitor.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Message-Id: <c3fc3cbb04daf4ad158fff9d7d1a3eccdb3c22df.1613654814.git.mprivozn@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -8523,6 +8523,9 @@ qemuProcessReconnect(void *opaque)
 
     qemuDomainVcpuPersistOrder(obj->def);
 
+    if (qemuDomainUpdateMemoryDeviceInfo(driver, obj, QEMU_ASYNC_JOB_NONE) < 0)
+        goto error;
+
     if (qemuProcessDetectIOThreadPIDs(driver, obj, QEMU_ASYNC_JOB_NONE) < 0)
         goto error;
 
