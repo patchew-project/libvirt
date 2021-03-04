patchew-project/libvirt

commit a208176ca1d9eedf8aa6bf12fde6a7a9579ab549

cpu_map/sync_qemu_i386.py: Add mapping for svme-addr-check
This was added to qemu in commit 5447089c2b3b084b51670af36fc86ee3979e04be.

Signed-off-by: Tim Wiederhake <twiederh@redhat.com>
Reviewed-by: Jiri Denemark <jdenemar@redhat.com>

src/cpu_map/sync_qemu_i386.py
@@ -142,6 +142,7 @@ def translate_feature(name):
         "CPUID_SS": "ss",
         "CPUID_SVM_NPT": "npt",
         "CPUID_SVM_NRIPSAVE": "nrip-save",
+        "CPUID_SVM_SVME_ADDR_CHK": "svme-addr-check",
         "CPUID_TSC": "tsc",
         "CPUID_VME": "vme",
         "CPUID_XSAVE_XGETBV1": "xgetbv1",

commit fe3c5965a242047933f26d5bb959ea351ff1ce4a

cpu_map: Fix spelling of svme-addr-chk feature
Commit a208176ca1d9eedf8aa6bf12fde6a7a9579ab549 introduced this feature
with an incorrect "svme-addr-check" spelling.

Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
Message-Id: <2ae69cbeb530de4b62ff612029b7d1e6d663d038.1614843861.git.jdenemar@redhat.com>
Reviewed-by: Tim Wiederhake <twiederh@redhat.com>

src/cpu_map/sync_qemu_i386.py
@@ -142,7 +142,7 @@ def translate_feature(name):
         "CPUID_SS": "ss",
         "CPUID_SVM_NPT": "npt",
         "CPUID_SVM_NRIPSAVE": "nrip-save",
-        "CPUID_SVM_SVME_ADDR_CHK": "svme-addr-check",
+        "CPUID_SVM_SVME_ADDR_CHK": "svme-addr-chk",
         "CPUID_TSC": "tsc",
         "CPUID_VME": "vme",
         "CPUID_XSAVE_XGETBV1": "xgetbv1",

src/cpu_map/x86_EPYC-Milan.xml
@@ -76,7 +76,7 @@
     <feature name='sse4a'/>
     <feature name='ssse3'/>
     <feature name='svm'/>
-    <feature name='svme-addr-check'/>
+    <feature name='svme-addr-chk'/>
     <feature name='syscall'/>
     <feature name='tsc'/>
     <feature name='umip'/>

src/cpu_map/x86_features.xml
@@ -554,7 +554,7 @@
   <feature name='pfthreshold'>
     <cpuid eax_in='0x8000000a' edx='0x00001000'/>
   </feature>
-  <feature name='svme-addr-check'>
+  <feature name='svme-addr-chk'>
     <cpuid eax_in='0x8000000a' edx='0x10000000'/>
   </feature>
 