patchew-project/libvirt

commit cb29e4e801dc3b4f25038233a502c784ebf1e0c5

qemuBackupJobTerminate: Don't calculate backup job stats if VM isn't active
If the VM isn't active calculating the job stats doesn't make sense.
Additionally this prevents a crash of libvirtd if qemu terminates while
libvirt wasn't running:

Thread 28 "init-backup-tes" received signal SIGSEGV, Segmentation fault.
[Switching to Thread 0x7fffb9310640 (LWP 3201116)]
qemuDomainJobInfoUpdateTime (jobInfo=0x0) at ../../../libvirt/src/qemu/qemu_domainjob.c:275
275	    if (!jobInfo->started)
(gdb) bt
 #0  qemuDomainJobInfoUpdateTime (jobInfo=0x0) at ../../../libvirt/src/qemu/qemu_domainjob.c:275
 #1  0x00007fffcba1a12d in qemuBackupJobTerminate (vm=0x7fff9c1bc840, jobstatus=QEMU_DOMAIN_JOB_STATUS_CANCELED) at ../../../libvirt/src/qemu/qemu_backup.c:563
 #2  0x00007fffcbaefcae in qemuProcessStop
    (driver=0x7fff9c144ff0, vm=0x7fff9c1bc840, reason=VIR_DOMAIN_SHUTOFF_DAEMON, asyncJob=QEMU_ASYNC_JOB_NONE, flags=<optimized out>)
    at ../../../libvirt/src/qemu/qemu_process.c:7812
 #3  0x00007fffcbaf2a10 in qemuProcessReconnect (opaque=<optimized out>) at ../../../libvirt/src/qemu/qemu_process.c:8578
 #4  0x00007ffff7c46bb5 in virThreadHelper (data=<optimized out>) at ../../../libvirt/src/util/virthread.c:233
 #5  0x00007ffff6e453f9 in start_thread () at /lib64/libpthread.so.0
 #6  0x00007ffff766fb53 in clone () at /lib64/libc.so.6

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/qemu/qemu_backup.c
@@ -583,6 +583,9 @@ qemuBackupJobTerminate(virDomainObjPtr vm,
         }
     }
 
+    if (!virDomainObjIsActive(vm))
+        return;
+
     qemuDomainJobInfoUpdateTime(priv->job.current);
 
     g_clear_pointer(&priv->job.completed, qemuDomainJobInfoFree);

commit 9f540572de369e9b8904cbada212d0bf15dc586e

qemuBackupJobTerminate: Fix job termination for inactive VMs
Commit cb29e4e801d didn't take into account that the VM can be inactive
when it's destroyed. This means that the job would remain active also
when the VM became inactive.

To fix this properly:

1) Remove the bogus VM liveness check and early return
    (reverts the aforementioned commit)

2) Conditionalize the stats assignment only when the stats object is
   present
    (properly fix the crash when VM dies when reconnecting)

3) end the asyncjob only when it was already set
   (prevent corruption of priv->jobs_queued)

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1937598
Fixes: cb29e4e801d
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Message-Id: <d8682afba5a6e5085d652c0b25605211534fc80d.1615477731.git.pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_backup.c
@@ -583,27 +583,28 @@ qemuBackupJobTerminate(virDomainObjPtr vm,
         }
     }
 
-    if (!virDomainObjIsActive(vm))
-        return;
+    if (priv->job.current) {
+        qemuDomainJobInfoUpdateTime(priv->job.current);
 
-    qemuDomainJobInfoUpdateTime(priv->job.current);
+        g_clear_pointer(&priv->job.completed, qemuDomainJobInfoFree);
+        priv->job.completed = qemuDomainJobInfoCopy(priv->job.current);
 
-    g_clear_pointer(&priv->job.completed, qemuDomainJobInfoFree);
-    priv->job.completed = qemuDomainJobInfoCopy(priv->job.current);
+        priv->job.completed->stats.backup.total = priv->backup->push_total;
+        priv->job.completed->stats.backup.transferred = priv->backup->push_transferred;
+        priv->job.completed->stats.backup.tmp_used = priv->backup->pull_tmp_used;
+        priv->job.completed->stats.backup.tmp_total = priv->backup->pull_tmp_total;
 
-    priv->job.completed->stats.backup.total = priv->backup->push_total;
-    priv->job.completed->stats.backup.transferred = priv->backup->push_transferred;
-    priv->job.completed->stats.backup.tmp_used = priv->backup->pull_tmp_used;
-    priv->job.completed->stats.backup.tmp_total = priv->backup->pull_tmp_total;
+        priv->job.completed->status = jobstatus;
+        priv->job.completed->errmsg = g_strdup(priv->backup->errmsg);
 
-    priv->job.completed->status = jobstatus;
-    priv->job.completed->errmsg = g_strdup(priv->backup->errmsg);
-
-    qemuDomainEventEmitJobCompleted(priv->driver, vm);
+        qemuDomainEventEmitJobCompleted(priv->driver, vm);
+    }
 
     virDomainBackupDefFree(priv->backup);
     priv->backup = NULL;
-    qemuDomainObjEndAsyncJob(priv->driver, vm);
+
+    if (priv->job.asyncJob == QEMU_ASYNC_JOB_BACKUP)
+        qemuDomainObjEndAsyncJob(priv->driver, vm);
 }
 
 